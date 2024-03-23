65dde8dd5f8551ef0029233c0eae58d02d91ae67
lvliang-intel <liang1.lv@intel.com>
2024-03-23 14:49:02 +0800

    Update scripts for official TGI Gaudi docker (#11)
    * use official docker image
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    * update scripts
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    ---------
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/serving/tgi_gaudi/build_docker.sh
+++ ChatQnA/serving/tgi_gaudi/build_docker.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/huggingface/tgi-gaudi.git
 cd ./tgi-gaudi/
-docker build -t tgi_gaudi . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
+docker build -t ghcr.io/huggingface/tgi-gaudi:1.2.1 . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
