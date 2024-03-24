ef88ec7402e79222624c4b8429d8784e9f1d0d62
lvliang-intel <liang1.lv@intel.com>
2024-03-24 22:36:11 +0800

    Support GuardRails (#9)
    * add guardrails
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    * fix dockerfile
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    * fix integration issues
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    * add package lock json file
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    
    ---------
    
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/langchain/docker/qna-app/app/server.py
+++ ChatQnA/langchain/docker/qna-app/app/server.py
@@ -2,20 +2,20 @@ import os
 from fastapi import FastAPI, APIRouter, Request, UploadFile, File
 from fastapi.responses import RedirectResponse, StreamingResponse, JSONResponse
 from langserve import add_routes
-from rag_redis.chain import chain as qna_rag_redis_chain
 from starlette.middleware.cors import CORSMiddleware
 from langchain_community.llms import HuggingFaceEndpoint
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
 from langchain_community.vectorstores import Redis
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.messages import HumanMessage
-from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.runnables import RunnablePassthrough
 from rag_redis.config import EMBED_MODEL, INDEX_NAME, REDIS_URL, INDEX_SCHEMA
 from utils import (
     create_retriever_from_files, reload_retriever, create_kb_folder, 
     get_current_beijing_time, create_retriever_from_links
 )
 from prompts import contextualize_q_prompt, qa_prompt
+from guardrails import moderation_prompt_for_chat, unsafe_dict
 
 app = FastAPI()
 
@@ -29,17 +29,18 @@ app.add_middleware(
 
 class RAGAPIRouter(APIRouter):
 
-    def __init__(self, upload_dir, entrypoint) -> None:
+    def __init__(self, upload_dir, entrypoint, safety_guard_endpoint) -> None:
         super().__init__()
         self.upload_dir = upload_dir
         self.entrypoint = entrypoint
+        self.safety_guard_endpoint = safety_guard_endpoint
         print(f"[rag - router] Initializing API Router, params:\n \
                     upload_dir={upload_dir}, entrypoint={entrypoint}")
 
         # Define LLM
         self.llm = HuggingFaceEndpoint(
             endpoint_url=entrypoint,
-            max_new_tokens=512,
+            max_new_tokens=1024,
             top_k=10,
             top_p=0.95,
             typical_p=0.95,
@@ -47,6 +48,16 @@ class RAGAPIRouter(APIRouter):
             repetition_penalty=1.03,
             streaming=True,
         )
+        if self.safety_guard_endpoint:
+            self.llm_guard = HuggingFaceEndpoint(
+                endpoint_url=safety_guard_endpoint,
+                max_new_tokens=100,
+                top_k=1,
+                top_p=0.95,
+                typical_p=0.95,
+                temperature=0.01,
+                repetition_penalty=1.03,
+            )
         print("[rag - router] LLM initialized.")
 
         # Define LLM Chain
@@ -85,12 +96,23 @@ class RAGAPIRouter(APIRouter):
         response = self.llm_chain.invoke({"question": query, "chat_history": self.chat_history})
         result = response.split("</s>")[0]
         self.chat_history.extend([HumanMessage(content=query), response])
+        # output guardrails
+        if self.safety_guard_endpoint:
+            response_output_guard = self.llm_guard(moderation_prompt_for_chat("Agent", f"User: {query}\n Agent: {response}"))
+            if 'unsafe' in response_output_guard:
+                policy_violation_level = response_output_guard.split("\n")[1].strip()
+                policy_violations = unsafe_dict[policy_violation_level]
+                print(f"Violated policies: {policy_violations}")
+                return policy_violations + " are found in the output"
+            else:
+                return result
         return result
 
 
 upload_dir = os.getenv("RAG_UPLOAD_DIR", "./upload_dir")
 tgi_endpoint = os.getenv("TGI_ENDPOINT", "http://localhost:8080")
-router = RAGAPIRouter(upload_dir, tgi_endpoint)
+safety_guard_endpoint = os.getenv("SAFETY_GUARD_ENDPOINT")
+router = RAGAPIRouter(upload_dir, tgi_endpoint, safety_guard_endpoint)
 
 
 @router.post("/v1/rag/chat")
@@ -100,6 +122,15 @@ async def rag_chat(request: Request):
     query = params['query']
     kb_id = params.get("knowledge_base_id", "default")
     print(f"[rag - chat] history: {router.chat_history}")
+
+    # prompt guardrails
+    if router.safety_guard_endpoint:
+        response_input_guard = router.llm_guard(moderation_prompt_for_chat("User", query))
+        if 'unsafe' in response_input_guard:
+            policy_violation_level = response_input_guard.split("\n")[1].strip()
+            policy_violations = unsafe_dict[policy_violation_level]
+            print(f"Violated policies: {policy_violations}")
+            return f"Violated policies: {policy_violations}, please check your input."
 
     if kb_id == "default":
         print(f"[rag - chat] use default knowledge base")
@@ -134,6 +165,19 @@ async def rag_chat(request: Request):
     query = params['query']
     kb_id = params.get("knowledge_base_id", "default")
     print(f"[rag - chat_stream] history: {router.chat_history}")
+
+    # prompt guardrails
+    if router.safety_guard_endpoint:
+        response_input_guard = router.llm_guard(moderation_prompt_for_chat("User", query))
+        if 'unsafe' in response_input_guard:
+            policy_violation_level = response_input_guard.split("\n")[1].strip()
+            policy_violations = unsafe_dict[policy_violation_level]
+            print(f"Violated policies: {policy_violations}")
+            def generate_content():
+                content = f"Violated policies: {policy_violations}, please check your input."
+                yield f"data: {content}\n\n"
+                yield f"data: [DONE]\n\n"
+            return StreamingResponse(generate_content(), media_type="text/event-stream")
 
     if kb_id == "default":
         retriever = reload_retriever(router.embeddings, INDEX_NAME)
@@ -248,7 +292,7 @@ app.include_router(router)
 async def redirect_root_to_docs():
     return RedirectResponse("/docs")
 
-add_routes(app, qna_rag_redis_chain, path="/rag-redis")
+add_routes(app, router.llm_chain, path="/rag-redis")
 
 if __name__ == "__main__":
     import uvicorn
