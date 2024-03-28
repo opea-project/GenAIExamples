dc18f661aec101cdebfc4ebea679a9d4f8441dc0
lvliang-intel <liang1.lv@intel.com>
2024-03-28 21:17:56 +0800

    clean the server code
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- CodeGen/codegen/codegen-app/server.py
+++ CodeGen/codegen/codegen-app/server.py
@@ -15,13 +15,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import os
-import types
-from concurrent import futures
 from typing import Optional
 
-import requests
 from fastapi import APIRouter, FastAPI
 from fastapi.responses import RedirectResponse, StreamingResponse
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
@@ -36,6 +32,35 @@ app.add_middleware(
     CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
 )
 
+def filter_code_format(code):
+    language_prefixes = {
+        "go": "```go",
+        "c": "```c",
+        "cpp": "```cpp",
+        "java": "```java",
+        "python": "```python",
+        "typescript": "```typescript",
+    }
+    suffix = "\n```"
+
+    # Find the first occurrence of a language prefix
+    first_prefix_pos = len(code)
+    for prefix in language_prefixes.values():
+        pos = code.find(prefix)
+        if pos != -1 and pos < first_prefix_pos:
+            first_prefix_pos = pos + len(prefix) + 1
+
+    # Find the first occurrence of the suffix after the first language prefix
+    first_suffix_pos = code.find(suffix, first_prefix_pos + 1)
+
+    # Extract the code block
+    if first_prefix_pos != -1 and first_suffix_pos != -1:
+        return code[first_prefix_pos:first_suffix_pos]
+    elif first_prefix_pos != -1:
+        return code[first_prefix_pos:]
+
+    return code
+
 
 class CodeGenAPIRouter(APIRouter):
     def __init__(self, entrypoint) -> None:
@@ -44,41 +69,33 @@ class CodeGenAPIRouter(APIRouter):
         print(f"[codegen - router] Initializing API Router, entrypoint={entrypoint}")
 
         # Define LLM
+        callbacks = [StreamingStdOutCallbackHandler()]
         self.llm = HuggingFaceEndpoint(
             endpoint_url=entrypoint,
-            max_new_tokens=512,
+            max_new_tokens=1024,
             top_k=10,
             top_p=0.95,
             typical_p=0.95,
             temperature=0.01,
             repetition_penalty=1.03,
             streaming=True,
+            callbacks=callbacks,
         )
         print("[codegen - router] LLM initialized.")
-
-    def is_generator(self, obj):
-        return isinstance(obj, types.GeneratorType)
 
     def handle_chat_completion_request(self, request: ChatCompletionRequest):
         try:
             print(f"Predicting chat completion using prompt '{request.prompt}'")
-            buffered_texts = ""
             if request.stream:
-                generator = self.llm(request.prompt, callbacks=[StreamingStdOutCallbackHandler()])
-                breakpoint()
-                print("generator================", generator)
-                if not self.is_generator(generator):
-                    generator = (generator,)
-
-                def stream_generator():
-                    nonlocal buffered_texts
-                    for output in generator:
-                        yield f"data: {output.encode()}\n\n"
+                async def stream_generator():
+                    for chunk in self.llm.stream(request.prompt):
+                        yield f"data: {chunk.encode()}\n\n"
                     yield "data: [DONE]\n\n"
 
                 return StreamingResponse(stream_generator(), media_type="text/event-stream")
             else:
-                response = self.llm(request.prompt)
+                result = self.llm(request.prompt)
+                response = filter_code_format(result)
         except Exception as e:
             print(f"An error occurred: {e}")
         else:
@@ -113,37 +130,6 @@ def check_completion_request(request: BaseModel) -> Optional[str]:
         return f"Param Error: {request.max_new_tokens} is not valid under any of the given schemas --- 'max_new_tokens'"
 
     return None
-
-
-def filter_code_format(code):
-    language_prefixes = {
-        "go": "```go",
-        "c": "```c",
-        "cpp": "```cpp",
-        "java": "```java",
-        "python": "```python",
-        "typescript": "```typescript",
-    }
-    suffix = "\n```"
-
-    # Find the first occurrence of a language prefix
-    first_prefix_pos = len(code)
-    for prefix in language_prefixes.values():
-        pos = code.find(prefix)
-        if pos != -1 and pos < first_prefix_pos:
-            first_prefix_pos = pos + len(prefix) + 1
-
-    # Find the first occurrence of the suffix after the first language prefix
-    first_suffix_pos = code.find(suffix, first_prefix_pos + 1)
-
-    # Extract the code block
-    if first_prefix_pos != -1 and first_suffix_pos != -1:
-        return code[first_prefix_pos:first_suffix_pos]
-    elif first_prefix_pos != -1:
-        return code[first_prefix_pos:]
-
-    return code
-
 
 # router /v1/code_generation only supports non-streaming mode.
 @router.post("/v1/code_generation")
