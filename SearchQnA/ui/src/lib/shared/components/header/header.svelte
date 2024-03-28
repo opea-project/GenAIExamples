ea907d4ff0bf09a7b5815eed0593e9a44417e484
lvliang-intel <liang1.lv@intel.com>
2024-03-28 23:15:16 +0800

    Add SearchQnA ui part
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

---
+++ SearchQnA/ui/src/lib/shared/components/header/header.svelte
@@ -0,0 +1,20 @@
+<script>
+
+  import IntelLogo from "$lib/assets/header/intelLogo.svelte";
+
+</script>
+<header
+  class="sticky top-0 z-40 flex-none w-full mx-auto bg-[#0054ae] border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800"
+>
+  <nav
+    class="bg-[#0054ae] dark:bg-gray-800 text-gray-500 dark:text-gray-400 border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 px-2 sm:px-4 w-full py-1.5"
+  >
+    <div class="mx-auto flex flex-wrap justify-between items-center w-full">
+        <IntelLogo />
+      <span
+        class="self-center whitespace-nowrap text-2xl font-semibold text-white ml-4"
+        data-svelte-h="svelte-1hbktnk">Web Search Chatbot</span
+      >
+    </div>
+  </nav>
+</header>
---
+++ SearchQnA/ui/src/lib/shared/components/webSearch/webSearch.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { isCheckedStore } from "$lib/shared/stores/common/Store";
+
+	let isChecked = false;
+	
+    $: isChecked ? isCheckedStore.set(isChecked) : '';
+</script>
+
+<label class="inline-flex cursor-pointer items-center">
+	<input
+		type="checkbox"
+		bind:checked={isChecked}
+		value=""
+		class="peer sr-only"
+	/>
+	<div
+		class="peer relative h-6 w-11 rounded-full bg-gray-200 after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
+	/>
+	<span class="ms-3 text-base font-medium text-gray-600 dark:text-gray-300"
+		>Web Access</span
+	>
+</label>
