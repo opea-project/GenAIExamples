37541573c9b8fca5ce022d0aaad710b5dec8960e
WenjiaoYue <wenjiao.yue@intel.com>
2024-04-15 11:32:23 +0800

    remove intel logo (#79)
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- SearchQnA/ui/src/lib/shared/components/header/header.svelte
+++ SearchQnA/ui/src/lib/shared/components/header/header.svelte
@@ -16,8 +16,6 @@
 
 <script>
 
-  import IntelLogo from "$lib/assets/header/intelLogo.svelte";
-
 </script>
 <header
   class="sticky top-0 z-40 flex-none w-full mx-auto bg-[#0054ae] border-b border-gray-200 dark:border-gray-600 dark:bg-gray-800"
@@ -25,10 +23,9 @@
   <nav
     class="bg-[#0054ae] dark:bg-gray-800 text-gray-500 dark:text-gray-400 border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 px-2 sm:px-4 w-full py-1.5"
   >
-    <div class="mx-auto flex flex-wrap justify-between items-center w-full">
-        <IntelLogo />
+    <div class="mx-auto flex flex-wrap justify-end items-center w-full">
       <span
-        class="self-center whitespace-nowrap text-2xl font-semibold text-white ml-4"
+        class="self-center py-2 whitespace-nowrap text-2xl font-semibold text-white ml-4"
         data-svelte-h="svelte-1hbktnk">Web Search Chatbot</span
       >
     </div>
