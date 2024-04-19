ea3c914d85ec162839e6794ca46c0c68d77440f2
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-04-19 13:26:35 +0800

    add AudioQnA UI
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ AudioQnA/front_end/src/lib/components/layout.svelte
@@ -0,0 +1,21 @@
+<script>
+	import TopNavigation from '$lib/components/topnavigation/Index.svelte';
+
+	const style = {
+		container: `bg-gray-100 h-screen overflow-hidden relative`,
+		main: `h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 lg:px-4`,
+		mainContainer: `flex flex-col h-screen  w-full `
+	};
+
+</script>
+
+<div class={style.container}>
+	<div class="flex items-start">
+		<div class={style.mainContainer}>
+			<TopNavigation />
+			<main class={style.main}>
+				<slot />
+			</main>
+		</div>
+	</div>
+</div>
---
+++ AudioQnA/front_end/src/lib/components/shared/shared-utils.ts
@@ -0,0 +1,8 @@
+export function scrollToBottom(scrollToDiv: HTMLElement) {
+  if (scrollToDiv) {
+    setTimeout(() => scrollToDiv.scroll({
+      behavior: "auto",
+      top: scrollToDiv.scrollHeight,
+    }), 300)
+  }
+}
