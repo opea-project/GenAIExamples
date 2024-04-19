99727926b0f6d16fb8bcd52cffcdd46137dd8f7b
WenjiaoYue <wenjiao.yue@intel.com>
2024-04-19 22:48:35 +0800

    Add AudioQnA UI (#92)
    * add AudioQnA UI
    
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ AudioQnA/front_end/src/lib/assets/icons/svelte/message-avatar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import PersonOutlined from "$lib/assets/icons/svelte/portrait.svelte";
+	import AssistantIcon from "$lib/assets/icons/svelte/assistant.svelte";
+	export let role: string;
+
+</script>
+
+{#if role === "Human"}
+	<PersonOutlined />
+{:else}
+	<AssistantIcon />
+{/if}
---
+++ AudioQnA/front_end/src/lib/components/talkbot/voice-wave.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let running = false
+</script>
+
+{#if running}
+    <div class="img bg-cover -mb-20"></div>
+{/if}
+
+<style>
+    .img {
+        width: 600px;
+        height: 300px;
+        animation-name: flowingAnimation;
+        animation-duration: 3s;
+        animation-iteration-count: infinite;
+        /* animation-timing-function: cubic-bezier(0.1, 0.7, 1, 0.1); */
+        animation-timing-function: linear;
+    }
+
+    @keyframes flowingAnimation {
+        0% {
+            background-image: url(../../assets/icons/svg/1.svg)
+        }
+
+        20% {
+            background-image: url(../../assets/icons/svg/2.svg)
+        }
+
+        40% {
+            background-image: url(../../assets/icons/svg/3.svg)
+        }
+
+        60% {
+            background-image: url(../../assets/icons/svg/4.svg)
+        }
+
+        80% {
+            background-image: url(../../assets/icons/svg/5.svg)
+        }
+
+        100% {
+            background-image: url(../../assets/icons/svg/1.svg)
+        }
+    }
+</style>
