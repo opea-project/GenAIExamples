cd32055f117b5b867d1f116d9023e5c21f8d229c
letonghan <letong.han@intel.com>
2024-04-24 17:45:08 +0800

    base classes of microservice
    Signed-off-by: letonghan <letong.han@intel.com>

---
+++ GenAIComps/mega/base_service.py
@@ -0,0 +1,149 @@
+import abc
+from logger import CustomLogger
+from types import SimpleNamespace
+from typing import Dict, Optional, TYPE_CHECKING
+
+
+__all__ = ['BaseServer']
+
+
+class BaseService():
+    """
+    BaseService creates a HTTP/gRPC server as a microservice.
+    """
+
+    def __init__(
+        self,
+        name: Optional[str] = 'Base service',
+        runtime_args: Optional[Dict] = None,
+        **kwargs,
+    ):
+        """
+        Initialize the BaseService with a name, runtime arguments, and any additional arguments.
+        """
+        self.name = name
+        self.runtime_args = runtime_args
+        self._process_runtime_args()
+        self.title = self.runtime_args.title
+        self.description = self.runtime_args.description
+        self.logger = CustomLogger(self.name)
+        self.server = None
+
+    def _process_runtime_args(self):
+        """
+        Process the runtime arguments to ensure they are in the correct format.
+        """
+        _runtime_args = (
+            self.runtime_args
+            if isinstance(self.runtime_args, dict)
+            else vars(self.runtime_args or {})
+        )
+        self.runtime_args = SimpleNamespace(**_runtime_args)
+
+    @property
+    def primary_port(self):
+        """
+        Gets the first port of the port list argument.
+        :return: The first port to be exposed
+        """
+        return (
+            self.runtime_args.port[0]
+            if isinstance(self.runtime_args.port, list)
+            else self.runtime_args.port
+        )
+
+    @property
+    def all_ports(self):
+        """
+        Gets all the list of ports from the runtime_args as a list.
+        :return: The lists of ports to be exposed
+        """
+        return (
+            self.runtime_args.port
+            if isinstance(self.runtime_args.port, list)
+            else [self.runtime_args.port]
+        )
+
+    @property
+    def protocols(self):
+        """
+        Gets all the list of protocols from the runtime_args as a list.
+        :return: The lists of protocols to be exposed
+        """
+        return (
+            self.runtime_args.protocol
+            if isinstance(self.runtime_args.protocol, list)
+            else [self.runtime_args.protocol]
+        )
+
+    @property
+    def host_address(self):
+        """
+        Gets the host from the runtime_args
+        :return: The host where to bind the gateway
+        """
+        return self.runtime_args.host or '127.0.0.1'
+
+    @abc.abstractmethod
+    async def initialize_server(self):
+        """
+        Abstract method to setup the server. This should be implemented in the child class.
+        """
+        ...
+
+    @abc.abstractmethod
+    async def execute_server(self):
+        """
+        Abstract method to run the server indefinitely. This should be implemented in the child class.
+        """
+        ...
+
+    @abc.abstractmethod
+    async def terminate_server(self):
+        """
+        Abstract method to shutdown the server and free other allocated resources, e.g, health check service, etc.
+        This should be implemented in the child class.
+        """
+        ...
+
+    @staticmethod
+    def check_server_readiness(
+        ctrl_address: str,
+        protocol: Optional[str] = 'http',
+        **kwargs,
+    ) -> bool:
+        """
+        Check if server status is ready.
+        :param ctrl_address: the address where the control request needs to be sent.
+        :param protocol: protocol of the service.
+        :param kwargs: extra keyword arguments.
+        :return: True if status is ready else False.
+        """
+        from http_service import HTTPService
+        res = False
+        if protocol is None or protocol == 'http':
+            res = HTTPService.check_readiness(ctrl_address)
+        return res
+
+    
+    @staticmethod
+    async def async_check_server_readiness(
+        ctrl_address: str,
+        protocol: Optional[str] = 'grpc',
+        **kwargs,
+    ) -> bool:
+        """
+        Asynchronously check if server status is ready.
+        :param ctrl_address: the address where the control request needs to be sent.
+        :param protocol: protocol of the service.
+        :param kwargs: extra keyword arguments.
+        :return: True if status is ready else False.
+        """
+        if TYPE_CHECKING:
+            from http_service import HTTPService
+        res = False
+        if protocol is None or protocol == 'http':
+            res = await HTTPService.async_check_readiness(ctrl_address)
+        return res
+
+
--- GenAIComps/mega/service.py
+++ GenAIComps/mega/service.py
@@ -1,1 +1,177 @@
-# microservice base class
+from typing import Optional
+from base_service import BaseService
+from fastapi import FastAPI
+from uvicorn import Config, Server
+
+
+class HTTPService(BaseService):
+    """
+    FastAPI HTTP service based on BaseService class. This property should return a fastapi app.
+    """
+
+    def __init__(
+        self,
+        uvicorn_kwargs: Optional[dict] = None,
+        cors: Optional[bool] = False,
+        **kwargs,
+    ):
+        """
+        Initialize the HTTPService
+        :param uvicorn_kwargs: Dictionary of kwargs arguments that will be passed to Uvicorn server when starting the server
+        :param cors: If set, a CORS middleware is added to FastAPI frontend to allow cross-origin access.
+        :param kwargs: keyword args
+        """
+        super().__init__(**kwargs)
+        self.uvicorn_kwargs = uvicorn_kwargs or {}
+        self.cors = cors
+        self._app = self._create_app()
+
+    @property
+    def app(self):
+        """
+        Get the default base API app for Server
+        :return: Return a FastAPI app for the default HTTPGateway
+        """
+        return self._app
+    
+    def _create_app(self):
+        """
+        Create a FastAPI application.
+        :return: a FastAPI application.
+        """
+        app = FastAPI(title=self.title, description=self.description)
+
+        if self.cors:
+            from fastapi.middleware.cors import CORSMiddleware
+            app.add_middleware(
+                CORSMiddleware,
+                allow_origins=['*'],
+                allow_credentials=True,
+                allow_methods=['*'],
+                allow_headers=['*'],
+            )
+            self.logger.info('CORS is enabled.')
+
+        @app.get(
+            path='/v1/health_check',
+            summary='Get the status of GenAI microservice',
+            tags=['Debug'],
+        )
+        async def _health_check():
+            """
+            Get the health status of this GenAI microservice.
+            """
+            return {
+                'Service Title': self.title,
+                'Service Description': self.description
+            }
+        
+        return app
+
+    async def initialize_server(self):
+        """
+        Initialize and return HTTP server
+        """
+        self.logger.info(f'Setting up HTTP server')
+        
+
+        class UviServer(Server):
+            """
+            The uvicorn server.
+            """
+
+            async def setup_server(self, sockets=None):
+                """
+                Setup uvicorn server.
+                :param sockets: sockets of server.
+                """
+                config = self.config
+                if not config.loaded:
+                    config.load()
+                self.lifespan = config.lifespan_class(config)
+                await self.startup(sockets=sockets)
+                if self.should_exit:
+                    return
+
+            async def start_server(self, **kwargs):
+                """
+                Start the server.
+                :param kwargs: keyword arguments
+                """
+                await self.main_loop()
+
+        app = self.app
+        
+        self.server = UviServer(
+            config=Config(
+                app=app,
+                host=self.host_address,
+                port=self.primary_port,
+                log_level='info',
+                **self.uvicorn_kwargs,
+            )
+        )
+        self.logger.info(f'Uvicorn server setup on port {self.primary_port}')
+        await self.server.setup_server()
+        self.logger.info(f'HTTP server setup successful')
+
+    async def execute_server(self):
+        """
+        Run the HTTP server indefinitely.
+        """
+        await self.server.start_server()
+    
+    async def terminate_server(self):
+        """
+        Terminate the HTTP server and free resources allocated when setting up the server.
+        """
+        self.logger.info('Initiating server termination')
+        await super().shutdown()
+        self.server.should_exit = True
+        await self.server.shutdown()
+        self.logger.info('Server termination completed')
+
+    @staticmethod
+    def check_server_readiness(
+        ctrl_address: str, 
+        timeout: float = 1.0, 
+        logger=None, 
+        **kwargs
+    ) -> bool:
+        """
+        Check if server status is ready.
+        :param ctrl_address: the address where the control request needs to be sent
+        :param timeout: timeout of the health check in seconds
+        :param logger: Customized Logger to be used
+        :param kwargs: extra keyword arguments
+        :return: True if status is ready else False.
+        """
+        import urllib.request
+        from http import HTTPStatus
+
+        try:
+            conn = urllib.request.urlopen(url=f'http://{ctrl_address}', timeout=timeout)
+            return conn.code == HTTPStatus.OK
+        except Exception as exc:
+            if logger:
+                logger.info(f'Exception: {exc}')
+
+            return False
+
+    @staticmethod
+    async def async_check_server_readiness(
+        ctrl_address: str, 
+        timeout: float = 1.0, 
+        logger=None, 
+        **kwargs
+    ) -> bool:
+        """
+        Asynchronously check if server status is ready.
+        :param ctrl_address: the address where the control request needs to be sent
+        :param timeout: timeout of the health check in seconds
+        :param logger: Customized Logger to be used
+        :param kwargs: extra keyword arguments
+        :return: True if status is ready else False.
+        """
+        return HTTPService.check_server_readiness(ctrl_address, timeout, logger=logger)
+
