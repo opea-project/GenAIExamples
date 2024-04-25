2fe35e4a30ea34a6801484d75bca711496c71009
letonghan <letong.han@intel.com>
2024-04-25 15:30:37 +0800

    modify docker name & stream data
    Signed-off-by: letonghan <letong.han@intel.com>

--- CodeTrans/langchain/docker/codetrans-app/prompts.py
+++ CodeTrans/langchain/docker/codetrans-app/prompts.py
@@ -4,8 +4,12 @@ prompt_template = """
     ### System: Please translate the following {language_from} codes into {language_to} codes.
 
     ### Original codes:
-    {source_code}
+    '''{language_from}
 
+    {source_code}
+    
+    '''
+    
     ### Translated codes:
 """
 codetrans_prompt_template = PromptTemplate.from_template(prompt_template)
