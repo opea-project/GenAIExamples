5f8108dc78898827ee39f7de5dbcd2c4e178aaec
WenjiaoYue <wenjiao.yue@intel.com>
2024-04-26 17:21:46 +0800

    language translation UI (#96)
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ LanguageTrans/ui/svelte/src/lib/shared/Network.ts
@@ -0,0 +1,43 @@
+// Copyright (c) 2024 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { env } from "$env/dynamic/public";
+
+const BASIC_URL = env.BASIC_URL;
+
+async function fetchPostRes(url, init) {
+  try {
+    const response = await fetch(url, init);
+    if (!response.ok) throw response.status;
+    return await response.json();
+  } catch (error) {
+    console.error("network error: ", error);
+    return undefined;
+  }
+}
+
+export async function fetchLanguageResponse(input: string, transform: string, transTo: string) {
+  const url = `${BASIC_URL}/v1/translation`;
+  const transData = {
+    language_from: transform,
+    language_to: transTo,
+    source_language: input,
+  };
+
+  const init: RequestInit = {
+    method: "POST",
+    body: JSON.stringify(transData),
+  };
+
+  return fetchPostRes(url, init);
+}
