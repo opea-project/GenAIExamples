9bec4784613f09f180edb9cf479c174757bd532c
chen, suyue <suyue.chen@intel.com>
2024-04-26 13:28:33 +0800

    update license check (#101)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- AudioQnA/front_end/src/lib/assets/icons/svelte/assistant.svelte
+++ AudioQnA/front_end/src/lib/assets/icons/svelte/assistant.svelte
@@ -1,1 +1,17 @@
+<!--
+  Copyright (c) 2024 Intel Corporation
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+-->
+
 <svg t="1713432596850" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="7421" width="32" height="32"><path d="M399.837 3.809h244.886v1016.381h-244.886v-1016.381z" fill="#231915" p-id="7422"></path></svg>
