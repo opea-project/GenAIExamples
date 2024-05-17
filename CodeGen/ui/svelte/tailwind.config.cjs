49999e0935245410ab0f54ccea76ca5aa2c73b1e
WenjiaoYue <wenjiao.yue@intel.com>
2024-05-17 12:05:05 +0800

    update Code Gen ui (#150)
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ CodeGen/ui/svelte/tailwind.config.cjs
@@ -0,0 +1,29 @@
+const config = {
+  content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+
+  plugins: [require("flowbite/plugin")],
+
+  darkMode: "class",
+
+  theme: {
+    extend: {
+      colors: {
+        // flowbite-svelte
+        primary: {
+          50: "#f2f8ff",
+          100: "#eef5ff",
+          200: "#deecff",
+          300: "#cce2ff",
+          400: "#add0ff",
+          500: "#5da2fe",
+          600: "#2f81ef",
+          700: "#2780eb",
+          800: "#226fcc",
+          900: "#1b5aa5",
+        },
+      },
+    },
+  },
+};
+
+module.exports = config;
