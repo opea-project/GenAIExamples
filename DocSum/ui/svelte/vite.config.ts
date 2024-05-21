c14207bd0c0260ab9e44e7feb770d54aee37de47
zehao-intel <zehao.huang@intel.com>
2024-05-21 16:30:21 +0800

    Fix DocSum UI svelte Folder (#161)
    Signed-off-by: zehao-intel <zehao.huang@intel.com>
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>

--- DocSum/ui/svelte/vite.config.ts
+++ DocSum/ui/svelte/vite.config.ts
@@ -13,11 +13,8 @@
 // limitations under the License.
 
 import { sveltekit } from "@sveltejs/kit/vite";
-import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
 
-const config: UserConfig = {
-	plugins: [sveltekit()],
-	server: {},
-};
-
-export default config;
+export default defineConfig({
+  plugins: [sveltekit()],
+});
