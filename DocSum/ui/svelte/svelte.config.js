9904faa52455590111e2737e8dbcb3705f891bfb
zehao-intel <zehao.huang@intel.com>
2024-05-21 16:30:21 +0800

    Fix DocSum UI svelte Folder (#161)
    Signed-off-by: zehao-intel <zehao.huang@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- DocSum/ui/svelte/svelte.config.js
+++ DocSum/ui/svelte/svelte.config.js
@@ -13,26 +13,23 @@
 // limitations under the License.
 
 import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
-import postcssPresetEnv from "postcss-preset-env";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		sourceMap: true,
-		postcss: {
-			plugins: [postcssPresetEnv({ features: { "nesting-rules": true } })],
-		},
-	}),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess({})],
 
-	kit: {
-		adapter: adapter(),
-		env: {
-			publicPrefix: "",
-		},
-	},
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter(),
+    env: {
+      publicPrefix: "",
+    },
+  },
 };
 
 export default config;
