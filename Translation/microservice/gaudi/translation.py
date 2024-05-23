2d36d5dbf2c85b48ae5c4f285413f3e79ef1aa9e
zehao-intel <zehao.huang@intel.com>
2024-05-23 10:32:38 +0800

    fix import
    Signed-off-by: zehao-intel <zehao.huang@intel.com>

--- Translation/microservice/gaudi/translation.py
+++ Translation/microservice/gaudi/translation.py
@@ -12,8 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
+import os
 
 from comps import DocSumGateway, MicroService, ServiceOrchestrator, ServiceType
+
+SERVICE_HOST_IP = os.getenv("MEGA_SERVICE_HOST_IP", "0.0.0.0")
 
 
 class TranslationService:
