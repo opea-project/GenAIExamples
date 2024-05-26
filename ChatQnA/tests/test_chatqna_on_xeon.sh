87886e6e02f40128033728d9e7507ffa44152c64
chensuyue <suyue.chen@intel.com>
2024-05-27 00:34:56 +0800

    add exit early with error
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- ChatQnA/tests/test_chatqna_on_xeon.sh
+++ ChatQnA/tests/test_chatqna_on_xeon.sh
@@ -2,7 +2,7 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
-set -x
+set -xe
 
 WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
--- CodeGen/tests/test_codegen_on_gaudi.sh
+++ CodeGen/tests/test_codegen_on_gaudi.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -x
+set -xe
 
 WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
