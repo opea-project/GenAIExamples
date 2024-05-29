cd0e378f26854d820eb873cf7e631baf5bd86656
Tian, Feng <feng.tian@intel.com>
2024-05-30 00:13:49 +0800

    GenAIExample code structure reorg (#207)
    Signed-off-by: Tian, Feng <feng.tian@intel.com>
    Signed-off-by: chensuyue <suyue.chen@intel.com>
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>

--- DocSum/tests/test_docsum_on_xeon.sh
+++ DocSum/tests/test_docsum_on_xeon.sh
@@ -15,17 +15,17 @@ function build_docker_images() {
 
     docker build --no-cache -t opea/llm-docsum-tgi:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/llms/summarization/tgi/Dockerfile .
 
-    cd $WORKPATH
+    cd $WORKPATH/docker
     docker build --no-cache -t opea/docsum:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
 
-    cd $WORKPATH/ui
+    cd $WORKPATH/docker/ui
     docker build --no-cache -t opea/docsum-ui:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f docker/Dockerfile .
 
     docker images
 }
 
 function start_services() {
-    cd $WORKPATH/docker-composer/xeon
+    cd $WORKPATH/docker/xeon
 
     export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
     export TGI_LLM_ENDPOINT="http://${ip_address}:8008"
@@ -96,7 +96,7 @@ function validate_megaservice() {
 }
 
 function stop_docker() {
-    cd $WORKPATH/docker-composer/xeon
+    cd $WORKPATH/docker/xeon
     container_list=$(cat docker_compose.yaml | grep container_name | cut -d':' -f2)
     for container_name in $container_list; do
         cid=$(docker ps -aq --filter "name=$container_name")
