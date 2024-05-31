5efa8ca3ff409848162891c4a34c06b5c1f35576
WenjiaoYue <wenjiao.yue@intel.com>
2024-05-31 18:50:50 +0800

    delete useless code

--- CodeTrans/docker/ui/svelte/playwright.config.ts
+++ CodeTrans/docker/ui/svelte/playwright.config.ts
@@ -45,43 +45,12 @@ export default defineConfig({
   },
 
   /* Configure projects for major browsers */
-  projects: [
-    // {
-    // 	name: "chromium",
-    // 	use: { ...devices["Desktop Chrome"] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { channel: 'msedge' },
-    // },
+  projects: [ 
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
     },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { channel: 'chrome' },
-    // },
+
   ],
 
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 });
