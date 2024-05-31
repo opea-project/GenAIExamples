8c11fdd65f50bb7a34a9e0176d4dc6459177deb2
WenjiaoYue <wenjiao.yue@intel.com>
2024-05-31 18:20:05 +0800

    Update codeGen playwright (#246)
    * update docSum ut
    
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update .env
    
    * update codeGen ut
    
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- CodeGen/docker/ui/svelte/tests/codeGen.spec.ts
+++ CodeGen/docker/ui/svelte/tests/codeGen.spec.ts
@@ -9,12 +9,15 @@ test.beforeEach(async ({ page }) => {
 });
 
 // Constants definition
-const CHAT_ITEMS = ["print('hello')"];
+const CHAT_ITEMS = [
+	"Implement a high-level API for a TODO list application. The API takes as input an operation request and updates the TODO list in place. If the request is invalid, raise an exception.",
+];
 
 // Helper function: Enter message to chat
 async function enterMessageToChat(page: Page, message: string) {
 	await page.getByTestId("code-input").click();
 	await page.getByTestId("code-input").fill(message);
+	await page.getByTestId("code-input").press("Enter");
 	await page.waitForTimeout(10000);
 	await expect(page.getByTestId("code-output")).toContainText("copy");
 }
