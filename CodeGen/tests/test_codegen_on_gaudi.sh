e79441044c5813d7d0e4c867208a164562f60f8b
Letong Han <106566639+letonghan@users.noreply.github.com>
2024-05-31 18:06:06 +0800

    refine codegen test scripts (#241)
    Signed-off-by: letonghan <letong.han@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- CodeGen/tests/test_codegen_on_gaudi.sh
+++ CodeGen/tests/test_codegen_on_gaudi.sh
@@ -43,58 +43,62 @@ function start_services() {
     sleep 2m # Waits 2 minutes
 }
 
-function validate_microservices() {
-    # Check if the microservices are running correctly.
-    # TODO: Any results check required??
+function validate_services() {
+    local URL="$1"
+    local EXPECTED_RESULT="$2"
+    local SERVICE_NAME="$3"
+    local DOCKER_NAME="$4"
+    local INPUT_DATA="$5"
 
-    export PATH="${HOME}/miniforge3/bin:$PATH"
+    local HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL")
+    if [ "$HTTP_STATUS" -eq 200 ]; then
+        echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
 
-    curl http://${ip_address}:8028/generate \
-        -X POST \
-        -d '{"inputs":"def print_hello_world():","parameters":{"max_new_tokens":1024, "do_sample": true}}' \
-        -H 'Content-Type: application/json' > ${LOG_PATH}/generate.log
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        echo "Microservice failed, please check the logs in artifacts!"
-        docker logs tgi-gaudi-server >> ${LOG_PATH}/generate.log
-        exit 1
-    fi
-    sleep 5s
+        local CONTENT=$(curl -s -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL" | tee ${LOG_PATH}/${SERVICE_NAME}.log)
 
-    curl http://${ip_address}:9000/v1/chat/completions \
-        -X POST \
-        -d '{"text":"def print_hello_world():"}' \
-        -H 'Content-Type: application/json' > ${LOG_PATH}/completions.log
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-        echo "Microservice failed, please check the logs in artifacts!"
-        docker logs llm-tgi-gaudi-server >> ${LOG_PATH}/completions.log
+        if echo "$CONTENT" | grep -q "$EXPECTED_RESULT"; then
+            echo "[ $SERVICE_NAME ] Content is as expected."
+        else
+            echo "[ $SERVICE_NAME ] Content does not match the expected result: $CONTENT"
+            docker logs ${DOCKER_NAME} >> ${LOG_PATH}/${SERVICE_NAME}.log
+            exit 1
+        fi
+    else
+        echo "[ $SERVICE_NAME ] HTTP status is not 200. Received status was $HTTP_STATUS"
+        docker logs ${DOCKER_NAME} >> ${LOG_PATH}/${SERVICE_NAME}.log
         exit 1
     fi
     sleep 5s
 }
 
+function validate_microservices() {
+    # tgi for llm service
+    validate_services \
+        "${ip_address}:8028/generate" \
+        "generated_text" \
+        "tgi-llm" \
+        "tgi-gaudi-server" \
+        '{"inputs":"def print_hello_world():","parameters":{"max_new_tokens":256, "do_sample": true}}'
+
+    # llm microservice
+    validate_services \
+        "${ip_address}:9000/v1/chat/completions" \
+        "data: " \
+        "llm" \
+        "llm-tgi-gaudi-server" \
+        '{"query":"def print_hello_world():"}'
+
+}
+
 function validate_megaservice() {
     # Curl the Mega Service
-    curl http://${ip_address}:6666/v1/codegen -H "Content-Type: application/json" -d '{
-        "messages": "def print_hello_world():"}' > ${LOG_PATH}/curl_megaservice.log
+    validate_services \
+        "${ip_address}:6666/v1/codegen" \
+        "print" \
+        "mega-codegen" \
+        "codegen-gaudi-backend-server" \
+        '{"messages": "def print_hello_world():"}'
 
-    echo "Checking response results, make sure the output is reasonable. "
-    local status=false
-    if [[ -f $LOG_PATH/curl_megaservice.log ]] && \
-    [[ $(grep -c "Hello" $LOG_PATH/curl_megaservice.log) != 0 ]]; then
-        status=true
-    fi
-
-    if [ $status == false ]; then
-        echo "Response check failed, please check the logs in artifacts!"
-        exit 1
-    else
-        echo "Response check succeed!"
-    fi
-
-    echo "Checking response format, make sure the output format is acceptable for UI."
-    # TODO
 }
 
 function stop_docker() {
