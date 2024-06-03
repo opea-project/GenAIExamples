041199e32a3526363fda6f3219e00b033683f12c
Bashir Mohd <bashir.mohammed@intel.com>
2024-06-03 13:50:34 -0700

    Add history parameter to the function

--- VideoRAGQnA/utils/prompt_handler.py
+++ VideoRAGQnA/utils/prompt_handler.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, BaseLoader
 
 PROMPT = open("utils/prompt_template.jinja2").read().strip()
 
-def get_formatted_prompt(scene, prompt):
+def get_formatted_prompt(scene, prompt, history):
     env = Environment(loader=BaseLoader())
     template = env.from_string(PROMPT)
-    return template.render(scene=scene, prompt=prompt)
+    return template.render(scene=scene, prompt=prompt, history=history)
