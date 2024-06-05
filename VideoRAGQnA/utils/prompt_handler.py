6db5f0c07d047449b72025fa83e0495aed56928d
Bashir Mohd <bashir.mohammed@intel.com>
2024-06-05 02:14:43 -0700

    Update prompt_handler.py

--- VideoRAGQnA/utils/prompt_handler.py
+++ VideoRAGQnA/utils/prompt_handler.py
@@ -1,9 +1,10 @@
-from jinja2 import Environment, BaseLoader, select_autoescape
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+from jinja2 import BaseLoader, Environment, select_autoescape
+
+PROMPT = open("utils/prompt_template.jinja2").read().strip()
 
 def get_formatted_prompt(scene, prompt, history):
-    env = Environment(
-        loader=BaseLoader(),
-        autoescape=select_autoescape(['html', 'xml'])
-    )
+    env = Environment(loader=BaseLoader(), autoescape=select_autoescape(["html", "xml"]))
     template = env.from_string(PROMPT)
-    
