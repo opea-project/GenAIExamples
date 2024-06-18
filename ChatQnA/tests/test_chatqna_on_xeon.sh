5fa523befacdadc6bc0627badcf0f89158e2ea12
Ying Chun Guo <yingchun.guo@intel.com>
2024-06-18 11:41:17 +0800

    Fix missing Dockerfiles path in GenAIComps (#303)
    Signed-off-by: Yingchun Guo <yingchun.guo@intel.com>
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>

--- ChatQnA/tests/test_chatqna_on_xeon.sh
+++ ChatQnA/tests/test_chatqna_on_xeon.sh
@@ -14,7 +14,7 @@ function build_docker_images() {
     cd GenAIComps
 
     docker build -t opea/embedding-tei:latest -f comps/embeddings/langchain/docker/Dockerfile .
-    docker build -t opea/retriever-redis:latest -f comps/retrievers/langchain/docker/Dockerfile .
+    docker build -t opea/retriever-redis:latest -f comps/retrievers/langchain/redis/docker/Dockerfile .
     docker build -t opea/reranking-tei:latest -f comps/reranks/langchain/docker/Dockerfile .
     docker build -t opea/llm-tgi:latest -f comps/llms/text-generation/tgi/Dockerfile .
     docker build -t opea/dataprep-redis:latest -f comps/dataprep/redis/langchain/docker/Dockerfile .
