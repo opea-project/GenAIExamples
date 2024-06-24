131badee40e70b5f3e7dbbbf36f37a99c20ecb58
jaswanth8888 <karani.jaswanth@gmail.com>
2024-06-24 17:20:16 +0530

    added vitest
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>

---
+++ ChatQnA/docker/ui/react/src/__tests__/util.test.ts
@@ -0,0 +1,12 @@
+import { describe, expect, test } from 'vitest';
+import { getCurrentTimeStamp, uuidv4 } from '../common/util';
+
+
+describe("unit tests", () => {
+    test('check UUID is of length 36', () => {
+        expect(uuidv4()).toHaveLength(36)
+    });
+    test('check TimeStamp generated is of unix', () => {
+        expect(getCurrentTimeStamp()).toBe(Math.floor(Date.now() / 1000));
+    });
+});
