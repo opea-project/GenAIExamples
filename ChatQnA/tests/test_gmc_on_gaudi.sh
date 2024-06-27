b8fa13d0f51c0d17bee06fe7cad43a70c583a2f0
zhlsunshine <huailong.zhang@intel.com>
2024-06-27 17:17:22 +0800

    change the yaml files addresses. Signed-off-by: zhlsunshine <huailong.zhang@intel.com> Co-authored-by: mkbhanda <malini.bhandaru@intel.com> Co-authored-by: daisy-ycguo <yingchun.guo@intel.com>

--- ChatQnA/tests/test_gmc_on_gaudi.sh
+++ ChatQnA/tests/test_gmc_on_gaudi.sh
@@ -9,10 +9,10 @@ MOUNT_DIR=/home/$USER_ID/.cache/huggingface/hub
 IMAGE_REPO=${IMAGE_REPO:-}
 
 function init_chatqna() {
-    wget https://github.com/opea-project/GenAIInfra/blob/main/microservices-connector/config/crd/bases/gmc.opea.io_gmconnectors.yaml
-    wget https://github.com/opea-project/GenAIInfra/blob/main/microservices-connector/config/rbac/gmc-manager-rbac.yaml
-    wget https://github.com/opea-project/GenAIInfra/blob/main/microservices-connector/config/manager/gmc-manager.yaml
-    wget -O manifests/gmc-router.yaml https://github.com/opea-project/GenAIInfra/blob/main/microservices-connector/config/gmcrouter/gmc-router.yaml
+    wget https://raw.githubusercontent.com/opea-project/GenAIInfra/main/microservices-connector/config/crd/bases/gmc.opea.io_gmconnectors.yaml 
+    wget https://raw.githubusercontent.com/opea-project/GenAIInfra/main/microservices-connector/config/rbac/gmc-manager-rbac.yaml 
+    wget https://raw.githubusercontent.com/opea-project/GenAIInfra/main/microservices-connector/config/manager/gmc-manager.yaml 
+    wget -O manifests/gmc-router.yaml https://raw.githubusercontent.com/opea-project/GenAIInfra/main/microservices-connector/config/gmcrouter/gmc-router.yaml
 
     # replace namespace for gmc-router and gmc-manager
     sed -i "s|namespace: system|namespace: $SYSTEM_NAMESPACE|g"  ./gmc-manager.yaml
