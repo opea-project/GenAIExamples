839408c705e782fdf1aee279646136e477b5a68a
jaswanth8888 <karani.jaswanth@gmail.com>
2024-07-04 11:52:46 +0530

    added code-gen-react-ui
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>

---
+++ CodeGen/docker/ui/react/src/config.ts
@@ -0,0 +1,6 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+export const CODE_GEN_URL = "http://10.223.24.231:7778/v1/codegen";
+// export const CODE_GEN_URL = import.meta.env.VITE_CODE_GEN_URL;
+
