61db5f628505eb5885c83961447c3b8f59d2df71
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-07-18 15:38:59 +0800

    update translate front_end UT
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

---
+++ Translation/docker/ui/svelte/tests/Translation.spec.ts
@@ -0,0 +1,28 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import { test, expect, type Page } from "@playwright/test";
+
+// Initialization before each test
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+// Constants definition
+const TRANSLATE_ITEMS = ["hello"];
+
+// Helper function: Enter message to chat
+async function enterMessageToChat(page: Page, message: string) {
+  await page.getByTestId("translate-input").click();
+  await page.getByTestId("translate-input").fill(message);
+  await page.waitForTimeout(10000);
+  await expect(page.getByTestId("translate-output")).toContainText("你好");
+}
+
+// Test description: New Doc Summary
+test.describe("New Translation", () => {
+  // Test: Enter message to summary
+  test("should enter message to translate", async ({ page }) => {
+    await enterMessageToChat(page, TRANSLATE_ITEMS[0]);
+  });
+});
