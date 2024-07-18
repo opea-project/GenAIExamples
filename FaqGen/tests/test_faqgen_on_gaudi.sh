3b87511c6d4c4f57ad24423a086c4df94c4f8147
Yogesh Pandey <yogesh.pandey@intel.com>
2024-07-18 16:36:59 +0530

    Added Gaudi Updates
    Signed-off-by: Yogesh Pandey <yogesh.pandey@intel.com>

--- FaqGen/tests/test_faqgen_on_gaudi.sh
+++ FaqGen/tests/test_faqgen_on_gaudi.sh
@@ -9,18 +9,26 @@ LOG_PATH="$WORKPATH/tests"
 ip_address=$(hostname -I | awk '{print $1}')
 
 function build_docker_images() {
-    cd $WORKPATH
-    git clone https://github.com/opea-project/GenAIComps.git
-    cd GenAIComps
+    cd $WORKPATH/../../
 
-    docker build --no-cache -t opea/llm-faqgen-tgi:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/llms/summarization/tgi/Dockerfile .
+    if [ ! -d "GenAIComps" ] ; then
+        git clone https://github.com/opea-project/GenAIComps.git
+    fi
+    cd GenAIComps
+    local CUR=$(git branch --show-current)
+    echo "Check to FAQGen component, Current Branch is ${CUR}"
+    if [ "${CUR}" != "PR318" ] ; then
+        git fetch origin pull/318/head:PR318; git checkout PR318
+    fi
+
+    docker build --no-cache -t opea/llm-faqgen-tgi:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f comps/llms/faq-generation/tgi/Dockerfile .
 
     docker pull ghcr.io/huggingface/tgi-gaudi:1.2.1
+    
+    cd $WORKPATH/../../
+    docker build --no-cache -t opea/faqgen:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f GenAIExamples/FaqGen/docker/Dockerfile .
 
-    cd $WORKPATH/docker
-    docker build --no-cache -t opea/faqgen:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
-
-    cd $WORKPATH/docker/ui
+    cd $WORKPATH/GenAIExamples/FaqGen/docker/ui
     docker build --no-cache -t opea/faqgen-ui:latest --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f docker/Dockerfile .
 
     docker images
