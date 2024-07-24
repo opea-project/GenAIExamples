f2f9470530ffa858dac038b4389413c17da0bf42
Lianhao Lu <lianhao.lu@intel.com>
2024-07-24 09:59:38 +0800

    Update Kubernetes manifest files for deploying ChatQnA (#445)
    Update Kubernetes manifest files for deploying ChatQnA without
    GMC.
    
    Signed-off-by: Lianhao Lu <lianhao.lu@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

---
+++ ChatQnA/kubernetes/manifests/README.md
@@ -0,0 +1,41 @@
+<h1 align="center" id="title">Deploy ChatQnA in Kubernetes Cluster</h1>
+
+> [NOTE]
+> The following values must be set before you can deploy:
+> HUGGINGFACEHUB_API_TOKEN
+
+> You can also customize the "MODEL_ID" if needed.
+
+> You need to make sure you have created the directory `/mnt/opea-models` to save the cached model on the node where the ChatQnA workload is running. Otherwise, you need to modify the `chatqna.yaml` file to change the `model-volume` to a directory that exists on the node.
+
+## Deploy On Xeon
+
+```
+cd GenAIExamples/ChatQnA/kubernetes/manifests/xeon
+export HUGGINGFACEHUB_API_TOKEN="YourOwnToken"
+sed -i "s/insert-your-huggingface-token-here/${HUGGINGFACEHUB_API_TOKEN}/g" chatqna.yaml
+kubectl apply -f chatqna.yaml
+```
+
+## Deploy On Gaudi
+
+```
+cd GenAIExamples/ChatQnA/kubernetes/manifests/gaudi
+export HUGGINGFACEHUB_API_TOKEN="YourOwnToken"
+sed -i "s/insert-your-huggingface-token-here/${HUGGINGFACEHUB_API_TOKEN}/g" chatqna.yaml
+kubectl apply -f chatqna.yaml
+```
+
+## Verify Services
+
+To verify the installation, run the command `kubectl get pod` to make sure all pods are running.
+
+Then run the command `kubectl port-forward svc/chatqna 8888:8888` to expose the ChatQnA service for access.
+
+Open another terminal and run the following command to verify the service if working:
+
+```console
+curl http://localhost:8888/v1/chatqna \
+    -H 'Content-Type: application/json' \
+    -d '{"messages": "What is the revenue of Nike in 2023?"}'
+```
--- ChatQnA/tests/test_manifest_on_gaudi.sh
+++ ChatQnA/tests/test_manifest_on_gaudi.sh
@@ -10,59 +10,58 @@ IMAGE_REPO=${IMAGE_REPO:-}
 IMAGE_TAG=${IMAGE_TAG:-latest}
 
 function init_chatqna() {
-    # replace the mount dir "path: /mnt" with "path: $CHART_MOUNT"
-    find . -name '*.yaml' -type f -exec sed -i "s#path: /mnt/models#path: $MOUNT_DIR#g" {} \;
+    # replace the mount dir "path: /mnt/opea-models" with "path: $CHART_MOUNT"
+    find . -name '*.yaml' -type f -exec sed -i "s#path: /mnt/opea-models#path: $MOUNT_DIR#g" {} \;
     # replace megaservice image tag
     find . -name '*.yaml' -type f -exec sed -i "s#image: opea/chatqna:latest#image: opea/chatqna:${IMAGE_TAG}#g" {} \;
     # replace the repository "image: opea/*" with "image: $IMAGE_REPO/opea/"
-    find . -name '*.yaml' -type f -exec sed -i "s#image: opea/*#image: ${IMAGE_REPO}opea/#g" {} \;
+    find . -name '*.yaml' -type f -exec sed -i "s#image: \"opea/*#image: \"${IMAGE_REPO}opea/#g" {} \;
     # set huggingface token
-    find . -name '*.yaml' -type f -exec sed -i "s#\${HUGGINGFACEHUB_API_TOKEN}#$(cat /home/$USER_ID/.cache/huggingface/token)#g" {} \;
+    find . -name '*.yaml' -type f -exec sed -i "s#insert-your-huggingface-token-here#$(cat /home/$USER_ID/.cache/huggingface/token)#g" {} \;
 }
 
 function install_chatqna {
-    # replace namespace "default" with real namespace
-    find . -name '*.yaml' -type f -exec sed -i "s#default.svc#$NAMESPACE.svc#g" {} \;
-    # for very yaml file in yaml_files, apply it to the k8s cluster
-    yaml_files=("qna_configmap_gaudi" "redis-vector-db"  "tei_embedding_gaudi_service" "tei_reranking_service" "tgi_gaudi_service" "retriever" "embedding" "reranking" "llm")
-    for yaml_file in ${yaml_files[@]}; do
-        kubectl apply -f $yaml_file.yaml -n $NAMESPACE
-    done
+    echo "namespace is $NAMESPACE"
+    kubectl apply -f . -n $NAMESPACE
+    # Sleep enough time for retreiver-usvc to be ready
     sleep 60
-    kubectl apply -f chaqna-xeon-backend-server.yaml -n $NAMESPACE
 }
 
 function validate_chatqna() {
     max_retry=20
-    # make sure microservice retriever is ready
+    # make sure microservice retriever-usvc is ready
     # try to curl retriever-svc for max_retry times
     test_embedding=$(python3 -c "import random; embedding = [random.uniform(-1, 1) for _ in range(768)]; print(embedding)")
     for ((i=1; i<=max_retry; i++))
     do
-        curl http://retriever-svc.$NAMESPACE:7000/v1/retrieval -X POST \
+        curl http://chatqna-retriever-usvc.$NAMESPACE:7000/v1/retrieval -X POST \
             -d "{\"text\":\"What is the revenue of Nike in 2023?\",\"embedding\":${test_embedding}}" \
             -H 'Content-Type: application/json' && break
-        sleep 10
+        sleep 30
     done
+    # if i is bigger than max_retry, then exit with error
+    if [ $i -gt $max_retry ]; then
+        echo "Microservice retriever failed, exit with error."
+        exit 1
+    fi
     # make sure microservice tgi-svc is ready
     for ((i=1; i<=max_retry; i++))
     do
-        curl http://tgi-gaudi-svc.$NAMESPACE:9009/generate -X POST \
+        curl http://chatqna-tgi.$NAMESPACE:80/generate -X POST \
             -d '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":17, "do_sample": true}}' \
             -H 'Content-Type: application/json' && break
         sleep 10
     done
     # if i is bigger than max_retry, then exit with error
     if [ $i -gt $max_retry ]; then
-        echo "Microservice failed, exit with error."
+        echo "Microservice tgi failed, exit with error."
         exit 1
     fi
 
     # check megaservice works
     # generate a random logfile name to avoid conflict among multiple runners
     LOGFILE=$LOG_PATH/curlmega_$NAMESPACE.log
-    curl http://chaqna-xeon-backend-server-svc.$NAMESPACE:8888/v1/chatqna -H "Content-Type: application/json" -d '{
-        "messages": "What is the revenue of Nike in 2023?"}' > $LOGFILE
+    curl http://chatqna.$NAMESPACE:8888/v1/chatqna -H "Content-Type: application/json" -d '{"messages": "What is the revenue of Nike in 2023?"}' > $LOGFILE
     exit_code=$?
     if [ $exit_code -ne 0 ]; then
         echo "Megaservice failed, please check the logs in $LOGFILE!"
@@ -90,19 +89,19 @@ fi
 
 case "$1" in
     init_ChatQnA)
-        pushd ChatQnA/kubernetes/manifests
+        pushd ChatQnA/kubernetes/manifests/gaudi
         init_chatqna
         popd
         ;;
     install_ChatQnA)
-        pushd ChatQnA/kubernetes/manifests
+        pushd ChatQnA/kubernetes/manifests/gaudi
         NAMESPACE=$2
         install_chatqna
         popd
         ;;
     validate_ChatQnA)
         NAMESPACE=$2
-        SERVICE_NAME=chaqna-xeon-backend-server-svc
+        SERVICE_NAME=chatqna
         validate_chatqna
         ;;
     *)
