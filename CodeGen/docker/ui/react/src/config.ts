6e1df710eca55fb8b6d14d551786d493e749971e
Jaswanth Karani <karani.jaswanth@gmail.com>
2024-07-24 21:31:16 +0530

    code-gen-react-ui (#417)
    * CodeGen: Manifest to deploy CodeGen with ReactUI into Kubernetes
    
    Include manifest to deploy CodeGen pipeline with ReactUI into
    Kubernetes. Include as well README file for the steps.
    
    Signed-off-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
    
    * code-gen-react-ui
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * made changes as per PR suggestions
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * updated readme
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update base image
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * updated codegn dockerfile react
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * updated wait time
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * updated as per PR comments
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * aded react ui in gaudi test file
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * fixed PR comments
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * added docker pull for text-generation-model
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    * removed unused files
    
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    
    ---------
    
    Signed-off-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
    Signed-off-by: jaswanth8888 <karani.jaswanth@gmail.com>
    Co-authored-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: chen, suyue <suyue.chen@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

---
+++ CodeGen/docker/ui/react/src/config.ts
@@ -0,0 +1,4 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+export const CODE_GEN_URL = import.meta.env.VITE_CODE_GEN_URL;
---
+++ CodeGen/docker/ui/react/src/main.tsx
@@ -0,0 +1,13 @@
+// Copyright (C) 2024 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.scss"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+      <App />
+  </React.StrictMode>
+)
