77d90048d5e31327d9f0678e1e23deba33e72b9b
Yue, Wenjiao <wenjiao.yue@intel.com>
2024-07-29 10:57:41 +0800

    Added the ChatQnA delete feature, and updated the corresponding README
    Signed-off-by: Yue, Wenjiao <wenjiao.yue@intel.com>

--- ChatQnA/docker/ui/svelte/src/lib/shared/components/doc_management/docCard.svelte
+++ ChatQnA/docker/ui/svelte/src/lib/shared/components/doc_management/docCard.svelte
@@ -3,18 +3,34 @@
   SPDX-License-Identifier: Apache-2.0
 -->
 
-<script>
+<script lang="ts">
 	import FolderIcon from "$lib/assets/DocManagement/folderIcon.svelte";
 	import LinkfolderIcon from "$lib/assets/DocManagement/LinkfolderIcon.svelte";
-	import { Modal } from "flowbite-svelte";
+	import { Button, Modal } from "flowbite-svelte";
 	import SvelteTree from "$lib/shared/components/doc_management/treeView/svelte-tree.svelte";
 	import FileIcon from "$lib/assets/DocManagement/fileIcon.svelte";
 	import { createEventDispatcher } from "svelte";
+	import DeleteIcon from "$lib/assets/upload/deleteIcon.svelte";
+	import { deleteFiles } from "$lib/network/upload/Network";
+	import { storageFiles } from "$lib/shared/stores/common/Store";
+	import { getNotificationsContext } from "svelte-notifications";
+
+	const { addNotification } = getNotificationsContext();
 
 	let dispatch = createEventDispatcher();
 	let showDirectory = false;
 	let chooseDir = undefined;
 	let currentIdx = 0;
+	let deleteModal = false;
+	/**
+	 * @type {any}
+	 */
+	let currentFile;
+	/**
+	 * @type {number}
+	 */
+	let currentFileIdx;
+
 
 	export let files = [];
 
@@ -27,7 +43,36 @@
 		console.log("chooseDir", chooseDir);
 	}
 
+	async function deleteCurrentFolder() {
 
+		const res = await deleteFiles(currentFile);
+		// succeed
+		if (res.status) {
+			$storageFiles = $storageFiles.filter((_, index) => index !== currentFileIdx);
+			files = $storageFiles;
+			showNotification("Deleted successfully", "success");
+
+		} else {
+			showNotification("Deletion failed", "success");
+
+		}
+	}
+
+	function showNotification(text: string, type: string) {
+		addNotification({
+			text: text,
+			position: "top-left",
+			type: type,
+			removeAfter: 3000,
+		});
+	}
+
+	function deleteFileIdx(file, index) {
+		currentFile = file;
+		currentFileIdx = index;
+		deleteModal = true;
+
+	}
 </script>
 
 <Modal
@@ -39,10 +84,23 @@
 >
 	<hr class="my-8 h-px border-0 bg-gray-200 dark:bg-gray-700" />
 	<SvelteTree data={chooseDir.children} {currentIdx} />
-
 </Modal>
 
-<div class="grid grid-cols-2 gap-5 max-h-[35rem]  overflow-auto">
+<Modal bind:open={deleteModal} size="xs" autoclose>
+	<div class="text-center">
+		<h3 class="mb-5 text-lg font-normal text-gray-500">Confirm file deletion?</h3>
+		<Button
+			color="red"
+			class="mr-2"
+			on:click={() => { deleteCurrentFolder() }}>Yes, I'm sure</Button
+		>
+		<Button color="alternative"
+			on:click={() => { deleteModal = false; }}
+		>No, cancel</Button>
+	</div>
+</Modal>
+
+<div class="grid max-h-[35rem] grid-cols-2 gap-5 overflow-auto mt-6">
 	{#each files as file, index}
 		<div
 			class="group relative flex w-full flex-col items-center justify-center p-2 px-12 text-center hover:bg-[#d9eeff] focus:bg-[#d9eeff]"
@@ -79,6 +137,14 @@
 					</p>
 				</button>
 			{/if}
+
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div
+				class="absolute right-0 top-0 hidden group-hover:block"
+				on:click={() => { deleteFileIdx(file.id, index)  }}
+			>
+				<DeleteIcon />
+			</div>
 		</div>
 	{/each}
 </div>
