181d7a33a58d7feba40841459c5e78df3af4b707
ZePan110 <ze.pan@intel.com>
2024-08-07 10:26:23 +0800

    Remove deprecated folder. (#536)
    Signed-off-by: zepan <ze.pan@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- DocSum/deprecated/langchain/docker/summarize-app/app/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (C) 2024 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-#
--- SearchQnA/deprecated/serving/tgi_gaudi/build_docker.sh
+++
@@ -1,9 +0,0 @@
-
-#!/bin/bash
-
-# Copyright (C) 2024 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-git clone https://github.com/huggingface/tgi-gaudi.git
-cd ./tgi-gaudi/
-docker build -t ghcr.io/huggingface/tgi-gaudi:1.2.1 . --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
