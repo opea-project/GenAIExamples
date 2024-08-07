7dd9952f5e784729c0e293567ba689e35fdaaf7d
ZePan110 <ze.pan@intel.com>
2024-08-07 10:26:23 +0800

    Remove deprecated folder. (#536)
    Signed-off-by: zepan <ze.pan@intel.com>

--- CodeTrans/deprecated/langchain/docker/build_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-# Copyright (C) 2024 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-docker build . -t intel/gen-ai-examples:code-translation --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy
