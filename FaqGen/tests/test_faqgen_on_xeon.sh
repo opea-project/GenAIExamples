7d73c88c2147a3194f49054aec967f71406aeadf
chensuyue <suyue.chen@intel.com>
2024-08-08 09:21:54 +0800

    fix container name
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- FaqGen/tests/test_faqgen_on_xeon.sh
+++ FaqGen/tests/test_faqgen_on_xeon.sh
@@ -94,7 +94,7 @@ function validate_microservices() {
         "${ip_address}:8008/generate" \
         "generated_text" \
         "tgi-service" \
-        "tgi_xeon_server" \
+        "tgi-xeon-server" \
         '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":17, "do_sample": true}}'
 
     # llm microservice
