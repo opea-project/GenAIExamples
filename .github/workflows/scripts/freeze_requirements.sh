5d9a8557f40a78488b11402de5a06d6d1984fdbf
Sun, Xuehao <xuehao.sun@intel.com>
2024-08-15 14:57:05 +0800

    Freeze requirements workflow for release tag (#485)
    Signed-off-by: Sun, Xuehao <xuehao.sun@intel.com>
    Signed-off-by: NeuralChatBot <grp_neural_chat_bot@intel.com>

---
+++ .github/workflows/scripts/freeze_requirements.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+function freeze() {
+    local file=$1
+    local folder=$(dirname "$file")
+    local keep_origin_packages="true"
+    echo "::group::Check $file ..."
+    pip-compile \
+        --no-upgrade \
+        --no-annotate \
+        --no-header \
+        --output-file "$folder/freeze.txt" \
+        "$file"
+    echo "::endgroup::"
+    if [[ -e "$folder/freeze.txt" ]]; then
+        if [[ "$keep_origin_packages" == "true" ]]; then
+            sed -i '/^\s*#/d; s/#.*//; /^\s*$/d' "$file"
+            sed -i '/^\s*#/d; s/#.*//; /^\s*$/d' "$folder/freeze.txt"
+
+            packages1=$(cut -d'=' -f1 "$file" | tr '[:upper:]' '[:lower:]' | sed 's/[-_]/-/g')
+            packages2=$(cut -d'=' -f1 "$folder/freeze.txt" | tr '[:upper:]' '[:lower:]' | sed 's/[-_]/-/g')
+            common_packages=$(comm -12 <(echo "$packages2" | sort) <(echo "$packages1" | sort))
+
+            rm "$file"
+            while IFS= read -r line; do
+                package=$(echo "$line" | cut -d'=' -f1)
+                package_transformed=$(echo "$package" | tr '[:upper:]' '[:lower:]' | sed 's/[_-]/-/g')
+                pattern=$(echo "$package_transformed" | sed 's/\[/\\\[/g; s/\]/\\\]/g')
+                if echo "$common_packages" | grep -q "^$pattern$"; then
+                    echo "$line" >>"$file"
+                fi
+            done <"$folder/freeze.txt"
+            rm "$folder/freeze.txt"
+        else
+            mv "$folder/freeze.txt" "$file"
+        fi
+    fi
+}
+
+function check_branch_name() {
+    if [[ "$GITHUB_REF_NAME" == "main" ]]; then
+        echo "$GITHUB_REF_NAME is protected branch"
+        exit 0
+    else
+        echo "branch name is $GITHUB_REF_NAME"
+    fi
+}
+
+function main() {
+    check_branch_name
+    echo "::group::pip install pip-tools" && pip install pip-tools --upgrade && echo "::endgroup::"
+    export -f freeze
+    find . -name "requirements.txt" | xargs -n 1 -I {} bash -c 'freeze "$@"' _ {}
+}
+
+main
