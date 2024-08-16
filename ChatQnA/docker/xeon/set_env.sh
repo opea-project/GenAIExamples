874fd91374ae24ce331515fd9f0286291b09976f
Letong Han <106566639+letonghan@users.noreply.github.com>
2024-08-16 18:15:33 +0800

    update port for dataprep in set_env.sh (#606)
    Signed-off-by: letonghan <letong.han@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/docker/xeon/set_env.sh
+++ ChatQnA/docker/xeon/set_env.sh
@@ -19,5 +19,5 @@ export RERANK_SERVICE_HOST_IP=${host_ip}
 export LLM_SERVICE_HOST_IP=${host_ip}
 export BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/chatqna"
 export DATAPREP_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep"
-export DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6008/v1/dataprep/get_file"
-export DATAPREP_DELETE_FILE_ENDPOINT="http://${host_ip}:6009/v1/dataprep/delete_file"
+export DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_file"
+export DATAPREP_DELETE_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/delete_file"
