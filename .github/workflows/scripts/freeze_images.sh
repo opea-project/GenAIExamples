5deb383adb1b4c9896fb65c954ad84fbfbef39bf
Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>
2024-08-20 07:09:32 -0700

    Using habana docker 1.16.1 everywhere (#528)
    Signed-off-by: Abolfazl Shahbazi <abolfazl.shahbazi@intel.com>

--- .github/workflows/scripts/freeze_images.sh
+++ .github/workflows/scripts/freeze_images.sh
@@ -6,7 +6,6 @@
 declare -A dict
 dict["langchain/langchain"]="docker://docker.io/langchain/langchain"
 dict["vault.habana.ai/gaudi-docker/1.16.1/ubuntu22.04/habanalabs/pytorch-installer-2.2.2"]="docker://vault.habana.ai/gaudi-docker/1.16.1/ubuntu22.04/habanalabs/pytorch-installer-2.2.2"
-dict["vault.habana.ai/gaudi-docker/1.16.0/ubuntu22.04/habanalabs/pytorch-installer-2.2.2"]="docker://vault.habana.ai/gaudi-docker/1.16.0/ubuntu22.04/habanalabs/pytorch-installer-2.2.2"
 
 function get_latest_version() {
     repo_image=$1
