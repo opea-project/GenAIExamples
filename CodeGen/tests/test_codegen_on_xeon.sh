6674832162f1d436c83b51397873306ccdb6a48d
Sihan Chen <39623753+Spycsh@users.noreply.github.com>
2024-08-21 22:17:07 +0800

    fix tgi xeon tag (#641)

--- CodeGen/tests/test_codegen_on_xeon.sh
+++ CodeGen/tests/test_codegen_on_xeon.sh
@@ -22,7 +22,7 @@ function build_docker_images() {
     service_list="codegen codegen-ui llm-tgi"
     docker compose -f docker_build_compose.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker pull ghcr.io/huggingface/text-generation-inference:latest-intel-cpu
+    docker pull ghcr.io/huggingface/text-generation-inference:sha-e4201f4-intel-cpu
     docker images
 }
 
