3bfd43fe472877c0bdb2b00a98f249d552a6efbd
Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>
2024-08-21 07:51:42 +0530

    ProductivitySuite: Update react keycloak realm and client
    Signed-off-by: Yeoh, Hoong Tee <hoong.tee.yeoh@intel.com>

--- ProductivitySuite/docker/ui/react/src/keycloack.ts
+++ ProductivitySuite/docker/ui/react/src/keycloack.ts
@@ -5,8 +5,8 @@ import Keycloak from "keycloak-js";
 import { KEYCLOACK_SERVICE_URL } from "./config";
 const keycloak = new Keycloak({
   url: KEYCLOACK_SERVICE_URL,
-  realm: "istio",
-  clientId: "istio",
+  realm: "productivitysuite",
+  clientId: "productivitysuite",
 });
 
 export default keycloak;
