fe2d75142d5181828c3d3b06175ec6e76ae9bac9
Ying Hu <ying.hu@intel.com>
2024-08-28 20:42:44 +0800

    Yinghu5 patch 1 (#676)
    * Update set_env.sh
    
    to fix #670
    
    * Update README.md
    
    fixed your_ip and host_ip
    
    * Update README.md
    
    fix your_ip and host_ip
    
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- DocSum/docker/set_env.sh
+++ DocSum/docker/set_env.sh
@@ -5,7 +5,7 @@
 
 
 export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
-export TGI_LLM_ENDPOINT="http://${your_ip}:8008"
+export TGI_LLM_ENDPOINT="http://${host_ip}:8008"
 export MEGA_SERVICE_HOST_IP=${host_ip}
 export LLM_SERVICE_HOST_IP=${host_ip}
 export BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/docsum"
