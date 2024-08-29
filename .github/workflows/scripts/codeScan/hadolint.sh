de33ceb6180b9998e5632d5c8f40df813e5d50df
zepan <ze.pan@intel.com>
2024-08-29 20:57:17 +0800

    test2.
    Signed-off-by: zepan <ze.pan@intel.com>

--- .github/workflows/scripts/codeScan/hadolint.sh
+++ .github/workflows/scripts/codeScan/hadolint.sh
@@ -10,13 +10,11 @@ find . -type f \( -name "Dockerfile*" \) -print -exec hadolint --ignore DL3006 -
 
 if [[ $(grep -c "error" ${log_dir}/hadolint.log) != 0 ]]; then
     $BOLD_RED && echo "Error!! Please Click on the artifact button to download and check error details." && $RESET
-    $BOLD_RED && echo $(grep "error" ${log_dir}/hadolint.log) && $RESET
     exit 1
 fi
 
 if [[ $(grep -c "warning" ${log_dir}/hadolint.log) != 0 ]]; then
     $BOLD_RED && echo "Warning!! Please Click on the artifact button to download and check warning details." && $RESET
-    $BOLD_RED && echo $(grep "warning" ${log_dir}/hadolint.log) && $RESET
     exit 1
 fi
 $BOLD_PURPLE && echo "Congratulations, Hadolint check passed!" && $LIGHT_PURPLE && echo " You can click on the artifact button to see the log details." && $RESET
