b8e6ff9ce79b187807ed17f4448b61fe74eb9158
ZePan110 <ze.pan@intel.com>
2024-08-30 11:58:57 +0800

    Modify the handling of detected warnings to only prompt. (#688)
    Signed-off-by: zepan <ze.pan@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- .github/workflows/scripts/codeScan/hadolint.sh
+++ .github/workflows/scripts/codeScan/hadolint.sh
@@ -5,16 +5,24 @@
 
 source /GenAIExamples/.github/workflows/scripts/change_color
 log_dir=/GenAIExamples/.github/workflows/scripts/codeScan
+ERROR_WARN=false
 
-find . -type f \( -name "Dockerfile*" \) -print -exec hadolint --ignore DL3006 --ignore DL3007 --ignore DL3008 --ignore DL3013 {} \; 2>&1 | tee ${log_dir}/hadolint.log
+find . -type f \( -name "Dockerfile*" \) -print -exec hadolint --ignore DL3006 --ignore DL3007 --ignore DL3008 --ignore DL3013 {} \; > ${log_dir}/hadolint.log
 
 if [[ $(grep -c "error" ${log_dir}/hadolint.log) != 0 ]]; then
     $BOLD_RED && echo "Error!! Please Click on the artifact button to download and check error details." && $RESET
-    exit 1
+    echo $(grep "error" ${log_dir}/hadolint.log)
+    ERROR_WARN=true
 fi
 
 if [[ $(grep -c "warning" ${log_dir}/hadolint.log) != 0 ]]; then
     $BOLD_RED && echo "Warning!! Please Click on the artifact button to download and check warning details." && $RESET
+    echo $(grep "warning" ${log_dir}/hadolint.log)
+    ERROR_WARN=true
+fi
+
+if [ "$ERROR_WARN" = true ]; then
+    echo $ERROR_WARN
     exit 1
 fi
 $BOLD_PURPLE && echo "Congratulations, Hadolint check passed!" && $LIGHT_PURPLE && echo " You can click on the artifact button to see the log details." && $RESET
