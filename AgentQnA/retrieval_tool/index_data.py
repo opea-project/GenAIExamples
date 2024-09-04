54b21360457c585b63a9f3c2f2fb98f2e53f3651
minmin-intel <minmin.hou@intel.com>
2024-09-04 23:05:17 +0000

    working DocIndexRetriever on xeon
    Signed-off-by: minmin-intel <minmin.hou@intel.com>

--- AgentQnA/retrieval_tool/index_data.py
+++ AgentQnA/retrieval_tool/index_data.py
@@ -28,9 +28,6 @@ def split_jsonl_into_txts(jsonl_file):
         for line in f:
             data = json.loads(line)
             docs.append(data["doc"])
-            # n+=1
-            # if n>2:
-            #     break
     return docs
 
 
