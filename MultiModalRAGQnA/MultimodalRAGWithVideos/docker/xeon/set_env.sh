7c91e8c570a824b5f094eef956ea2aedeabe04dd
Tiep Le <tiep.le@intel.com>
2024-09-06 08:48:54 +0000

    add more env var
    Signed-off-by: Tiep Le <tiep.le@intel.com>

--- MultiModalRAGQnA/MultimodalRAGWithVideos/docker/xeon/set_env.sh
+++ MultiModalRAGQnA/MultimodalRAGWithVideos/docker/xeon/set_env.sh
@@ -14,3 +14,9 @@ export LLAVA_SERVER_PORT=8399
 export LVM_ENDPOINT="http://${host_ip}:8399/v1/lvm"
 export EMBEDDING_MODEL_ID="BridgeTower/bridgetower-large-itm-mlm-itc"
 export WHISPER_MODEL="base"
+
+export DATAPREP_GEN_TRANSCRIPT_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/generate_transcripts"
+export DATAPREP_VIDEO_WITH_TRANSCRIPT_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/videos_with_transcripts"
+export DATAPREP_GEN_CAPTION_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/generate_captions"
+export DATAPREP_GET_VIDEO_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get_videos"
+export DATAPREP_DELETE_VIDEO_ENDPOINT="http://${host_ip}:6007/v1/dataprep/delete_videos"
