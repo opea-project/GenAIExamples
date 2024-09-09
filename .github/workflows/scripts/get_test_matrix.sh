429531b81073cf74a57f9574d2d5accc5b9469c5
chensuyue <suyue.chen@intel.com>
2024-09-09 22:08:20 +0800

    update deploy method for get test matrix
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .github/workflows/scripts/get_test_matrix.sh
+++ .github/workflows/scripts/get_test_matrix.sh
@@ -7,11 +7,15 @@
 
 set -e
 changed_files=$changed_files
+deploy_method=$deploy_method
 run_matrix="{\"include\":["
 hardware_list="xeon gaudi" # current support hardware list
 
 examples=$(printf '%s\n' "${changed_files[@]}" | grep '/' | cut -d'/' -f1 | sort -u)
 for example in ${examples}; do
+    cd $WORKSPACE/$example
+    if [[ ! $(find . -type f | grep ${deploy_method}) ]]; then continue; fi
+
     run_hardware=""
     if [[ $(printf '%s\n' "${changed_files[@]}" | grep ${example} | cut -d'/' -f2 | grep -E '*.py|Dockerfile*|ui|docker_image_build' ) ]]; then
         # run test on all hardware if megaservice or ui code change
@@ -29,4 +33,5 @@ for example in ${examples}; do
 done
 
 run_matrix=$run_matrix"]}"
+echo "run_matrix=${run_matrix}"
 echo "run_matrix=${run_matrix}" >> $GITHUB_OUTPUT
