6f551aa328110d6df7455266cb3ad6b815d79ac8
minmin-intel <minmin.hou@intel.com>
2024-09-10 21:47:21 +0000

    update docker compose and test script
    Signed-off-by: minmin-intel <minmin.hou@intel.com>

--- AgentQnA/tests/test_compose_on_gaudi.sh
+++ AgentQnA/tests/test_compose_on_gaudi.sh
@@ -9,6 +9,7 @@ export WORKDIR=$WORKPATH/../../
 echo "WORKDIR=${WORKDIR}"
 export ip_address=$(hostname -I | awk '{print $1}')
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
+export TOOLSET_PATH=$WORKDIR/GenAIExamples/AgentQnA/tools/
 
 function stop_crag() {
     cid=$(docker ps -aq --filter "name=kdd-cup-24-crag-service")
@@ -17,13 +18,7 @@ function stop_crag() {
 }
 
 function stop_agent_docker() {
-    cd $WORKPATH/docker_compose/intel/hpu/gaudi
-    # container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
-    # for container_name in $container_list; do
-    #     cid=$(docker ps -aq --filter "name=$container_name")
-    #     echo "Stopping container $container_name"
-    #     if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
-    # done
+    cd $WORKPATH/docker_compose/intel/hpu/gaudi/
     docker compose -f compose.yaml down
 }
 
@@ -31,42 +26,36 @@ function stop_retrieval_tool() {
     echo "Stopping Retrieval tool"
     local RETRIEVAL_TOOL_PATH=$WORKPATH/../DocIndexRetriever
     cd $RETRIEVAL_TOOL_PATH/docker_compose/intel/cpu/xeon/
-    # container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
-    # for container_name in $container_list; do
-    #     cid=$(docker ps -aq --filter "name=$container_name")
-    #     echo "Stopping container $container_name"
-    #     if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
-    # done
     docker compose -f compose.yaml down
 }
 echo "workpath: $WORKPATH"
 echo "=================== Stop containers ===================="
 stop_crag
 stop_agent_docker
-# stop_retrieval_tool
+stop_retrieval_tool
 
 cd $WORKPATH/tests
 
-# echo "=================== #1 Building docker images===================="
-# bash 1_build_images.sh
-# echo "=================== #1 Building docker images completed===================="
+echo "=================== #1 Building docker images===================="
+bash 1_build_images.sh
+echo "=================== #1 Building docker images completed===================="
 
-# echo "=================== #2 Start retrieval tool===================="
-# bash 2_start_retrieval_tool.sh
-# echo "=================== #2 Retrieval tool started===================="
+echo "=================== #2 Start retrieval tool===================="
+bash 2_start_retrieval_tool.sh
+echo "=================== #2 Retrieval tool started===================="
 
-# echo "=================== #3 Ingest data and validate retrieval===================="
-# bash 3_ingest_data_and_validate_retrieval.sh
-# echo "=================== #3 Data ingestion and validation completed===================="
+echo "=================== #3 Ingest data and validate retrieval===================="
+bash 3_ingest_data_and_validate_retrieval.sh
+echo "=================== #3 Data ingestion and validation completed===================="
 
 echo "=================== #4 Start agent and API server===================="
 bash 4_launch_and_validate_agent_tgi.sh
 echo "=================== #4 Agent test passed ===================="
 
-# echo "=================== #5 Stop agent and API server===================="
-# stop_crag
-# stop_docker
-# stop_retrieval_tool
-# echo "=================== #5 Agent and API server stopped===================="
+echo "=================== #5 Stop agent and API server===================="
+stop_crag
+stop_docker
+stop_retrieval_tool
+echo "=================== #5 Agent and API server stopped===================="
 
-# echo "ALL DONE!"
+echo "ALL DONE!"
