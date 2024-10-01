2c4b389c0ed94f12ce95380eaa78aa4752b43967
lvliang-intel <liang1.lv@intel.com>
2024-10-01 11:38:07 +0800

    fix dependency issue
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- ChatQnA/tests/test_compose_vllm_on_xeon.sh
+++ ChatQnA/tests/test_compose_vllm_on_xeon.sh
@@ -44,9 +44,9 @@ function start_services() {
     export RETRIEVER_SERVICE_HOST_IP=${ip_address}
     export RERANK_SERVER_HOST_IP=${ip_address}
     export LLM_SERVER_HOST_IP=${ip_address}
-    export EMBEDDING_SERVER_PORT=8090
+    export EMBEDDING_SERVER_PORT=6006
     export RERANK_SERVER_PORT=8808
-    export LLM_SERVER_PORT=8007
+    export LLM_SERVER_PORT=9009
     export BACKEND_SERVICE_ENDPOINT="http://${ip_address}:8888/v1/chatqna"
     export DATAPREP_SERVICE_ENDPOINT="http://${ip_address}:6007/v1/dataprep"
 
