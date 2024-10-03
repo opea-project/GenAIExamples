32fd20df47253db0f12417e935896e758c47382a
supriya-krishnamurthi <supriya.krishnamurthi@intel.com>
2024-10-03 17:03:05 +0530

    Dynamically set hostname for VITE_TEXT_TO_SQL_URL
    Signed-off-by: supriya-krishnamurthi <supriya.krishnamurthi@intel.com>

--- TextToSql/ui/react/vite.config.ts
+++ TextToSql/ui/react/vite.config.ts
@@ -3,6 +3,7 @@
 
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import os from "os";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,6 +23,8 @@ export default defineConfig({
     environment: "jsdom",
   },
   define: {
+    // Dynamically set the hostname for the VITE_TEXT_TO_SQL_URL
+    "import.meta.env.VITE_TEXT_TO_SQL_URL": JSON.stringify(`http://${os.hostname()}:9090/v1`),
     "import.meta.env": process.env,
   },
 });
