10912e41348993aa327304ddee1e600e6751d0a8
Mustafa <mustafa.cetin@intel.com>
2024-10-16 13:34:38 -0700

    update the compose file
    Signed-off-by: Mustafa <mustafa.cetin@intel.com>

--- DocSum/docker_compose/set_env.sh
+++ DocSum/docker_compose/set_env.sh
@@ -8,12 +8,21 @@ export HUGGINGFACEHUB_API_TOKEN=""
 export host_ip=$(hostname -I | awk '{print $1}')
 export ip_address=$(hostname -I | awk '{print $1}')
 
+export no_proxy="${no_proxy},${host_ip}"
+
+
+
 export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
 export TGI_LLM_ENDPOINT="http://${host_ip}:8008"
 export MEGA_SERVICE_HOST_IP=${host_ip}
 export LLM_SERVICE_HOST_IP=${host_ip}
 export BACKEND_SERVICE_ENDPOINT="http://${host_ip}:8888/v1/docsum"
 
-export ASR_ENDPOINT=http://$host_ip:7066
-export ASR_SERVICE_HOST_IP=${host_ip}
-export ASR_SERVICE_PORT=9099 # 3001
+# export ASR_ENDPOINT=http://$host_ip:7066
+# export ASR_SERVICE_HOST_IP=${host_ip}
+# export ASR_SERVICE_PORT=9099 # 3001
+
+
+export A2T_ENDPOINT=http://$host_ip:7066
+export A2T_SERVICE_HOST_IP=${host_ip}
+export A2T_SERVICE_PORT=9099 # 3001
