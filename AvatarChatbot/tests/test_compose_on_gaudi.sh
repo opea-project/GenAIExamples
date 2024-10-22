2be4759f677901677ce5d79e7367201be2ba300b
Chun Tao <chun.tao@intel.com>
2024-10-22 01:54:03 +0000

    add env vars in the beginning
    Signed-off-by: Chun Tao <chun.tao@intel.com>

--- AvatarChatbot/tests/test_compose_on_gaudi.sh
+++ AvatarChatbot/tests/test_compose_on_gaudi.sh
@@ -3,7 +3,12 @@
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
-echo "IMAGE_REPO=${IMAGE_REPO}"
+IMAGE_REPO=${IMAGE_REPO:-"opea"}
+IMAGE_TAG=${IMAGE_TAG:-"latest"}
+echo "REGISTRY=IMAGE_REPO=${IMAGE_REPO}"
+echo "TAG=IMAGE_TAG=${IMAGE_TAG}"
+export REGISTRY=${IMAGE_REPO}
+export TAG=${IMAGE_TAG}
 
 WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
