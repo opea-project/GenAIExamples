a68441c76eb832f01d64cc826eda5ee4ee91b4fd
WenjiaoYue <wenjiao.yue@intel.com>
2024-11-06 16:53:39 +0800

    Fix docSum ui error in accessing parsed files

--- DocSum/ui/svelte/src/lib/shared/Network.ts
+++ DocSum/ui/svelte/src/lib/shared/Network.ts
@@ -20,13 +20,19 @@ export async function fetchTextStream(query: string | Blob, params: string, file
   const url = `${DOC_BASE_URL}`; // Ensure the URL is constructed correctly
   const formData = new FormData();
 
-  if (params === "doc_id" && file) {
+  if (!file) {
+    file = new Blob([""], { type: "text/plain" });
+    fileName = "empty.txt";
+  }
+
+  if (params === "doc_id") {
     formData.append("files", file, fileName);
     formData.append("messages", query);
   } else if (params === "text") {
     formData.append("files", file, fileName);
     formData.append("messages", query);
   }
+
 
   // Initiate the POST request to upload the file
   const init = {
