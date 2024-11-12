24b9f03f48e584b4cc2abe39207e69c99b2ab0cc
sgurunat <gurunath.s@intel.com>
2024-11-12 12:53:31 +0530

    vLLM support for Codegen (#886)
    * Add model parameter for CodeGenGateway in gateway.py file
    
    Signed-off-by: sgurunat <gurunath.s@intel.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: sgurunat <gurunath.s@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- comps/cores/mega/gateway.py
+++ comps/cores/mega/gateway.py
@@ -274,6 +274,7 @@ class CodeGenGateway(Gateway):
             presence_penalty=chat_request.presence_penalty if chat_request.presence_penalty else 0.0,
             repetition_penalty=chat_request.repetition_penalty if chat_request.repetition_penalty else 1.2,
             streaming=stream_opt,
+            model=chat_request.model if chat_request.model else None,
         )
         result_dict, runtime_graph = await self.megaservice.schedule(
             initial_inputs={"query": prompt}, llm_parameters=parameters
