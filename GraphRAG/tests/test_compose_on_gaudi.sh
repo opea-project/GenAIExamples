67eac52e22cae3329f337d5d97993820a9edc65f
Rita Brugarolas <rita.brugarolas.brufau@intel.com>
2024-11-13 20:10:02 +0000

    fix cicd missing variable
    Signed-off-by: Rita Brugarolas <rita.brugarolas.brufau@intel.com>

--- GraphRAG/tests/test_compose_on_gaudi.sh
+++ GraphRAG/tests/test_compose_on_gaudi.sh
@@ -38,6 +38,7 @@ function start_services() {
     export HF_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
     export NEO4J_USERNAME="neo4j"
     export NEO4J_PASSWORD="neo4jtest"
+    export NEO4J_URL="bolt://${ip_address}:7687"
     export TEI_EMBEDDING_ENDPOINT="http://${ip_address}:6006"
     export TGI_LLM_ENDPOINT="http://${ip_address}:6005"
     export host_ip=${ip_address}
