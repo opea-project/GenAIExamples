dcd4348a1637089b9179b1482509522703c62423
lvliang-intel <liang1.lv@intel.com>
2024-11-14 02:45:50 +0800

    Use fixed version of TEI Gaudi for stability (#1101)
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    Co-authored-by: Malini Bhandaru <malini.bhandaru@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/tests/test_compose_on_gaudi.sh
+++ ChatQnA/tests/test_compose_on_gaudi.sh
@@ -24,7 +24,7 @@ function build_docker_images() {
 
     docker pull ghcr.io/huggingface/tgi-gaudi:2.0.6
     docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
-    docker pull ghcr.io/huggingface/tei-gaudi:latest
+    docker pull ghcr.io/huggingface/tei-gaudi:1.5.0
 
     docker images && sleep 1s
 }
