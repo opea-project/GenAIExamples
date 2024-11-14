d961b303c4be4eaad656760cf45876ae653616e8
sgurunat <gurunath.s@intel.com>
2024-11-14 08:24:11 +0000

    Modified test_compose_on_gaudi compose to use build.yaml file
    Signed-off-by: sgurunat <gurunath.s@intel.com>

--- ProductivitySuite/tests/test_compose_on_gaudi.sh
+++ ProductivitySuite/tests/test_compose_on_gaudi.sh
@@ -19,10 +19,10 @@ function build_docker_images() {
     git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    docker compose -f build_vllm.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
+    docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker pull ghcr.io/huggingface/tei-gaudi:latest
-    docker pull opea/vllm-hpu:latest
+    # docker pull ghcr.io/huggingface/tei-gaudi:latest
+    # docker pull opea/vllm-hpu:latest
     docker images && sleep 1s
 }
 
