783b1fa26d06a13a6bd0bfafba4faa8a8c24293d
Artem Astafev <a.astafev@datamonsters.com>
2024-11-14 12:55:00 +0700

    Update test_compose_on_rocm.sh
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- DocSum/tests/test_compose_on_rocm.sh
+++ DocSum/tests/test_compose_on_rocm.sh
@@ -36,7 +36,8 @@ function build_docker_images() {
     git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    docker compose -f build.yaml build --no-cache > "${LOG_PATH}"/docker_image_build.log
+    service_list="docsum docsum-ui llm-docsum-tgi"
+    docker compose -f build.yaml build ${service_list} --no-cache > "${LOG_PATH}"/docker_image_build.log
 
     docker pull ghcr.io/huggingface/text-generation-inference:2.3.1-rocm
     docker images && sleep 1s
