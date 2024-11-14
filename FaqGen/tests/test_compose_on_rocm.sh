56392b8aeb6091b011446968be1e7faa650072dd
Artem Astafev <a.astafev@datamonsters.com>
2024-11-14 12:41:47 +0700

    Update test_compose_on_rocm.sh
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- FaqGen/tests/test_compose_on_rocm.sh
+++ FaqGen/tests/test_compose_on_rocm.sh
@@ -32,8 +32,7 @@ function build_docker_images() {
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     service_list="faqgen faqgen-ui llm-faqgen-tgi"
-    docker compose -f build.yaml ${service_list} build --no-cache > "${LOG_PATH}"/docker_image_build.log
-
+    docker compose -f build.yaml build ${service_list} --no-cache > "${LOG_PATH}"/docker_image_build.log
     docker pull ghcr.io/huggingface/text-generation-inference:2.3.1-rocm
     docker images && sleep 1s
 }
