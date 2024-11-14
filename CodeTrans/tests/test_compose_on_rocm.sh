5f9a87a1b83ab9806d1483a11d49f48008acf80f
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-14 22:38:29 +0700

    Fix CodeTrans tests script
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- CodeTrans/tests/test_compose_on_rocm.sh
+++ CodeTrans/tests/test_compose_on_rocm.sh
@@ -170,7 +170,7 @@ function main() {
     validate_microservices
     sleep 10
     validate_megaservice
-#    validate_frontend
+    validate_frontend
 
     stop_docker
     echo y | docker system prune
