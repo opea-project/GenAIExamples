20f8480e91b39b08163c861183523b1de4609618
Mustafa <mustafa.cetin@intel.com>
2024-11-14 19:34:33 -0800

    update the test and readme
    Signed-off-by: Mustafa <mustafa.cetin@intel.com>

--- DocSum/tests/test_compose_on_xeon.sh
+++ DocSum/tests/test_compose_on_xeon.sh
@@ -42,9 +42,8 @@ ROOT_FOLDER=$(dirname "$(readlink -f "$0")")
 
 function build_docker_images() {
     cd $WORKPATH/docker_image_build
-    # git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
-    git clone -b docsum_gt_merge https://github.com/MSCetin37/GenAIComps.git
-
+    git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
+    
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     service_list="docsum docsum-ui whisper multimedia2text a2t v2a llm-docsum-tgi"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
@@ -228,7 +227,6 @@ function validate_megaservice() {
     fi
     sleep 1s
 }
-
 
 function validate_megaservice_json() {
     # Curl the Mega Service
