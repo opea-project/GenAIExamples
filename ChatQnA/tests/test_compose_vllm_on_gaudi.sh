c5b9e7089c8dc725e35565c04c7fcf8aa2a1ffc7
ZePan110 <ze.pan@intel.com>
2024-11-19 22:18:41 +0800

    Rename image name XXX-hpu to XXX-gaudi (#1154)
    Signed-off-by: ZePan110 <ze.pan@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/tests/test_compose_vllm_on_gaudi.sh
+++ ChatQnA/tests/test_compose_vllm_on_gaudi.sh
@@ -20,7 +20,7 @@ function build_docker_images() {
     git clone https://github.com/HabanaAI/vllm-fork.git && cd vllm-fork && git checkout 3c39626 && cd ../
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="chatqna chatqna-ui dataprep-redis retriever-redis vllm-hpu nginx"
+    service_list="chatqna chatqna-ui dataprep-redis retriever-redis vllm-gaudi nginx"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
