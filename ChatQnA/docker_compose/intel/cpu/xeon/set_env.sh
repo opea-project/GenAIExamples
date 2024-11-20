2698276bb194d5cf11c3c13eff434b109b6b73db
louie-tsai <louie.tsai@intel.com>
2024-11-20 03:33:04 +0000

    First Draft of OPEA Triage Tool
    Signed-off-by: Tsai, Louie <louie.tsai@intel.com>

--- ChatQnA/docker_compose/intel/cpu/xeon/set_env.sh
+++ ChatQnA/docker_compose/intel/cpu/xeon/set_env.sh
@@ -6,7 +6,6 @@
 pushd "../../../../../" > /dev/null
 source .set_env.sh
 popd > /dev/null
-
 export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
 export RERANK_MODEL_ID="BAAI/bge-reranker-base"
 export LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
