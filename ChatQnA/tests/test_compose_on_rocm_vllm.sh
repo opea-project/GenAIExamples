be4550c127e5adbfc6a32bd043665f6800b8a52e
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-21 13:21:04 +0700

    ChatQnA - add tests script for deploy ChatQnA app with vLLM on ROCm
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- ChatQnA/tests/test_compose_on_rocm_vllm.sh
+++ ChatQnA/tests/test_compose_on_rocm_vllm.sh
@@ -239,7 +239,7 @@ function stop_docker() {
 function main() {
 
     stop_docker
-    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+#    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_time=$(date +%s)
     start_services
     end_time=$(date +%s)
