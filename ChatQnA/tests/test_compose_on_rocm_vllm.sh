b21a75e8b5811c71d0a95826cda42ba60d544efe
Chingis Yundunov <YundunovCN@sibedge.com>
2024-11-21 14:09:19 +0700

    ChatQnA - add tests script for deploy ChatQnA app with vLLM on ROCm
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- ChatQnA/tests/test_compose_on_rocm_vllm.sh
+++ ChatQnA/tests/test_compose_on_rocm_vllm.sh
@@ -205,7 +205,7 @@ function validate_frontend() {
     echo "[ TEST INFO ]: --------- frontend test started ---------"
     cd "$WORKPATH"/ui/svelte
     local conda_env_name="OPEA_e2e"
-    export PATH=${HOME}/miniforge3/bin/:$PATH
+    export PATH=${HOME}/miniconda3/bin/:$PATH
     if conda info --envs | grep -q "$conda_env_name"; then
         echo "$conda_env_name exist!"
     else
