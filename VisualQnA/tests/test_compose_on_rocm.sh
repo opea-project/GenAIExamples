c0b1bcd02d99d7bd2562f615ae9904de521926bb
Artem Astafev <a.astafev@datamonsters.com>
2024-11-28 10:45:12 +0700

    Update test_compose_on_rocm.sh
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- VisualQnA/tests/test_compose_on_rocm.sh
+++ VisualQnA/tests/test_compose_on_rocm.sh
@@ -41,7 +41,7 @@ function build_docker_images() {
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker ghcr.io/huggingface/text-generation-inference:2.4.1-rocm
+    docker pull ghcr.io/huggingface/text-generation-inference:2.4.1-rocm
     docker images && sleep 1s
 }
 
