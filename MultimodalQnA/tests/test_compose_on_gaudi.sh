3d22cf59125b7a9f20d7ca7c6825b192276ed5f6
Omar Khleif <omar.khleif@intel.com>
2024-12-02 15:40:16 -0800

    Build, compose, and doc updates for audio query (#12)
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * validated, updated tests
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added one more curl test for audio
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * fixed typo
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * reverted git clone command
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added ASR test
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * fixed command with backslashes
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    ---------
    
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    Signed-off-by: dmsuehir <dina.s.jones@intel.com>

--- MultimodalQnA/tests/test_compose_on_gaudi.sh
+++ MultimodalQnA/tests/test_compose_on_gaudi.sh
@@ -35,6 +35,9 @@ function setup_env() {
     export EMBEDDER_PORT=6006
     export MMEI_EMBEDDING_ENDPOINT="http://${host_ip}:$EMBEDDER_PORT/v1/encode"
     export MM_EMBEDDING_PORT_MICROSERVICE=6000
+    export ASR_ENDPOINT=http://$host_ip:7066
+    export ASR_SERVICE_PORT=3001
+    export ASR_SERVICE_ENDPOINT="http://${host_ip}:${ASR_SERVICE_PORT}/v1/audio/transcriptions"
     export REDIS_URL="redis://${host_ip}:6379"
     export REDIS_HOST=${host_ip}
     export INDEX_NAME="mm-rag-redis"
@@ -238,6 +241,14 @@ function validate_megaservice() {
         "multimodalqna" \
         "multimodalqna-backend-server" \
         '{"messages": "What is the revenue of Nike in 2023?"}'
+    
+    echo "Validate megaservice with first audio query"
+    validate_service \
+        "http://${host_ip}:8888/v1/multimodalqna" \
+        '"time_of_frame_ms":' \
+        "multimodalqna" \
+        "multimodalqna-backend-server" \
+        '{"messages": [{"role": "user", "content": [{"type": "audio", "audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA"}]}]}'
 
     echo "Validate megaservice with follow-up query"
     validate_service \
@@ -245,7 +256,15 @@ function validate_megaservice() {
         '"content":"' \
         "multimodalqna" \
         "multimodalqna-backend-server" \
-        '{"messages": [{"role": "user", "content": [{"type": "text", "text": "hello, "}, {"type": "image_url", "image_url": {"url": "https://www.ilankelman.org/stopsigns/australia.jpg"}}]}, {"role": "assistant", "content": "opea project! "}, {"role": "user", "content": "chao, "}], "max_tokens": 10}'
+        '{"messages": [{"role": "user", "content": [{"type": "audio", "audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA"}, {"type": "image_url", "image_url": {"url": "https://www.ilankelman.org/stopsigns/australia.jpg"}}]}, {"role": "assistant", "content": "opea project! "}, {"role": "user", "content": [{"type": "text", "text": "goodbye"}]}]}'
+
+    echo "Validate megaservice with multiple text queries"
+    validate_service \
+        "http://${host_ip}:8888/v1/multimodalqna" \
+        '"content":"' \
+        "multimodalqna" \
+        "multimodalqna-backend-server" \
+        '{"messages": [{"role": "user", "content": [{"type": "text", "text": "hello, "}]}, {"role": "assistant", "content": "opea project! "}, {"role": "user", "content": [{"type": "text", "text": "goodbye"}]}]}'
 
 }
 
