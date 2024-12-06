8e42a3ba978b26e7fe7109e0b879f9160147f79f
Artem Astafev <a.astafev@datamonsters.com>
2024-12-06 17:32:01 +0700

    Update test_compose_on_rocm.sh
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- MultimodalQnA/tests/test_compose_on_rocm.sh
+++ MultimodalQnA/tests/test_compose_on_rocm.sh
@@ -220,8 +220,8 @@ function validate_microservices() {
     validate_service \
         "http://${host_ip}:9399/v1/lvm" \
         '"text":"' \
-        "lvm-llava-svc" \
-        "lvm-llava-svc" \
+        "lvm-tgi" \
+        "lvm-tgi" \
         '{"retrieved_docs": [], "initial_query": "What is this?", "top_n": 1, "metadata": [{"b64_img_str": "iVBORw0KGgoAAAANSUhEUgAAAAoAAAAKCAYAAACNMs+9AAAAFUlEQVR42mP8/5+hnoEIwDiqkL4KAcT9GO0U4BxoAAAAAElFTkSuQmCC", "transcript_for_inference": "yellow image", "video_id": "8c7461df-b373-4a00-8696-9a2234359fe0", "time_of_frame_ms":"37000000", "source_video":"WeAreGoingOnBullrun_8c7461df-b373-4a00-8696-9a2234359fe0.mp4"}], "chat_template":"The caption of the image is: '\''{context}'\''. {question}"}'
 
     # data prep requiring lvm
