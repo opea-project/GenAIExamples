2be4d137dbbb49f2488c91b5914e4a8e980261e0
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-17 00:23:30 +0700

    DBQnA - add files for deploy application on AMD
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- DBQnA/ui/react/src/App.test.tsx
+++ DBQnA/ui/react/src/App.test.tsx
@@ -21,7 +21,7 @@ const getHostIP = () => {
 test('testing api with dynamic host', async () => {
   // Get the dynamic host IP
   const host = await getHostIP();
-  const endpointUrl = `http://${host}:9090/v1/texttosql`;
+  const endpointUrl = `http://texttosql-service:9090/v1/texttosql`;
 
   const formData = {
     user: 'postgres',
