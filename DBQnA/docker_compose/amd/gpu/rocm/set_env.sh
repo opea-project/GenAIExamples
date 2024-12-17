b4e8dba6c60a3fcc331b6aac5bee94c338bf9535
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-17 13:15:47 +0700

    DBQnA - fix Docker Compose file for deploy application on AMD
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- DBQnA/docker_compose/amd/gpu/rocm/set_env.sh
+++ DBQnA/docker_compose/amd/gpu/rocm/set_env.sh
@@ -2,8 +2,8 @@
 
 # SPDX-License-Identifier: Apache-2.0
 
-export host_ip="192.165.1.21"
-export DBQNA_HUGGINGFACEHUB_API_TOKEN="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"
+export host_ip=""
+export DBQNA_HUGGINGFACEHUB_API_TOKEN=""
 export DBQNA_TGI_SERVICE_PORT=8008
 export DBQNA_TGI_LLM_ENDPOINT="http://${host_ip}:${DBQNA_TGI_SERVICE_PORT}"
 export DBQNA_LLM_MODEL_ID="mistralai/Mistral-7B-Instruct-v0.3"
@@ -11,5 +11,5 @@ export MODEL_ID=${DBQNA_LLM_MODEL_ID}
 export POSTGRES_USER="postgres"
 export POSTGRES_PASSWORD="testpwd"
 export POSTGRES_DB="chinook"
-export DBQNA_TEXT_TO_SQL_PORT=18142
-export DBQNA_UI_PORT=18143
+export DBQNA_TEXT_TO_SQL_PORT=9090
+export DBQNA_UI_PORT=5174
