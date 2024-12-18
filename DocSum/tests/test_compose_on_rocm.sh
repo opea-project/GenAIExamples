f1669d3a53ae558dc472a22e8b522e0b3c6b3015
Chingis Yundunov <YundunovCN@sibedge.com>
2024-12-18 14:17:31 +0700

    DocSum - fix problem of deploy DocSum on ROCm
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- DocSum/tests/test_compose_on_rocm.sh
+++ DocSum/tests/test_compose_on_rocm.sh
@@ -115,6 +115,7 @@ input_data_for_test() {
             echo "THIS IS A TEST >>>> and a number of states are starting to adopt them voluntarily special correspondent john delenco of education week reports it takes just 10 minutes to cross through gillette wyoming this small city sits in the northeast corner of the state surrounded by 100s of miles of prairie but schools here in campbell county are on the edge of something big the next generation science standards you are going to build a strand of dna and you are going to decode it and figure out what that dna actually says for christy mathis at sage valley junior high school the new standards are about learning to think like a scientist there is a lot of really good stuff in them every standard is a performance task it is not you know the child needs to memorize these things it is the student needs to be able to do some pretty intense stuff we are analyzing we are critiquing we are."
             ;;
         ("audio")
+            ls
             get_base64_str "./tests/data/test.wav"
             ;;
         ("video")
