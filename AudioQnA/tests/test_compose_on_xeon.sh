d449fd228cd8eb6b46cd2cedd1acd2beddb9a6b4
Spycsh <sihan.chen@intel.com>
2024-12-27 03:50:22 -0800

    debug

--- AudioQnA/tests/test_compose_on_xeon.sh
+++ AudioQnA/tests/test_compose_on_xeon.sh
@@ -59,7 +59,16 @@ function start_services() {
 
 
 function validate_megaservice() {
-    http_proxy="" curl http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json' | sed 's/^"//;s/"$//' | base64 -d > speech.mp3
+    response=$(http_proxy="" curl http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json')
+    #  | sed 's/^"//;s/"$//' | base64 -d > speech.mp3
+    # always print the log
+    docker logs whisper-service > $LOG_PATH/whisper-service.log
+    docker logs speecht5-service > $LOG_PATH/tts-service.log
+    docker logs tgi-service > $LOG_PATH/tgi-service.log
+    docker logs audioqna-xeon-backend-server > $LOG_PATH/audioqna-xeon-backend-server.log
+    echo "$response" | sed 's/^"//;s/"$//' | base64 -d > speech.mp3
+
+
     if [[ $(file speech.mp3) == *"RIFF"* ]]; then
         echo "Result correct."
     else
