798914ff11904e8de305cd4bbcdd689721386ccd
lvliang-intel <liang1.lv@intel.com>
2024-12-31 00:20:18 -0800

    fix ci issue
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- MultimodalQnA/multimodalqna.py
+++ MultimodalQnA/multimodalqna.py
@@ -15,7 +15,7 @@ from comps.cores.proto.api_protocol import (
     ChatMessage,
     UsageInfo,
 )
-from comps.cores.proto.docarray import LLMParams
+from comps.cores.proto.docarray import LLMParams, TextDoc, ImageDoc, TextImageDoc
 from fastapi import Request
 from fastapi.responses import JSONResponse, StreamingResponse
 from PIL import Image
@@ -28,6 +28,18 @@ MM_RETRIEVER_SERVICE_PORT = int(os.getenv("MM_RETRIEVER_SERVICE_PORT", 7000))
 LVM_SERVICE_HOST_IP = os.getenv("LVM_SERVICE_HOST_IP", "0.0.0.0")
 LVM_SERVICE_PORT = int(os.getenv("LVM_SERVICE_PORT", 9399))
 
+def align_inputs(self, inputs, cur_node, runtime_graph, llm_parameters_dict, **kwargs):
+    if self.services[cur_node].service_type == ServiceType.EMBEDDING:
+        if "text" in inputs and "image" in inputs:
+            text_doc = TextDoc(text=inputs["text"])
+            image_doc = ImageDoc(base64_image=inputs["image"])
+            inputs = TextImageDoc(text=text_doc, image=image_doc).dict()
+        elif "image" in inputs:
+            inputs = ImageDoc(base64_image=inputs["image"]).dict()
+        elif "text" in inputs:
+            inputs = TextDoc(text=inputs["text"]).dict()
+    return inputs
+
 
 class MultimodalQnAService:
     asr_port = int(os.getenv("ASR_SERVICE_PORT", 3001))
@@ -36,6 +48,7 @@ class MultimodalQnAService:
     def __init__(self, host="0.0.0.0", port=8000):
         self.host = host
         self.port = port
+        ServiceOrchestrator.align_inputs = align_inputs
         self.lvm_megaservice = ServiceOrchestrator()
         self.megaservice = ServiceOrchestrator()
         self.endpoint = str(MegaServiceEndpoint.MULTIMODAL_QNA)
