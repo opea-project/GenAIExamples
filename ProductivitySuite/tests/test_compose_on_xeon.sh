e999009c8951b27684d1f30e5afc161113b4c78f
Xinyao Wang <xinyao.wang@intel.com>
2025-01-09 19:48:11 +0800

    fix env name
    Signed-off-by: Xinyao Wang <xinyao.wang@intel.com>

--- ProductivitySuite/tests/test_compose_on_xeon.sh
+++ ProductivitySuite/tests/test_compose_on_xeon.sh
@@ -80,7 +80,7 @@ function start_services() {
     export LLM_SERVER_PORT=9009
     export PROMPT_COLLECTION_NAME="prompt"
     export host_ip=${ip_address}
-    export LLM_BACKEND="tgi"
+    export FAQGen_COMPONENT_NAME="OPEAFAQGen_TGI"
     export LOGFLAG=True
 
     # Start Docker Containers
