48d06fe274d0c537b05ef056b87a740f22a383c8
Melanie Buehler <melanie.h.buehler@intel.com>
2025-01-15 11:05:06 -0800

    Rollback accidental commit
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>

--- MultimodalQnA/tests/test_compose_on_xeon.sh
+++ MultimodalQnA/tests/test_compose_on_xeon.sh
@@ -44,7 +44,8 @@ function check_service_ready() {
 
 function build_docker_images() {
     cd $WORKPATH/docker_image_build
-    git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
+    #git clone https://github.com/opea-project/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"main"}" && cd ../
+    git clone https://github.com/mhbuehler/GenAIComps.git && cd GenAIComps && git checkout "${opea_branch:-"mmqna-image-query"}" && cd ../
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     service_list="multimodalqna multimodalqna-ui embedding-multimodal-bridgetower embedding retriever-redis lvm-llava lvm dataprep-multimodal-redis asr whisper"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
