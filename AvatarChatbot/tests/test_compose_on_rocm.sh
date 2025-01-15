79130ea350c42cf0fe72c1b4fe27bcd8cee0f856
Chingis Yundunov <YundunovCN@sibedge.com>
2025-01-15 14:14:35 +0700

    AvatarChatbot - fix tests script
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AvatarChatbot/tests/test_compose_on_rocm.sh
+++ AvatarChatbot/tests/test_compose_on_rocm.sh
@@ -141,7 +141,7 @@ function stop_docker() {
 function main() {
 
     stop_docker
-    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+#    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_services
     # validate_microservices
     validate_megaservice
