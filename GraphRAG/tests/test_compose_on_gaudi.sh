6182a3bde309ceeb97bd8547aab030736b1ccf03
Liang Lv <liang1.lv@intel.com>
2025-01-20 23:00:07 +0800

    Update test_compose_on_gaudi.sh

--- GraphRAG/tests/test_compose_on_gaudi.sh
+++ GraphRAG/tests/test_compose_on_gaudi.sh
@@ -33,7 +33,7 @@ function build_docker_images() {
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker pull ghcr.io/huggingface/tgi-gaudi:2.3.1
+    docker pull ghcr.io/huggingface/tgi-gaudi:2.0.6
     docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
     docker images && sleep 1s
 }
@@ -77,6 +77,7 @@ function validate_service() {
     if [[ $SERVICE_NAME == *"extract_graph_neo4j"* ]]; then
         cd $LOG_PATH
         HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -F 'files=@./dataprep_file.txt' -H 'Content-Type: multipart/form-data' "$URL")
+        echo $HTTP_RESPONSE
     elif [[ $SERVICE_NAME == *"neo4j-apoc"* ]]; then
          HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" "$URL")
     else
