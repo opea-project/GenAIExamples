daf1b9b798579b2ce7a62f65d471388418babffa
lvliang-intel <liang1.lv@intel.com>
2025-01-22 22:38:20 +0800

    fix image name
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>

--- MultimodalQnA/tests/test_compose_on_gaudi.sh
+++ MultimodalQnA/tests/test_compose_on_gaudi.sh
@@ -59,7 +59,7 @@ function build_docker_images() {
     git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="multimodalqna multimodalqna-ui embedding-multimodal-bridgetower embedding retriever lvm dataprep whisper"
+    service_list="multimodalqna multimodalqna-ui embedding-multimodal-bridgetower-gaudi embedding retriever lvm dataprep whisper"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker pull ghcr.io/huggingface/tgi-gaudi:2.0.6
