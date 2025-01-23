16a55941ef10a85caff299c2915e2235adc41b81
Ye, Xinyu <xinyu.ye@intel.com>
2025-01-22 20:28:05 -0500

    use compose yaml for UT.
    Signed-off-by: Ye, Xinyu <xinyu.ye@intel.com>

--- Text2Image/tests/test_compose_on_gaudi.sh
+++ Text2Image/tests/test_compose_on_gaudi.sh
@@ -14,18 +14,22 @@ LOG_PATH="$WORKPATH/tests"
 ip_address=$(hostname -I | awk '{print $1}')
 text2image_service_port=9379
 MODEL=stabilityai/stable-diffusion-2-1
+export MODEL=${MODEL}
 
 function build_docker_images() {
     cd $WORKPATH/docker_image_build
     if [ ! -d "GenAIComps" ] ; then
         git clone --depth 1 --branch ${opea_branch:-"main"} https://github.com/opea-project/GenAIComps.git
     fi
-    docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
+    service_list="text2image-gaudi text2image-ui nginx"
+    docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 }
 
 function start_service() {
+    cd $WORKPATH/docker_compose/intel/hpu/gaudi
     export no_proxy="localhost,127.0.0.1,"${ip_address}
-    docker run -d --name="text2image-server" -p $text2image_service_port:$text2image_service_port --runtime=habana -e HABANA_VISIBLE_DEVICES=all -e OMPI_MCA_btl_vader_single_copy_mechanism=none --cap-add=sys_nice --ipc=host -e http_proxy=$http_proxy -e https_proxy=$https_proxy -e MODEL=$MODEL -e no_proxy=$no_proxy ${IMAGE_REPO}/text2image-gaudi:${IMAGE_TAG}
+    # Start Docker Containers
+    docker compose -f compose.yaml up -d > ${LOG_PATH}/start_services_with_compose.log
     sleep 30s
 }
 
@@ -38,11 +42,11 @@ function validate_microservice() {
     HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -X POST -d '{"prompt":"An astronaut riding a green horse", "num_images_per_prompt":1}' -H 'Content-Type: application/json' "$URL")
     HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
     RESPONSE_BODY=$(echo $HTTP_RESPONSE | sed -e 's/HTTPSTATUS\:.*//g')
-    SERVICE_NAME="text2image-server - generate image"
+    SERVICE_NAME="text2image-gaudi-server - generate image"
 
     if [ "$HTTP_STATUS" -ne "200" ]; then
         echo "[ $SERVICE_NAME ] HTTP status is not 200. Received status was $HTTP_STATUS"
-        docker logs text2image-server >> ${LOG_PATH}/text2image-server_generate_image.log
+        docker logs text2image-gaudi-server >> ${LOG_PATH}/text2image-gaudi-server_generate_image.log
         exit 1
     else
         echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
@@ -52,14 +56,14 @@ function validate_microservice() {
         echo "Content correct."
     else
         echo "Content wrong."
-        docker logs text2image-server >> ${LOG_PATH}/text2image-server_generate_image.log
+        docker logs text2image-gaudi-server >> ${LOG_PATH}/text2image-gaudi-server_generate_image.log
         exit 1
     fi
 }
 
 function stop_docker() {
-    cid=$(docker ps -aq --filter "name=text2image-server*")
-    if [[ ! -z "$cid" ]]; then docker stop $cid && docker rm $cid && sleep 1s; fi
+    cd $WORKPATH/docker_compose/intel/hpu/gaudi
+    docker compose -f compose.yaml down
 }
 
 function main() {
