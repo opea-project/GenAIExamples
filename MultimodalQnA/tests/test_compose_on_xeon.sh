8254c7d748b2d86722183748dc34fed56e30fa15
Melanie Hart Buehler <melanie.h.buehler@intel.com>
2025-01-22 17:11:09 -0800

    Fixes MultimodalQnA dataprep endpoint and port in the UI (#1457)
    Signed-off-by: Melanie Buehler <melanie.h.buehler@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- MultimodalQnA/tests/test_compose_on_xeon.sh
+++ MultimodalQnA/tests/test_compose_on_xeon.sh
@@ -79,7 +79,7 @@ function setup_env() {
     export REDIS_URL="redis://${host_ip}:${REDIS_DB_PORT}"
     export REDIS_HOST=${host_ip}
     export INDEX_NAME="mm-rag-redis"
-    export DATAPREP_MMR_PORT=5000
+    export DATAPREP_MMR_PORT=6007
     export DATAPREP_INGEST_SERVICE_ENDPOINT="http://${host_ip}:${DATAPREP_MMR_PORT}/v1/dataprep/ingest"
     export DATAPREP_GEN_TRANSCRIPT_SERVICE_ENDPOINT="http://${host_ip}:${DATAPREP_MMR_PORT}/v1/dataprep/generate_transcripts"
     export DATAPREP_GEN_CAPTION_SERVICE_ENDPOINT="http://${host_ip}:${DATAPREP_MMR_PORT}/v1/dataprep/generate_captions"
@@ -206,11 +206,6 @@ function validate_microservices() {
         '{"text": {"text" : "This is some sample text."}, "image" : {"url": "https://github.com/docarray/docarray/blob/main/tests/toydata/image-data/apple.png?raw=true"}}'
 
     sleep 1m # retrieval can't curl as expected, try to wait for more time
-
-    export DATAPREP_INGEST_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/ingest"
-    export DATAPREP_GEN_TRANSCRIPT_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/generate_transcripts"
-    export DATAPREP_GEN_CAPTION_SERVICE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/generate_captions"
-    export DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6007/v1/dataprep/get"
 
     # test data prep
     echo "Validating Data Prep with Generating Transcript for Video"
