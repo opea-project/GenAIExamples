a6bc6e8294148b965fd4d2846aec5323910aa53e
okhleif-IL <omar.khleif@intel.com>
2025-01-31 13:51:07 -0800

    nltk bug fix
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>

--- DocSum/ui/gradio/docsum_ui_gradio.py
+++ DocSum/ui/gradio/docsum_ui_gradio.py
@@ -312,6 +312,7 @@ app = gr.mount_gradio_app(app, demo, path="/")
 
 if __name__ == "__main__":
     import argparse
+    import nltk
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="0.0.0.0")
@@ -319,5 +320,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     logger.info(">>> Starting server at %s:%d", args.host, args.port)
-
+    nltk.download('punkt_tab')
+    nltk.download('averaged_perceptron_tagger_eng')
     uvicorn.run(app, host=args.host, port=args.port)
