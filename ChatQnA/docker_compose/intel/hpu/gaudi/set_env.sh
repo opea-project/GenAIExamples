958580465ee5938b0c42893c5007096b2a353f7b
Liang Lv <liang1.lv@intel.com>
2025-02-05 08:30:04 +0800

    Add support for latest deepseek models on Gaudi (#1491)
    Signed-off-by: lvliang-intel <liang1.lv@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/docker_compose/intel/hpu/gaudi/set_env.sh
+++ ChatQnA/docker_compose/intel/hpu/gaudi/set_env.sh
@@ -11,6 +11,7 @@ export EMBEDDING_MODEL_ID="BAAI/bge-base-en-v1.5"
 export RERANK_MODEL_ID="BAAI/bge-reranker-base"
 export LLM_MODEL_ID="meta-llama/Meta-Llama-3-8B-Instruct"
 export INDEX_NAME="rag-redis"
+export NUM_CARDS=1
 # Set it as a non-null string, such as true, if you want to enable logging facility,
 # otherwise, keep it as "" to disable it.
 export LOGFLAG=""
