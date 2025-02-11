817cf3550bb3dca0a9d208376fd3f03391d5e3ea
Chingis Yundunov <YundunovCN@sibedge.com>
2025-02-11 11:46:36 +0700

    ChatQnA - fix deploy app with vLLM ROCm
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- ChatQnA/tests/test_compose_on_rocm_vllm.sh
+++ ChatQnA/tests/test_compose_on_rocm_vllm.sh
@@ -205,7 +205,7 @@ function stop_docker() {
 function main() {
 
     stop_docker
-#    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_time=$(date +%s)
     start_services
     end_time=$(date +%s)
