1cdd6bab2e3dcba727ddf9a2bce65a36c580e236
Chingis Yundunov <YundunovCN@sibedge.com>
2025-02-19 21:22:19 +0700

    FaqGen - fix files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

---
+++ FaqGen/docker_compose/amd/gpu/rocm/set_env_vllm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Copyright (C) 2024 Advanced Micro Devices, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+export HOST_IP=''
+export EXTERNAL_HOST_IP=''
+export FAQGEN_LLM_MODEL_ID="meta-llama/Meta-Llama-3-8B-Instruct"
+export FAQGEN_VLLM_SERVICE_PORT=8883
+export FAQGEN_LLM_SERVER_PORT=9001
+export FAGGEN_LLM_BACKEND="http://${HOST_IP}:${FAQGEN_VLLM_SERVICE_PORT}"
+export FAQGEN_HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
+export FAQGEN_BACKEND_SERVER_PORT=18074
+export FAQGEN_BACKEND_ENDPOINT="http://${EXTERNAL_HOST_IP}:${FAQGEN_BACKEND_SERVER_PORT}/v1/faqgen"
+export FAGGEN_UI_PORT=18075
