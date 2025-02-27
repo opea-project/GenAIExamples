19d7d34dd4d63e214bd8c00d271838109e151efe
rbrugaro <rita.brugarolas.brufau@intel.com>
2025-02-27 05:10:22 +0000

    test script fixes
    Signed-off-by: rbrugaro <rita.brugarolas.brufau@intel.com>

--- GraphRAG/tests/test_compose_on_gaudi.sh
+++ GraphRAG/tests/test_compose_on_gaudi.sh
@@ -12,7 +12,7 @@ export TAG=${IMAGE_TAG}
 
 WORKPATH=$(dirname "$PWD")
 LOG_PATH="$WORKPATH/tests"
-host_ip=$(hostname -I | awk '{print $1}')
+export host_ip=$(hostname -I | awk '{print $1}')
 
 function build_docker_images() {
     opea_branch=${opea_branch:-"main"}
@@ -140,10 +140,18 @@ function validate_microservices() {
         "${host_ip}:${TEI_EMBEDDER_PORT}/embed" \
         "[[" \
         "tei-embedding-service" \
-        "tei-embedding-server" \
+        "tei-embedding-serving" \
         '{"inputs":"What is Deep Learning?"}'
 
     sleep 1m # retrieval can't curl as expected, try to wait for more time
+
+    # tgi for llm service
+    validate_service \
+        "${host_ip}:${LLM_ENDPOINT_PORT}/generate" \
+        "generated_text" \
+        "tgi-gaudi-service" \
+        "tgi-gaudi-server" \
+        '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":17, "do_sample": true}}'
 
     # test /v1/dataprep/ingest graph extraction
     echo "Like many companies in the O&G sector, the stock of Chevron (NYSE:CVX) has declined about 10% over the past 90-days despite the fact that Q2 consensus earnings estimates have risen sharply (~25%) during that same time frame. Over the years, Chevron has kept a very strong balance sheet. FirstEnergy (NYSE:FE – Get Rating) posted its earnings results on Tuesday. The utilities provider reported $0.53 earnings per share for the quarter, topping the consensus estimate of $0.52 by $0.01, RTT News reports. FirstEnergy had a net margin of 10.85% and a return on equity of 17.17%. The Dáil was almost suspended on Thursday afternoon after Sinn Féin TD John Brady walked across the chamber and placed an on-call pager in front of the Minister for Housing Darragh O’Brien during a debate on retained firefighters. Mr O’Brien said Mr Brady had taken part in an act of theatre that was obviously choreographed.Around 2,000 retained firefighters around the country staged a second day of industrial action on Tuesday and are due to start all out-strike action from next Tuesday. The mostly part-time workers, who keep the services going outside of Ireland’s larger urban centres, are taking industrial action in a dispute over pay and working conditions. Speaking in the Dáil, Sinn Féin deputy leader Pearse Doherty said firefighters had marched on Leinster House today and were very angry at the fact the Government will not intervene. Reintroduction of tax relief on mortgages needs to be considered, O’Brien says. Martin withdraws comment after saying People Before Profit would ‘put the jackboot on people’ Taoiseach ‘propagated fears’ farmers forced to rewet land due to nature restoration law – Cairns An intervention is required now. I’m asking you to make an improved offer in relation to pay for retained firefighters, Mr Doherty told the housing minister.I’m also asking you, and challenging you, to go outside after this Order of Business and meet with the firefighters because they are just fed up to the hilt in relation to what you said.Some of them have handed in their pagers to members of the Opposition and have challenged you to wear the pager for the next number of weeks, put up with an €8,600 retainer and not leave your community for the two and a half kilometres and see how you can stand over those type of pay and conditions. At this point, Mr Brady got up from his seat, walked across the chamber and placed the pager on the desk in front of Mr O’Brien. Ceann Comhairle Seán Ó Fearghaíl said the Sinn Féin TD was completely out of order and told him not to carry out a charade in this House, adding it was absolutely outrageous behaviour and not to be encouraged.Mr O’Brien said Mr Brady had engaged in an act of theatre here today which was obviously choreographed and was then interrupted with shouts from the Opposition benches. Mr Ó Fearghaíl said he would suspend the House if this racket continues.Mr O’Brien later said he said he was confident the dispute could be resolved and he had immense regard for firefighters. The minister said he would encourage the unions to re-engage with the State’s industrial relations process while also accusing Sinn Féin of using the issue for their own political gain." > $LOG_PATH/dataprep_file.txt
@@ -151,7 +159,7 @@ function validate_microservices() {
         "http://${host_ip}:${DATAPREP_PORT}/v1/dataprep/ingest" \
         "Data preparation succeeded" \
         "extract_graph_neo4j" \
-        "dataprep-neo4j-server"
+        "dataprep-neo4j-llamaindex"
 
     sleep 2m
 
@@ -163,15 +171,7 @@ function validate_microservices() {
         "retriever_neo4j" \
         "{\"messages\": [{\"role\": \"user\",\"content\": \"Who is John Brady and has he had any confrontations?\"}]}"
 
-
-    # tgi for llm service
-    validate_service \
-        "${host_ip}:${LLM_ENDPOINT_PORT}/generate" \
-        "generated_text" \
-        "tgi-gaudi-service" \
-        "tgi-gaudi-server" \
-        '{"inputs":"What is Deep Learning?","parameters":{"max_new_tokens":17, "do_sample": true}}'
-}
+    }
 
 function validate_megaservice() {
     # Curl the Mega Service
