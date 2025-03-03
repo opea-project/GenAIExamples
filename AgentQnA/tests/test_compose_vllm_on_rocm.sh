3f51c7bacc1e0234fb6956f29e90b7ffc498049b
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-03 20:45:48 +0700

    AgentQnA - add files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/tests/test_compose_vllm_on_rocm.sh
+++ AgentQnA/tests/test_compose_vllm_on_rocm.sh
@@ -2,15 +2,14 @@
 # Copyright (C) 2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-set -e
+set -xe
 
 WORKPATH=$(dirname "$PWD")
-export LOG_PATH=${WORKPATH}
-export WORKDIR=${WORKPATH}/../../
+export WORKDIR=$WORKPATH/../../
 echo "WORKDIR=${WORKDIR}"
 export ip_address=$(hostname -I | awk '{print $1}')
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
-export TOOLSET_PATH=$WORKPATH/tools/
+export TOOLSET_PATH=$WORKDIR/GenAIExamples/AgentQnA/tools/
 
 function stop_crag() {
     cid=$(docker ps -aq --filter "name=kdd-cup-24-crag-service")
@@ -20,16 +19,27 @@ function stop_crag() {
 
 function stop_agent_docker() {
     cd $WORKPATH/docker_compose/amd/gpu/rocm
-    docker compose -f compose_vllm.yaml down
+    # docker compose -f compose.yaml down
+    container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
+    for container_name in $container_list; do
+        cid=$(docker ps -aq --filter "name=$container_name")
+        echo "Stopping container $container_name"
+        if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
+    done
 }
 
 function stop_retrieval_tool() {
     echo "Stopping Retrieval tool"
-    local RETRIEVAL_TOOL_PATH=$WORKDIR/GenAIExamples/DocIndexRetriever
+    local RETRIEVAL_TOOL_PATH=$WORKPATH/../DocIndexRetriever
     cd $RETRIEVAL_TOOL_PATH/docker_compose/intel/cpu/xeon/
-    docker compose -f compose.yaml down
+    # docker compose -f compose.yaml down
+    container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
+    for container_name in $container_list; do
+        cid=$(docker ps -aq --filter "name=$container_name")
+        echo "Stopping container $container_name"
+        if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
+    done
 }
-
 echo "workpath: $WORKPATH"
 echo "=================== Stop containers ===================="
 stop_crag
@@ -39,19 +49,19 @@ stop_retrieval_tool
 cd $WORKPATH/tests
 
 echo "=================== #1 Building docker images===================="
-bash step1_build_images_rocm_vllm.sh
+bash step1_build_images.sh
 echo "=================== #1 Building docker images completed===================="
 
 echo "=================== #2 Start retrieval tool===================="
-bash step2_start_retrieval_tool_rocm_vllm.sh
+bash step2_start_retrieval_tool.sh
 echo "=================== #2 Retrieval tool started===================="
 
 echo "=================== #3 Ingest data and validate retrieval===================="
-bash step3_ingest_data_and_validate_retrieval_rocm_vllm.sh
+bash step3_ingest_data_and_validate_retrieval.sh
 echo "=================== #3 Data ingestion and validation completed===================="
 
 echo "=================== #4 Start agent and API server===================="
-bash step4_launch_and_validate_agent_rocm_vllm.sh
+bash step4a_launch_and_validate_agent_tgi_on_rocm.sh
 echo "=================== #4 Agent test passed ===================="
 
 echo "=================== #5 Stop agent and API server===================="
