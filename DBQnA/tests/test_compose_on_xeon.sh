11c51ed56a2a1b5b4e728210d535edec813a5254
Ying Hu <ying.hu@intel.com>
2025-03-03 16:17:19 +0800

    Update DBQnA tgi docker image to latest tgi 2.4.0 (#1593)
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- DBQnA/tests/test_compose_on_xeon.sh
+++ DBQnA/tests/test_compose_on_xeon.sh
@@ -22,6 +22,9 @@ function build_docker_images() {
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
     docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
+
+    docker pull ghcr.io/huggingface/text-generation-inference:2.4.0-intel-cpu
+    docker images && sleep 1s
 }
 
 function start_service() {
