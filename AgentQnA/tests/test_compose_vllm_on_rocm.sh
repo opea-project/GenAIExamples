b9d48a82960734d86bb8d11c3803e9f271b27953
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-04 23:43:14 +0700

    AgentQnA - add files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/tests/test_compose_vllm_on_rocm.sh
+++ AgentQnA/tests/test_compose_vllm_on_rocm.sh
@@ -32,34 +32,34 @@ function stop_retrieval_tool() {
 
 echo "workpath: $WORKPATH"
 echo "=================== Stop containers ===================="
-#stop_crag
-#stop_agent_docker
-#stop_retrieval_tool
+stop_crag
+stop_agent_docker
+stop_retrieval_tool
 
 cd $WORKPATH/tests
 
-#echo "=================== #1 Building docker images===================="
-#bash step1_build_images_rocm_vllm.sh
-#echo "=================== #1 Building docker images completed===================="
+echo "=================== #1 Building docker images===================="
+bash step1_build_images_rocm_vllm.sh
+echo "=================== #1 Building docker images completed===================="
 
-#echo "=================== #2 Start retrieval tool===================="
-#bash step2_start_retrieval_tool_rocm_vllm.sh
-#echo "=================== #2 Retrieval tool started===================="
+echo "=================== #2 Start retrieval tool===================="
+bash step2_start_retrieval_tool_rocm_vllm.sh
+echo "=================== #2 Retrieval tool started===================="
 
-#echo "=================== #3 Ingest data and validate retrieval===================="
-#bash step3_ingest_data_and_validate_retrieval_rocm_vllm.sh
-#echo "=================== #3 Data ingestion and validation completed===================="
+echo "=================== #3 Ingest data and validate retrieval===================="
+bash step3_ingest_data_and_validate_retrieval_rocm_vllm.sh
+echo "=================== #3 Data ingestion and validation completed===================="
 
 echo "=================== #4 Start agent and API server===================="
 bash step4_launch_and_validate_agent_rocm_vllm.sh
 echo "=================== #4 Agent test passed ===================="
 
 echo "=================== #5 Stop agent and API server===================="
-#stop_crag
-#stop_agent_docker
-#stop_retrieval_tool
+stop_crag
+stop_agent_docker
+stop_retrieval_tool
 echo "=================== #5 Agent and API server stopped===================="
 
-#echo y | docker system prune
+echo y | docker system prune
 
 echo "ALL DONE!!"
