2532dba521701a163e97ef54f5eb56a42ef16088
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-04 22:13:33 +0700

    AgentQnA - add files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/docker_compose/amd/gpu/rocm/launch_agent_service_vllm_rocm.sh
+++ AgentQnA/docker_compose/amd/gpu/rocm/launch_agent_service_vllm_rocm.sh
@@ -7,20 +7,23 @@
 # export LANGCHAIN_API_KEY="your_langchain_api_key"
 # export LANGCHAIN_TRACING_V2=""
 
-
+# Set server hostname or IP address
 export ip_address=${host_ip}
+
+# Set services IP ports
+export VLLM_SERVICE_PORT="18110"
+export WORKER_RAG_AGENT_PORT="18111"
+export WORKER_SQL_AGENT_PORT="18112"
+export SUPERVISOR_REACT_AGENT_PORT="18113"
+export CRAG_SERVER_PORT="18114"
 
 export WORKPATH=$(dirname "$PWD")
 export WORKDIR=${WORKPATH}/../../../
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
-export VLLM_SERVICE_PORT="8081"
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
 export VLLM_LLM_MODEL_ID="Intel/neural-chat-7b-v3-3"
-
 export HF_CACHE_DIR="./data"
-
 export TOOLSET_PATH=${WORKPATH}/../../../tools/
-export WORKER_RAG_AGENT_PORT="9095"
 export recursion_limit_worker=12
 export LLM_ENDPOINT_URL=http://${ip_address}:${VLLM_SERVICE_PORT}
 export LLM_MODEL_ID=${VLLM_LLM_MODEL_ID}
@@ -29,20 +32,13 @@ export max_new_tokens=512
 export RETRIEVAL_TOOL_URL="http://${ip_address}:8889/v1/retrievaltool"
 export LANGCHAIN_API_KEY=${LANGCHAIN_API_KEY}
 export LANGCHAIN_TRACING_V2=${LANGCHAIN_TRACING_V2}
-
-export WORKER_SQL_AGENT_PORT="9096"
 export db_name=Chinook
 export db_path="sqlite:////home/user/chinook-db/Chinook_Sqlite.sqlite"
 export recursion_limit_worker=12
-
-export SUPERVISOR_REACT_AGENT_PORT="9090"
 export recursion_limit_supervisor=10
-export CRAG_SERVER_PORT="18881"
 export CRAG_SERVER=http://${ip_address}:${CRAG_SERVER_PORT}
 export WORKER_AGENT_URL="http://${ip_address}:${WORKER_RAG_AGENT_PORT}/v1/chat/completions"
 export SQL_AGENT_URL="http://${ip_address}:${WORKER_SQL_AGENT_PORT}/v1/chat/completions"
-
-host_ip=$(hostname -I | awk '{print $1}')
 export HF_CACHE_DIR=${HF_CACHE_DIR}
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
 export no_proxy=${no_proxy}
@@ -65,5 +61,4 @@ export DATAPREP_GET_FILE_ENDPOINT="http://${host_ip}:6008/v1/dataprep/get"
 export DATAPREP_DELETE_FILE_ENDPOINT="http://${host_ip}:6009/v1/dataprep/delete"
 
 docker compose -f ../../../../../DocIndexRetriever/docker_compose/intel/cpu/xeon/compose.yaml up -d
-
 docker compose -f compose_vllm.yaml up -d
