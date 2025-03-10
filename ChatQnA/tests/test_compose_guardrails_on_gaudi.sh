43d0a182708501b159dbc56b378ba4c68490889d
chen, suyue <suyue.chen@intel.com>
2025-03-10 17:36:26 +0800

    Enhance ChatQnA test scripts (#1643)
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- ChatQnA/tests/test_compose_guardrails_on_gaudi.sh
+++ ChatQnA/tests/test_compose_guardrails_on_gaudi.sh
@@ -162,7 +162,7 @@ function validate_megaservice() {
     # Curl the Mega Service
     validate_service \
         "${ip_address}:8888/v1/chatqna" \
-        "data: " \
+        "Nike" \
         "mega-chatqna" \
         "chatqna-gaudi-guardrails-server" \
         '{"messages": "What is the revenue of Nike in 2023?"}'
