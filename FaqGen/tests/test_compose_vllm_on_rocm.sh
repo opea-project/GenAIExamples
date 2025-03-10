f9affe1227e316732a7f9eea70bc0c25e3148565
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-10 15:11:01 +0700

    FaqGen - add files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- FaqGen/tests/test_compose_vllm_on_rocm.sh
+++ FaqGen/tests/test_compose_vllm_on_rocm.sh
@@ -176,17 +176,17 @@ function stop_docker() {
 
 function main() {
 
-#    stop_docker
+    stop_docker
 
-#    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_services
 
-#    validate_microservices
-#    validate_megaservice
+    validate_microservices
+    validate_megaservice
     validate_frontend
 
-#    stop_docker
-#    echo y | docker system prune
+    stop_docker
+    echo y | docker system prune
 
 }
 
