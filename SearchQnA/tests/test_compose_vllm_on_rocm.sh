39dc7fd2b78cd1df61f71aeb3d22c8c1ea9927e9
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-11 18:06:18 +0700

    SearchQnA - add files for deploy with ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- SearchQnA/tests/test_compose_vllm_on_rocm.sh
+++ SearchQnA/tests/test_compose_vllm_on_rocm.sh
@@ -64,7 +64,7 @@ function start_services() {
     docker compose -f compose_vllm.yaml up -d > ${LOG_PATH}/start_services_with_compose.log
     n=0
     until [[ "$n" -ge 100 ]]; do
-        docker logs search-vllm-service > $LOG_PATH/search-vllm-service_start.log
+        docker logs search-vllm-service >& $LOG_PATH/search-vllm-service_start.log
         if grep -q "Application startup complete" $LOG_PATH/search-vllm-service_start.log; then
             break
         fi
