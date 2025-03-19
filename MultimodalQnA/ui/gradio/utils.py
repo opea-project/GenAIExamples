fdeaf212696ba776277698c3179a6cb85ea1d2ba
Harsha Ramayanam <harsha.ramayanam@intel.com>
2025-03-19 15:07:35 -0700

    Adds audio response toggle button to UI (#67)
    * first commit for tts addition
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added TTS linkage to backend
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * removed unused import
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added necessary env vars
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * reworked temp tts toggle logic
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added modalities as a toggle
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * removed print statement
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * removed gaudi from tts
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * doc updates and code refactor
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * added tts test to megaservice tests
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * remove log diles
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * addressed recent review comments
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    
    * Added Logic for audio responses & refactored code to align with new gradio version
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Minr bug fixes and UI changes
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * UI layout update & handling empty text with spaces
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Updates on review comments
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Update on review comments
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Update MultimodalQnA/ui/gradio/multimodalqna_ui_gradio.py
    
    Co-authored-by: Dina Suehiro Jones <dina.s.jones@intel.com>
    
    * Some updates to review comments. More to come after testing
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Restrict file media types to known/working formats
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Remove extra whitespace
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Fix test_compose_on_gaudi.sh script's diff not syncing with phase3
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Changes per review comments
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Added single space to the pload
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Added logic to flush chatbot assistant's voice reponse .wav
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Fixed issue where assistant's image is not sent
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Revert build yaml
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Clear diff
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * changes per review
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * small change
    
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    
    * Update Dockerfile
    
    Revert Dockerfile
    
    ---------
    
    Signed-off-by: okhleif-IL <omar.khleif@intel.com>
    Signed-off-by: Harsha Ramayanam <harsha.ramayanam@intel.com>
    Co-authored-by: okhleif-IL <omar.khleif@intel.com>
    Co-authored-by: Dina Suehiro Jones <dina.s.jones@intel.com>

--- MultimodalQnA/ui/gradio/utils.py
+++ MultimodalQnA/ui/gradio/utils.py
@@ -7,15 +7,20 @@ import logging.handlers
 import os
 import shutil
 import sys
+import tempfile
 from pathlib import Path
 
 import cv2
 from moviepy.video.io.VideoFileClip import VideoFileClip
 
 LOGDIR = "."
+TMP_DIR = "/tmp"
 
 server_error_msg = "**NETWORK ERROR DUE TO HIGH TRAFFIC. PLEASE REGENERATE OR REFRESH THIS PAGE.**"
 moderation_msg = "YOUR INPUT VIOLATES OUR CONTENT MODERATION GUIDELINES. PLEASE TRY AGAIN."
+
+GRADIO_IMAGE_FORMATS = [".jpeg", ".png", ".jpg", ".gif"]
+GRADIO_AUDIO_FORMATS = [".wav", ".mp3",]
 
 handler = None
 save_log = False
@@ -186,3 +191,15 @@ def convert_audio_to_base64(audio_path):
     """Convert .wav file to base64 string."""
     encoded_string = base64.b64encode(open(audio_path, "rb").read())
     return encoded_string.decode("utf-8")
+
+def convert_base64_to_audio(b64_str):
+    """Decodes the base64 encoded audio data and returns a saved filepath."""
+    
+    audio_data = base64.b64decode(b64_str)
+
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(dir=TMP_DIR, delete=False, suffix=".wav") as temp_audio:
+        temp_audio.write(audio_data)
+        temp_audio_path = temp_audio.name  # Store the path
+
+    return temp_audio_path
