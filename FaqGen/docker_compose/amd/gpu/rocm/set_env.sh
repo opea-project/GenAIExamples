dba5eb723b8faa2c4c7679772e5c8caeee7767ff
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-19 18:51:13 +0700

    FaqGen - fix files for deploy on ROCm
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- FaqGen/docker_compose/amd/gpu/rocm/set_env.sh
+++ FaqGen/docker_compose/amd/gpu/rocm/set_env.sh
@@ -3,13 +3,12 @@
 # Copyright (C) 2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+export HOST_IP=''
+export EXTERNAL_HOST_IP=''
 export FAQGEN_LLM_MODEL_ID="meta-llama/Meta-Llama-3-8B-Instruct"
-export FAQGEN_TGI_SERVICE_IMAGE="ghcr.io/huggingface/text-generation-inference:2.3.1-rocm"
-export HOST_IP=${host_ip}
-export FAQGEN_CARD_ID="card0"
-export FAQGEN_RENDER_ID="renderD128"
 export FAQGEN_TGI_SERVICE_PORT=8883
 export FAQGEN_LLM_SERVER_PORT=9001
-export FAQGEN_HUGGINGFACEHUB_API_TOKEN=${your_hf_api_token}
-export FAQGEN_BACKEND_SERVER_PORT=8881
-export FAGGEN_UI_PORT=5174
+export FAQGEN_HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
+export FAQGEN_BACKEND_SERVER_PORT=18074
+export FAQGEN_BACKEND_ENDPOINT="http://${EXTERNAL_HOST_IP}:${FAQGEN_BACKEND_SERVER_PORT}/v1/faqgen"
+export FAGGEN_UI_PORT=18075
