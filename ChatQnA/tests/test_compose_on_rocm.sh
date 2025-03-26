147a48da5e9530f81290ada689f376fae09b4cba
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-26 15:57:18 +0700

    ChatQnA - fix files for deploy on ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- ChatQnA/tests/test_compose_on_rocm.sh
+++ ChatQnA/tests/test_compose_on_rocm.sh
@@ -151,7 +151,7 @@ function validate_microservices() {
         "chatqna-tei-embedding-service" \
         '{"inputs":"What is Deep Learning?"}'
 
-    sleep 1m # retrieval can't curl as expected, try to wait for more time
+    sleep 2m # retrieval can't curl as expected, try to wait for more time
 
     # test /v1/dataprep/ingest upload file
     echo "Deep learning is a subset of machine learning that utilizes neural networks with multiple layers to analyze various levels of abstract data representations. It enables computers to identify patterns and make decisions with minimal human intervention by learning from large amounts of data." > "$LOG_PATH"/dataprep_file.txt
