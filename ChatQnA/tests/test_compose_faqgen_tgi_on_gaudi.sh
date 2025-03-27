af46b54ae398db586e32b936b1d6f7286df9f1c1
xiguiw <111278656+xiguiw@users.noreply.github.com>
2025-03-27 09:40:22 +0800

    Update TEI docker image to 1.6 (#1650)
    Signed-off-by: Wang, Xigui <xigui.wang@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- ChatQnA/tests/test_compose_faqgen_tgi_on_gaudi.sh
+++ ChatQnA/tests/test_compose_faqgen_tgi_on_gaudi.sh
@@ -35,7 +35,7 @@ function build_docker_images() {
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker pull ghcr.io/huggingface/tgi-gaudi:2.0.6
-    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
+    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.6
     docker pull ghcr.io/huggingface/tei-gaudi:1.5.0
     docker images && sleep 1s
 }
--- GraphRAG/tests/test_compose_on_gaudi.sh
+++ GraphRAG/tests/test_compose_on_gaudi.sh
@@ -35,7 +35,7 @@ function build_docker_images() {
     docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker pull ghcr.io/huggingface/tgi-gaudi:2.3.1
-    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
+    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.6
     docker images && sleep 1s
 }
 
