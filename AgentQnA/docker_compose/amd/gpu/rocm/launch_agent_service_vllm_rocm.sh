b73f5dfc4ba2b5a94200df8c738dbf92c2631ec1
Chingis Yundunov <YundunovCN@sibedge.com>
2025-03-27 11:28:45 +0700

    AgentQnA - fix files for deploy on ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/docker_compose/amd/gpu/rocm/launch_agent_service_vllm_rocm.sh
+++ AgentQnA/docker_compose/amd/gpu/rocm/launch_agent_service_vllm_rocm.sh
@@ -68,7 +68,7 @@ echo ${CRAG_SERVER_PORT} > ${WORKPATH}/CRAG_SERVER_PORT_tmp
 
 echo "Downloading chinook data..."
 git clone https://github.com/lerocha/chinook-database.git
-cp chinook-database/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite ~/agentqna-test/GenAIExamples/AgentQnA/tests
+cp chinook-database/ChinookDatabase/DataSources/Chinook_Sqlite.sqlite ~/agentqna-install/GenAIExamples/AgentQnA/tests
 
 docker compose -f ../../../../../DocIndexRetriever/docker_compose/intel/cpu/xeon/compose.yaml up -d
 docker compose -f compose_vllm.yaml up -d
