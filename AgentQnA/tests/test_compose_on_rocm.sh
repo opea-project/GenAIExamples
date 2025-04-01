b0e66e3836cb6db2f02b4c35d52669b028c1d126
Chingis Yundunov <YundunovCN@sibedge.com>
2025-04-01 13:04:25 +0700

    AgentQnA - fix files for deploy on ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/tests/test_compose_on_rocm.sh
+++ AgentQnA/tests/test_compose_on_rocm.sh
@@ -9,7 +9,7 @@ export WORKDIR=$WORKPATH/../../
 echo "WORKDIR=${WORKDIR}"
 export ip_address=$(hostname -I | awk '{print $1}')
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
-export TOOLSET_PATH=$WORKPATH/AgentQnA/tools/
+export TOOLSET_PATH=$WORKPATH/../AgentQnA/tools/
 export MODEL_CACHE="./data"
 
 function stop_crag() {
