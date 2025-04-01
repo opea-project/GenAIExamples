6045d5781b8b0d578eb5532caa80520151484eb7
Chingis Yundunov <YundunovCN@sibedge.com>
2025-04-01 20:27:33 +0700

    AgentQnA - fix files for deploy on ROCm vLLM
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/tests/test_compose_on_rocm.sh
+++ AgentQnA/tests/test_compose_on_rocm.sh
@@ -21,13 +21,7 @@ function stop_crag() {
 
 function stop_agent_docker() {
     cd $WORKPATH/docker_compose/amd/gpu/rocm
-    # docker compose -f compose.yaml down
-    container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
-    for container_name in $container_list; do
-        cid=$(docker ps -aq --filter "name=$container_name")
-        echo "Stopping container $container_name"
-        if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
-    done
+    bash stop_agent_service_tgi_rocm.sh
 }
 
 function stop_retrieval_tool() {
