5cc047ce348bc2e96c9e66b5784504c9c645859f
chyundunovDatamonsters <c.yundunov@datamonsters.com>
2025-04-02 10:17:07 +0700

    Adding files to deploy AgentQnA application on ROCm vLLM (#1613)
    Signed-off-by: Chingis Yundunov <YundunovCN@sibedge.com>

--- AgentQnA/tests/test_compose_vllm_on_rocm.sh
+++ AgentQnA/tests/test_compose_vllm_on_rocm.sh
@@ -2,11 +2,11 @@
 # Copyright (C) 2024 Advanced Micro Devices, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-set -xe
+set -e
 
 WORKPATH=$(dirname "$PWD")
-ls $WORKPATH
-export WORKDIR=$WORKPATH/../../
+export LOG_PATH=${WORKPATH}
+export WORKDIR=${WORKPATH}/../../
 echo "WORKDIR=${WORKDIR}"
 export ip_address=$(hostname -I | awk '{print $1}')
 export HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
@@ -21,21 +21,16 @@ function stop_crag() {
 
 function stop_agent_docker() {
     cd $WORKPATH/docker_compose/amd/gpu/rocm
-    bash stop_agent_service_tgi_rocm.sh
+    bash stop_agent_service_vllm_rocm.sh
 }
 
 function stop_retrieval_tool() {
     echo "Stopping Retrieval tool"
-    local RETRIEVAL_TOOL_PATH=$WORKPATH/../DocIndexRetriever
+    local RETRIEVAL_TOOL_PATH=$WORKDIR/GenAIExamples/DocIndexRetriever
     cd $RETRIEVAL_TOOL_PATH/docker_compose/intel/cpu/xeon/
-    # docker compose -f compose.yaml down
-    container_list=$(cat compose.yaml | grep container_name | cut -d':' -f2)
-    for container_name in $container_list; do
-        cid=$(docker ps -aq --filter "name=$container_name")
-        echo "Stopping container $container_name"
-        if [[ ! -z "$cid" ]]; then docker rm $cid -f && sleep 1s; fi
-    done
+    docker compose -f compose.yaml down
 }
+
 echo "workpath: $WORKPATH"
 echo "=================== Stop containers ===================="
 stop_crag
@@ -45,19 +40,19 @@ stop_retrieval_tool
 cd $WORKPATH/tests
 
 echo "=================== #1 Building docker images===================="
-bash step1_build_images.sh
+bash step1_build_images_rocm_vllm.sh
 echo "=================== #1 Building docker images completed===================="
 
 echo "=================== #2 Start retrieval tool===================="
-bash step2_start_retrieval_tool.sh
+bash step2_start_retrieval_tool_rocm_vllm.sh
 echo "=================== #2 Retrieval tool started===================="
 
 echo "=================== #3 Ingest data and validate retrieval===================="
-bash step3_ingest_data_and_validate_retrieval.sh
+bash step3_ingest_data_and_validate_retrieval_rocm_vllm.sh
 echo "=================== #3 Data ingestion and validation completed===================="
 
 echo "=================== #4 Start agent and API server===================="
-bash step4a_launch_and_validate_agent_tgi_on_rocm.sh
+bash step4_launch_and_validate_agent_rocm_vllm.sh
 echo "=================== #4 Agent test passed ===================="
 
 echo "=================== #5 Stop agent and API server===================="
