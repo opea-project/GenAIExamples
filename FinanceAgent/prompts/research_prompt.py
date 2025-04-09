4a905517b7a03ad5740e9793a1cdf0d06cc1a75d
lkk12014402 <kaokao.lv@intel.com>
2025-04-09 14:11:55 +0000

    update research agent tools and prompt.

--- FinanceAgent/prompts/research_prompt.py
+++ FinanceAgent/prompts/research_prompt.py
@@ -3,21 +3,52 @@
 
 REACT_AGENT_LLAMA_PROMPT = """\
 
-You are a Market Analyst, one must possess strong analytical and problem-solving abilities, collect necessary financial information and aggregate them based on client's requirement. For coding tasks, only use the functions you have been provided with. Reply TERMINATE when the task is done.
+Role: Expert Investor
+Department: Finance
+Primary Responsibility: Generation of Customized Financial Analysis Reports
+
+Role Description:
+As an Expert Investor within the finance domain, your expertise is harnessed to develop bespoke Financial Analysis Reports that cater to specific client requirements. This role demands a deep dive into financial statements and market data to unearth insights regarding a company's financial performance and stability. Engaging directly with clients to gather essential information and continuously refining the report with their feedback ensures the final product precisely meets their needs and expectations.
+
+Key Objectives:
+
+Analytical Precision: Employ meticulous analytical prowess to interpret financial data, identifying underlying trends and anomalies.
+Effective Communication: Simplify and effectively convey complex financial narratives, making them accessible and actionable to non-specialist audiences.
+Client Focus: Dynamically tailor reports in response to client feedback, ensuring the final analysis aligns with their strategic objectives.
+Adherence to Excellence: Maintain the highest standards of quality and integrity in report generation, following established benchmarks for analytical rigor.
+Performance Indicators: The efficacy of the Financial Analysis Report is measured by its utility in providing clear, actionable insights. This encompasses aiding corporate decision-making, pinpointing areas for operational enhancement, and offering a lucid evaluation of the company's financial health. Success is ultimately reflected in the report's contribution to informed investment decisions and strategic planning.
+
+Reply TERMINATE when everything is settled.
 
 
 You have access to the following tools:
 {tools}
 
-For a comprehensive analysis, you can use all the tools provided to retrieve information available for the company.
+For writing a comprehensive analysis financial research report, you can use all the tools provided to retrieve information available for the company.
 
-**Procedure:**
-1. Read the question carefully. You need get accurate `start_date` and `end_date` value first because most tools need the 2 values like company news, financials. You can get `end_date` with `get_current_date` tool if user doesn't provide. And you can infer `start_date` with `end_date` using the rule `start_date is one year earlier than end_date` if user doesn't provide.
-2. Analyze the positive developments and potential concerns of the company with 2-4 most important factors respectively and keep them concise. Most factors should be inferred from company related news.
-3. Make a rough prediction (e.g. up/down by 2-3%) of the company stock price movement for next week.
-4. Provide a summary analysis to support your prediction.
-5. Read the execution history if any to understand the tools that have been called and the information that has been gathered.
-6. Reason about the information gathered so far and decide if you can answer the question or if you need to call more tools.
+**Procedures:**
+1. Explicitly explain your working plan before you kick off.
+2. Read the question carefully. Firstly You need get accurate `start_date` and `end_date` value, because most tools need the 2 values like company news, financials. You can get `end_date` with `get_current_date` tool if user doesn't provide. And you can infer `start_date` with `end_date` using the rule `start_date is one year earlier than end_date` if user doesn't provide.
+3. Analyze Key Financial Statements:
+    - **Balance Sheet Analysis**: Use the `analyze_balance_sheet` tool to obtain insights into the company's financial stability and liquidity.
+    - **Income Statement Analysis**: Employ the `analyze_income_stmt` tool to assess the company's profitability over the fiscal year.
+    - **Cash Flow Statement Analysis**: Apply the `analyze_cash_flow` tool to evaluate the cash inflows and outflows, providing a clear view of the company's operational efficiency.
+4. Perform Additional Analyses:
+    - **Business Highlights**: Use the `get_company_news` tool to summarize the company's operational achievements and strategic milestones over the year.
+    - **Company Description**: Utilize the `get_company_profile` tool to provide a detailed overview of the company's business model, industry positioning, and strategic initiatives.
+    - **Risk Assessment**: Employ all the tool output financial information to identify and summarize the top three risks facing the company.
+5. Share Performance: analysis the stock performance using the `get_share_performance` tool.
+6. Generate Report: Finally, compile all the analyzed data, insights, and visuals into a comprehensive financial report, which contains the following paragraphs:
+    - income summarization
+    - market position
+    - business overview
+    - risk assessment
+    - competitors analysis
+    - share performance analysis
+7. All the paragraphs should combine between 400 and 450 words.
+8. Read the execution history if any to understand the tools that have been called and the information that has been gathered.
+9. Reason about the information gathered so far and decide if you can answer the question or if you need to call more tools.
+10. Do not repeat your thinking process!!! Only gather all information when you generate report.
 
 **Output format:**
 You should output your thought process. Finish thinking first. Output tool calls or your answer at the end.
@@ -25,8 +56,8 @@ When making tool calls, you should use the following format:
 TOOL CALL: {{"tool": "tool1", "args": {{"arg1": "value1", "arg2": "value2", ...}}}}
 TOOL CALL: {{"tool": "tool2", "args": {{"arg1": "value1", "arg2": "value2", ...}}}}
 
-If you can answer the question, provide the answer in the following format:
-FINAL ANSWER: {{"answer": "your answer here"}}
+If you can generate the financial, provide the report in the following format:
+FINAL ANSWER: {{"answer": "your financial research report here"}}
 
 Follow these guidelines when formulating your answer:
 1. If the question contains a false premise or assumption, answer “invalid question”.
@@ -34,6 +65,7 @@ Follow these guidelines when formulating your answer:
 3. Give concise, factual and relevant answers.
 
 **IMPORTANT:**
+* Do not generate history messages repeatly.
 * Divide the question into sub-questions and conquer sub-questions one by one.
 * Questions may be time sensitive. Pay attention to the time when the question was asked.
 * You may need to combine information from multiple tools to answer the question.
--- FinanceAgent/tools/research_tools.py
+++ FinanceAgent/tools/research_tools.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2025 Intel Corporation
+# Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
 import json
@@ -6,7 +6,8 @@ import os
 import random
 from collections import defaultdict
 from datetime import date, datetime, timedelta
-from typing import Annotated
+from typing import Annotated, Any, List, Optional
+from textwrap import dedent
 
 import pandas as pd
 
@@ -23,6 +24,164 @@ try:
 
 except:
     pass
+
+
+# https://github.com/langchain-ai/langchain/blob/master/libs/community/langchain_community/utilities/financial_datasets.py
+"""
+Util that calls several of financial datasets stock market REST APIs.
+Docs: https://docs.financialdatasets.ai/
+"""
+
+import requests
+from pydantic import BaseModel
+
+
+FINANCIAL_DATASETS_BASE_URL = "https://api.financialdatasets.ai/"
+
+class FinancialDatasetsAPIWrapper(BaseModel):
+    """Wrapper for financial datasets API."""
+
+    financial_datasets_api_key: Optional[str] = None
+
+    def __init__(self, **data: Any):
+        super().__init__(**data)
+        self.financial_datasets_api_key = data["api_key"]
+
+    @property
+    def _api_key(self) -> str:
+        if self.financial_datasets_api_key is None:
+            raise ValueError(
+                "API key is required for the FinancialDatasetsAPIWrapper. "
+                "Please provide the API key by either:\n"
+                "1. Manually specifying it when initializing the wrapper: "
+                "FinancialDatasetsAPIWrapper(financial_datasets_api_key='your_api_key')\n"
+                "2. Setting it as an environment variable: FINANCIAL_DATASETS_API_KEY"
+            )
+        return self.financial_datasets_api_key
+
+    def get_income_statements(
+        self,
+        ticker: str,
+        period: str,
+        limit: Optional[int],
+    ) -> Optional[dict]:
+        """
+        Get the income statements for a stock `ticker` over a `period` of time.
+
+        :param ticker: the stock ticker
+        :param period: the period of time to get the balance sheets for.
+            Possible values are: annual, quarterly, ttm.
+        :param limit: the number of results to return, default is 10
+        :return: a list of income statements
+        """
+        url = (
+            f"{FINANCIAL_DATASETS_BASE_URL}financials/income-statements/"
+            f"?ticker={ticker}"
+            f"&period={period}"
+            f"&limit={limit if limit else 10}"
+        )
+
+        # Add the api key to the headers
+        headers = {"X-API-KEY": self._api_key}
+
+        # Execute the request
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        return data.get("income_statements", None)
+
+    def get_balance_sheets(
+        self,
+        ticker: str,
+        period: str,
+        limit: Optional[int],
+    ) -> List[dict]:
+        """
+        Get the balance sheets for a stock `ticker` over a `period` of time.
+
+        :param ticker: the stock ticker
+        :param period: the period of time to get the balance sheets for.
+            Possible values are: annual, quarterly, ttm.
+        :param limit: the number of results to return, default is 10
+        :return: a list of balance sheets
+        """
+        url = (
+            f"{FINANCIAL_DATASETS_BASE_URL}financials/balance-sheets/"
+            f"?ticker={ticker}"
+            f"&period={period}"
+            f"&limit={limit if limit else 10}"
+        )
+
+        # Add the api key to the headers
+        headers = {"X-API-KEY": self._api_key}
+
+        # Execute the request
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        return data.get("balance_sheets", None)
+
+    def get_cash_flow_statements(
+        self,
+        ticker: str,
+        period: str,
+        limit: Optional[int],
+    ) -> List[dict]:
+        """
+        Get the cash flow statements for a stock `ticker` over a `period` of time.
+
+        :param ticker: the stock ticker
+        :param period: the period of time to get the balance sheets for.
+            Possible values are: annual, quarterly, ttm.
+        :param limit: the number of results to return, default is 10
+        :return: a list of cash flow statements
+        """
+
+        url = (
+            f"{FINANCIAL_DATASETS_BASE_URL}financials/cash-flow-statements/"
+            f"?ticker={ticker}"
+            f"&period={period}"
+            f"&limit={limit if limit else 10}"
+        )
+
+        # Add the api key to the headers
+        headers = {"X-API-KEY": self._api_key}
+
+        # Execute the request
+        response = requests.get(url, headers=headers)
+        data = response.json()
+
+        return data.get("cash_flow_statements", None)
+
+    def run(self, mode: str, ticker: str, **kwargs: Any) -> str:
+        if mode == "get_income_statements":
+            period = kwargs.get("period", "annual")
+            limit = kwargs.get("limit", 10)
+            return json.dumps(self.get_income_statements(ticker, period, limit))
+        elif mode == "get_balance_sheets":
+            period = kwargs.get("period", "annual")
+            limit = kwargs.get("limit", 10)
+            return json.dumps(self.get_balance_sheets(ticker, period, limit))
+        elif mode == "get_cash_flow_statements":
+            period = kwargs.get("period", "annual")
+            limit = kwargs.get("limit", 10)
+            return json.dumps(self.get_cash_flow_statements(ticker, period, limit))
+        else:
+            raise ValueError(f"Invalid mode {mode} for financial datasets API.")
+
+
+financial_datasets_client = None
+
+try:
+    if os.environ.get("FINANCIAL_DATASETS_API_KEY") is None:
+        print("Please set the environment variable FINANCIAL_DATASETS_API_KEY to use the financialdatasets.ai data.")
+    else:
+        financial_datasets_client = FinancialDatasetsAPIWrapper(api_key=os.environ["FINANCIAL_DATASETS_API_KEY"])
+        print("FINANCIAL DATASETS client initialized")
+
+except Exception as e:
+    print(str(e))
+
 
 
 def get_company_profile(symbol: Annotated[str, "ticker symbol"]) -> str:
@@ -55,7 +214,7 @@ def get_company_news(
         "end date of the search period for the company's basic financials, yyyy-mm-dd",
     ],
     max_news_num: Annotated[int, "maximum number of news to return, default to 10"] = 10,
-) -> str:
+):
     """Retrieve market news related to designated company."""
     news = finnhub_client.company_news(symbol, _from=start_date, to=end_date)
     if len(news) == 0:
@@ -147,8 +306,160 @@ def get_current_date():
     return date.today().strftime("%Y-%m-%d")
 
 
-if __name__ == "__main__":
+def combine_prompt(instruction, resource):
+    prompt = f"Resource: {resource}\n\nInstruction: {instruction}"
+    return prompt
 
-    print(get_company_profile("AAPL"))
-    # print(FinnHubUtils.get_basic_financials_history("AAPL", "annual", "2019-01-01", "2021-01-01"))
-    # print(FinnHubUtils.get_basic_financials("AAPL"))
+def analyze_balance_sheet(
+    symbol: Annotated[str, "ticker symbol"],
+    period: Annotated[str, "the period of time to get the balance sheets for. Possible values are: annual, quarterly, ttm."],
+    limit: int = 10
+) -> str:
+    """
+    Retrieve the balance sheet for the given ticker symbol with the related section of its 10-K report.
+    Then return with an instruction on how to analyze the balance sheet.
+    """
+
+    balance_sheet = financial_datasets_client.run(
+            mode="get_balance_sheets",
+            ticker=symbol,
+            period=period,
+            limit=limit,
+    )
+
+    df_string = "Balance sheet:\n" + balance_sheet
+
+    instruction = dedent(
+        """
+        Delve into a detailed scrutiny of the company's balance sheet for the most recent fiscal year, pinpointing
+        the structure of assets, liabilities, and shareholders' equity to decode the firm's financial stability and
+        operational efficiency. Focus on evaluating the liquidity through current assets versus current liabilities,
+        the solvency via long-term debt ratios, and the equity position to gauge long-term investment potential.
+        Contrast these metrics with previous years' data to highlight financial trends, improvements, or deteriorations.
+        Finalize with a strategic assessment of the company's financial leverage, asset management, and capital structure,
+        providing insights into its fiscal health and future prospects in a single paragraph. Less than 130 words.
+        """
+    )
+
+    prompt = combine_prompt(instruction, df_string)
+    return prompt
+
+
+def analyze_income_stmt(
+    symbol: Annotated[str, "ticker symbol"],
+    period: Annotated[str, "the period of time to get the balance sheets for. Possible values are: annual, quarterly, ttm."],
+    limit: int = 10
+) -> str:
+    """
+    Retrieve the income statement for the given ticker symbol with the related section of its 10-K report.
+    Then return with an instruction on how to analyze the income statement.
+    """
+    # Retrieve the income statement
+    income_stmt = financial_datasets_client.run(
+            mode="get_income_statements",
+            ticker=symbol,
+            period=period,
+            limit=limit,
+    )
+    df_string = "Income statement:\n" + income_stmt
+
+    # Analysis instruction
+    instruction = dedent(
+        """
+        Conduct a comprehensive analysis of the company's income statement for the current fiscal year.
+        Start with an overall revenue record, including Year-over-Year or Quarter-over-Quarter comparisons,
+        and break down revenue sources to identify primary contributors and trends. Examine the Cost of
+        Goods Sold for potential cost control issues. Review profit margins such as gross, operating,
+        and net profit margins to evaluate cost efficiency, operational effectiveness, and overall profitability.
+        Analyze Earnings Per Share to understand investor perspectives. Compare these metrics with historical
+        data and industry or competitor benchmarks to identify growth patterns, profitability trends, and
+        operational challenges. The output should be a strategic overview of the company’s financial health
+        in a single paragraph, less than 130 words, summarizing the previous analysis into 4-5 key points under
+        respective subheadings with specific discussion and strong data support.
+        """
+    )
+
+
+    # Combine the instruction, section text, and income statement
+    prompt = combine_prompt(instruction, df_string)
+
+    return prompt
+
+def analyze_cash_flow(
+    symbol: Annotated[str, "ticker symbol"],
+    period: Annotated[str, "the period of time to get the balance sheets for. Possible values are: annual, quarterly, ttm."],
+    limit: int = 10
+) -> str:
+    """
+    Retrieve the cash flow statement for the given ticker symbol with the related section of its 10-K report.
+    Then return with an instruction on how to analyze the cash flow statement.
+    """
+
+    cash_flow = financial_datasets_client.run(
+        mode="get_cash_flow_statements",
+        ticker=symbol,
+        period=period,
+        limit=limit,
+    )
+
+    df_string = "Cash flow statement:\n" + cash_flow
+
+    instruction = dedent(
+        """
+        Dive into a comprehensive evaluation of the company's cash flow for the latest fiscal year, focusing on cash inflows
+        and outflows across operating, investing, and financing activities. Examine the operational cash flow to assess the
+        core business profitability, scrutinize investing activities for insights into capital expenditures and investments,
+        and review financing activities to understand debt, equity movements, and dividend policies. Compare these cash movements
+        to prior periods to discern trends, sustainability, and liquidity risks. Conclude with an informed analysis of the company's
+        cash management effectiveness, liquidity position, and potential for future growth or financial challenges in a single paragraph.
+        Less than 130 words.
+        """
+    )
+
+    prompt = combine_prompt(instruction, df_string)
+    return prompt
+
+def get_share_performance(
+    symbol: Annotated[str, "Ticker symbol of the stock (e.g., 'AAPL' for Apple)"],
+    end_date: Annotated[
+        str,
+        "end date of the search period for the company's basic financials, yyyy-mm-dd",
+    ],
+    ) -> str:
+    """Plot the stock performance of a company compared to the S&P 500 over the past year."""
+    filing_date = datetime.strptime(end_date, "%Y-%m-%d")
+
+    start = (filing_date - timedelta(days=60)).strftime("%Y-%m-%d")
+    end = filing_date.strftime("%Y-%m-%d")
+    interval = 'day'         # possible values are {'second', 'minute', 'day', 'week', 'month', 'year'}
+    interval_multiplier = 1     # every 1 day
+
+    # create the URL
+    url = (
+        f'https://api.financialdatasets.ai/prices/'
+        f'?ticker={symbol}'
+        f'&interval={interval}'
+        f'&interval_multiplier={interval_multiplier}'
+        f'&start_date={start}'
+        f'&end_date={end}'
+    )
+
+    headers = {"X-API-KEY": "your_api_key_here"}
+
+    response = requests.get(url, headers=headers)
+    # parse prices from the response
+
+    prices = response.json().get('prices')
+
+    df_string = "Past 60 days Stock prices:\n" + json.dumps(prices)
+
+    instruction = dedent(
+        """
+        Dive into a comprehensive evaluation of the company's stock price for the latest 60 days.
+        Less than 130 words.
+        """
+    )
+
+    prompt = combine_prompt(instruction, df_string)
+    return prompt
+
