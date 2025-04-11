8e5ea2803451d63a93278b0ceefe601819dd29cf
minmin-intel <minmin.hou@intel.com>
2025-04-11 13:33:19 -0700

    update AgentQnA (#1790)
    Signed-off-by: minmin-intel <minmin.hou@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- AgentQnA/retrieval_tool/run_ingest_data.sh
+++ AgentQnA/retrieval_tool/run_ingest_data.sh
@@ -1,7 +1,9 @@
 # Copyright (C) 2024 Intel Corporation
 # SPDX-License-Identifier: Apache-2.0
 
+host_ip=$(hostname -I | awk '{print $1}')
+port=6007
 FILEDIR=${WORKDIR}/GenAIExamples/AgentQnA/example_data/
 FILENAME=test_docs_music.jsonl
 
-python3 index_data.py --filedir ${FILEDIR} --filename ${FILENAME} --host_ip $host_ip
+python3 index_data.py --filedir ${FILEDIR} --filename ${FILENAME} --host_ip $host_ip --port $port
