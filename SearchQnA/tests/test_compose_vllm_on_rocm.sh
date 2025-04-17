e408ad3fc8caf19ddcb0a0ca26f5c5514bc28e3f
Artem Astafev <a.astafev@datamonsters.com>
2025-04-17 17:38:08 +0700

    Update test_compose_vllm_on_rocm.sh
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- SearchQnA/tests/test_compose_vllm_on_rocm.sh
+++ SearchQnA/tests/test_compose_vllm_on_rocm.sh
@@ -23,7 +23,7 @@ function build_docker_images() {
     service_list="searchqna searchqna-ui embedding web-retriever reranking llm-textgen vllm-rocm"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.5
+    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.6
     docker images && sleep 3s
 }
 
