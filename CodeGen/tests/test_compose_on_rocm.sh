9da1f4f186cfe321bff92e62e9194ae3285bbce4
Artem Astafev <a.astafev@datamonsters.com>
2025-04-18 18:19:27 +0700

    update compose example
    Signed-off-by: Artem Astafev <a.astafev@datamonsters.com>

--- CodeGen/tests/test_compose_on_rocm.sh
+++ CodeGen/tests/test_compose_on_rocm.sh
@@ -195,8 +195,6 @@ function main() {
     if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
     start_services
 
-    sleep 5m
-
     validate_microservices
     validate_megaservice
     validate_frontend
