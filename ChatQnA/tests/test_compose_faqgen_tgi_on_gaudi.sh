8e8809cf2c1d81404a7c14db5aaeb11e31bd44e0
chensuyue <suyue.chen@intel.com>
2025-04-25 21:50:54 +0800

    downgrade ghcr.io/huggingface/text-embeddings-inference version to fix the perf regression.
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- ChatQnA/tests/test_compose_faqgen_tgi_on_gaudi.sh
+++ ChatQnA/tests/test_compose_faqgen_tgi_on_gaudi.sh
@@ -28,9 +28,6 @@ function build_docker_images() {
     service_list="chatqna chatqna-ui dataprep retriever llm-faqgen nginx"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
-    docker pull ghcr.io/huggingface/tgi-gaudi:2.0.6
-    docker pull ghcr.io/huggingface/text-embeddings-inference:cpu-1.6
-    docker pull ghcr.io/huggingface/tei-gaudi:1.5.0
     docker images && sleep 1s
 }
 
