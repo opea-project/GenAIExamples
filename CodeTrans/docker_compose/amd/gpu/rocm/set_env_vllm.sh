971088282b02d614f899560aa079c71de6222911
ZePan110 <ze.pan@intel.com>
2025-04-28 13:53:50 +0800

    Integrate set_env to ut scripts for CodeTrans. (#1868)
    Signed-off-by: ZePan110 <ze.pan@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- CodeTrans/docker_compose/amd/gpu/rocm/set_env_vllm.sh
+++ CodeTrans/docker_compose/amd/gpu/rocm/set_env_vllm.sh
@@ -8,14 +8,14 @@
 # which can be used to connect to the server from the Internet. It must be specified in the EXTERNAL_HOST_IP variable.
 # If the server is used only on the internal network or has a direct external address,
 # specify it in HOST_IP and in EXTERNAL_HOST_IP.
-export HOST_IP=''
-export EXTERNAL_HOST_IP=''
+export HOST_IP=${ip_address}
+export EXTERNAL_HOST_IP=${ip_address}
 
 ### Model ID
 export CODETRANS_LLM_MODEL_ID="Qwen/Qwen2.5-Coder-7B-Instruct"
 
 ### The port of the TGI service. On this port, the TGI service will accept connections
-export CODETRANS_VLLM_SERVICE_PORT=18156
+export CODETRANS_VLLM_SERVICE_PORT=8008
 
 ### The endpoint of the TGI service to which requests to this service will be sent (formed from previously set variables)
 export CODETRANS_LLM_ENDPOINT="http://${HOST_IP}:${CODETRANS_VLLM_SERVICE_PORT}"
@@ -24,7 +24,7 @@ export CODETRANS_LLM_ENDPOINT="http://${HOST_IP}:${CODETRANS_VLLM_SERVICE_PORT}"
 export CODETRANS_HUGGINGFACEHUB_API_TOKEN=${HUGGINGFACEHUB_API_TOKEN}
 
 ### The port of the LLM service. On this port, the LLM service will accept connections
-export CODETRANS_LLM_SERVICE_PORT=18157
+export CODETRANS_LLM_SERVICE_PORT=9000
 
 ### The IP address or domain name of the server for CodeTrans MegaService
 export CODETRANS_MEGA_SERVICE_HOST_IP=${HOST_IP}
@@ -36,7 +36,7 @@ export CODETRANS_LLM_SERVICE_HOST_IP=${HOST_IP}
 export CODETRANS_FRONTEND_SERVICE_IP=${HOST_IP}
 
 ### The port of the frontend service
-export CODETRANS_FRONTEND_SERVICE_PORT=18155
+export CODETRANS_FRONTEND_SERVICE_PORT=5173
 
 ### Name of GenAI service for route requests to application
 export CODETRANS_BACKEND_SERVICE_NAME=codetrans
@@ -45,10 +45,10 @@ export CODETRANS_BACKEND_SERVICE_NAME=codetrans
 export CODETRANS_BACKEND_SERVICE_IP=${HOST_IP}
 
 ### The port of the backend service
-export CODETRANS_BACKEND_SERVICE_PORT=18154
+export CODETRANS_BACKEND_SERVICE_PORT=7777
 
 ### The port of the Nginx reverse proxy for application
-export CODETRANS_NGINX_PORT=18153
+export CODETRANS_NGINX_PORT=8088
 
 ### Endpoint of the backend service
 export CODETRANS_BACKEND_SERVICE_URL="http://${EXTERNAL_HOST_IP}:${CODETRANS_BACKEND_SERVICE_PORT}/v1/codetrans"
