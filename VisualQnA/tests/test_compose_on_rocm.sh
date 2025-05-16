f5859f94afac0ead68c797a0ab8d97f6b8218535
chensuyue <suyue.chen@intel.com>
2025-05-16 10:54:59 +0800

    Fix typo
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- VisualQnA/tests/test_compose_on_rocm.sh
+++ VisualQnA/tests/test_compose_on_rocm.sh
@@ -46,7 +46,7 @@ function build_docker_images() {
     popd && sleep 1s
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="chatqna chatqna-ui dataprep retriever vllm-rocm nginx"
+    service_list="visualqna visualqna-ui lvm nginx"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker images && sleep 1s
--- VisualQnA/tests/test_compose_vllm_on_rocm.sh
+++ VisualQnA/tests/test_compose_vllm_on_rocm.sh
@@ -45,7 +45,8 @@ function build_docker_images() {
     popd && sleep 1s
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
+    service_list="visualqna visualqna-ui lvm nginx vllm-rocm"
+    docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker images && sleep 1s
 }
