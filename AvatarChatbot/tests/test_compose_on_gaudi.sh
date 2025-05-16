fea01c9b3a276f576d3607fe003b0e72c63cd5c6
ZePan110 <ze.pan@intel.com>
2025-05-16 15:18:06 +0800

    Integrate AvatarChatbot set_env to ut scripts. (#1961)
    Signed-off-by: ZePan110 <ze.pan@intel.com>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- AvatarChatbot/tests/test_compose_on_gaudi.sh
+++ AvatarChatbot/tests/test_compose_on_gaudi.sh
@@ -45,37 +45,7 @@ function build_docker_images() {
 function start_services() {
     cd $WORKPATH/docker_compose/intel/hpu/gaudi
 
-    export HUGGINGFACEHUB_API_TOKEN=$HUGGINGFACEHUB_API_TOKEN
-    export host_ip=$(hostname -I | awk '{print $1}')
-
-    export LLM_MODEL_ID=Intel/neural-chat-7b-v3-3
-
-    export WAV2LIP_ENDPOINT=http://$host_ip:7860
-
-    export MEGA_SERVICE_HOST_IP=${host_ip}
-    export WHISPER_SERVER_HOST_IP=${host_ip}
-    export WHISPER_SERVER_PORT=7066
-    export SPEECHT5_SERVER_HOST_IP=${host_ip}
-    export SPEECHT5_SERVER_PORT=7055
-    export LLM_SERVER_HOST_IP=${host_ip}
-    export LLM_SERVER_PORT=3006
-    export ANIMATION_SERVICE_HOST_IP=${host_ip}
-    export ANIMATION_SERVICE_PORT=3008
-
-    export MEGA_SERVICE_PORT=8888
-
-    export DEVICE="hpu"
-    export WAV2LIP_PORT=7860
-    export INFERENCE_MODE='wav2lip+gfpgan'
-    export CHECKPOINT_PATH='/usr/local/lib/python3.10/dist-packages/Wav2Lip/checkpoints/wav2lip_gan.pth'
-    export FACE="/home/user/comps/animation/src/assets/img/avatar1.jpg"
-    # export AUDIO='assets/audio/eg3_ref.wav' # audio file path is optional, will use base64str in the post request as input if is 'None'
-    export AUDIO='None'
-    export FACESIZE=96
-    export OUTFILE="/outputs/result.mp4"
-    export GFPGAN_MODEL_VERSION=1.4 # latest version, can roll back to v1.3 if needed
-    export UPSCALE_FACTOR=1
-    export FPS=10
+    source set_env.sh
 
     # Start Docker Containers
     docker compose up -d > ${LOG_PATH}/start_services_with_compose.log
