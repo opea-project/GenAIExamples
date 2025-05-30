808e02674cb1a1ed066454ed6eb972d98911eb83
CICD-at-OPEA <CICD@opea.dev>
2025-05-30 22:41:57 +0000

    Update vLLM version to v0.9.0.1
    Signed-off-by: CICD-at-OPEA <CICD@opea.dev>

--- CodeGen/tests/test_compose_on_xeon.sh
+++ CodeGen/tests/test_compose_on_xeon.sh
@@ -42,7 +42,7 @@ function build_docker_images() {
     git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
 
     git clone https://github.com/vllm-project/vllm.git && cd vllm
-    VLLM_VER=v0.9.0
+    VLLM_VER=v0.9.0.1
     echo "Check out vLLM tag ${VLLM_VER}"
     git checkout ${VLLM_VER} &> /dev/null
     cd ../
