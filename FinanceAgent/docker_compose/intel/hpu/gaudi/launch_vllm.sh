34d40dcfc17092e945a797e9d976b42019a84687
Mustafa <109312699+MSCetin37@users.noreply.github.com>
2025-06-02 23:06:32 -0700

    Refine the README, folder/file hierarchy and test file for FinanceAgent     (#1996)
    Signed-off-by: Mustafa <mustafa.cetin@intel.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Signed-off-by: Ed Lee <16417837+edlee123@users.noreply.github.com>

--- FinanceAgent/docker_compose/intel/hpu/gaudi/launch_vllm.sh
+++
@@ -1,7 +0,0 @@
-# Copyright (C) 2025 Intel Corporation
-# SPDX-License-Identifier: Apache-2.0
-
-export LLM_MODEL_ID="meta-llama/Llama-3.3-70B-Instruct"
-export MAX_LEN=16384
-
-docker compose -f vllm_compose.yaml up -d
