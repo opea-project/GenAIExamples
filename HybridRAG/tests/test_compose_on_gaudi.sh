e48ac41caa38444dbfa31f9edb9357e32b9723c5
CICD-at-OPEA <cicd@opea.dev>
2025-06-09 14:47:43 +0800

    Update vLLM version to v0.9.0.1 (#1921)
    Signed-off-by: CICD-at-OPEA <CICD@opea.dev>
    Signed-off-by: cogniware-devops <ambarish.desai@cogniware.ai>

--- HybridRAG/tests/test_compose_on_gaudi.sh
+++ HybridRAG/tests/test_compose_on_gaudi.sh
@@ -45,8 +45,8 @@ function build_docker_images() {
     popd && sleep 1s
 
     git clone https://github.com/vllm-project/vllm.git && cd vllm
-    VLLM_VER="$(git describe --tags "$(git rev-list --tags --max-count=1)" )"
-    VLLM_VER="v0.8.3"
+    VLLM_VER=v0.9.0.1
+    VLLM_VER=v0.9.0.1
     echo "Check out vLLM tag ${VLLM_VER}"
     git checkout ${VLLM_VER} &> /dev/null
     # make sure NOT change the pwd
