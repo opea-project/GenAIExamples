dc4990ed5e114ce9f02237ce398e3bb923313e07
ZePan110 <ze.pan@intel.com>
2025-06-12 10:07:37 +0800

    Update VideoQnA/tests/test_compose_on_xeon.sh
    Signed-off-by: ZePan110 <ze.pan@intel.com>
    
    Co-authored-by: Eero Tamminen <eero.t.tamminen@intel.com>

--- VideoQnA/tests/test_compose_on_xeon.sh
+++ VideoQnA/tests/test_compose_on_xeon.sh
@@ -86,7 +86,7 @@ function build_docker_images() {
     sed -i "s|$OLD_STRING|$NEW_STRING|g" "GenAIComps/comps/third_parties/clip/src/Dockerfile"
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log
+    docker compose -f build.yaml build --no-cache > ${LOG_PATH}/docker_image_build.log 2>&1
 
 	docker pull intellabs/vdms:latest
     docker images && sleep 1s
