1ed0b915f2b690a70c2c07afba0168abd2a69e44
Yongbozzz <yongbo.zhu@intel.com>
2025-07-17 11:11:03 +0800

    minor fix
    Signed-off-by: Yongbozzz <yongbo.zhu@intel.com>

--- EdgeCraftRAG/edgecraftrag/api/v1/knowledge_base.py
+++ EdgeCraftRAG/edgecraftrag/api/v1/knowledge_base.py
@@ -41,9 +41,9 @@ async def create_knowledge_base(knowledge: KnowledgeBaseCreateIn):
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Knowledge base names must begin with a letter or underscore")
         kb = ctx.knowledgemgr.create_knowledge_base(knowledge)
-        if active_pl.indexer.comp_subtype == "milvus_vector" and kb.active:
-                    active_pl.indexer.reinitialize_indexer(kb.name)
-                    active_pl.update_indexer_to_retriever()
+        if kb.active:
+            active_pl.indexer.reinitialize_indexer(kb.name)
+            active_pl.update_indexer_to_retriever()
         await save_knowledge_to_file()
         return "Create knowledge base successfully"
     except Exception as e:
