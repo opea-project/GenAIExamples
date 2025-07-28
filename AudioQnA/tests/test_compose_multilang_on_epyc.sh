200ebca01849a2367247cce9acf3f946f239f1ae
Jereshea J M <jejohnma@amd.com>
2025-07-28 08:22:13 +0000

    VLLM docker error fix
    Signed-off-by: Jereshea J M <jejohnma@amd.com>

--- AudioQnA/tests/test_compose_multilang_on_epyc.sh
+++ AudioQnA/tests/test_compose_multilang_on_epyc.sh
@@ -18,115 +18,119 @@ LOG_PATH="$WORKPATH/tests/logs"
 ip_address=$(hostname -I | awk '{print $1}')
 
 function build_docker_images() {
-    opea_branch=${opea_branch:-"main"}
+	opea_branch=${opea_branch:-"main"}
 
-    cd $WORKPATH/docker_image_build
-    git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
-    pushd GenAIComps
-    echo "GenAIComps test commit is $(git rev-parse HEAD)"
-    docker build --no-cache -t ${REGISTRY}/comps-base:${TAG} --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
-    popd && sleep 1s
+	cd $WORKPATH/docker_image_build
+	git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
+	pushd GenAIComps
+	echo "GenAIComps test commit is $(git rev-parse HEAD)"
+	docker build --no-cache -t ${REGISTRY}/comps-base:${TAG} --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
+	popd && sleep 1s
 
-    git clone https://github.com/vllm-project/vllm.git
-    cd ./vllm/
-    VLLM_VER=v0.9.0.1
-    echo "Check out vLLM tag ${VLLM_VER}"
-    git checkout ${VLLM_VER} &> /dev/null && cd ../
+	git clone https://github.com/vllm-project/vllm.git
+	cd ./vllm/
+	VLLM_VER=v0.9.0.1
+	echo "Check out vLLM tag ${VLLM_VER}"
+	git checkout ${VLLM_VER} &>/dev/null
+	VLLM_REQ_FILE="requirements/cpu.txt"
+	if ! grep -q "^transformers" "$VLLM_REQ_FILE"; then
+		echo "Adding transformers<4.54.0 to $VLLM_REQ_FILE"
+		echo "transformers<4.54.0" >>"$VLLM_REQ_FILE"
+	fi
+	cd ../
 
-    echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="audioqna-multilang audioqna-ui whisper gpt-sovits vllm"
-    docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
+	echo "Build all the images with --no-cache, check docker_image_build.log for details..."
+	service_list="audioqna-multilang audioqna-ui whisper gpt-sovits vllm"
+	docker compose -f build.yaml build ${service_list} --no-cache >${LOG_PATH}/docker_image_build.log
 
-    docker images && sleep 1s
+	docker images && sleep 1s
 }
 
 function start_services() {
-    cd $WORKPATH/docker_compose/amd/cpu/epyc/
-    export host_ip=${ip_address}
-    source set_env.sh
-    # sed -i "s/backend_address/$ip_address/g" $WORKPATH/ui/svelte/.env
+	cd $WORKPATH/docker_compose/amd/cpu/epyc/
+	export host_ip=${ip_address}
+	source set_env.sh
+	# sed -i "s/backend_address/$ip_address/g" $WORKPATH/ui/svelte/.env
 
-    # Start Docker Containers
-    docker compose -f compose_multilang.yaml up -d > ${LOG_PATH}/start_services_with_compose.log
-    n=0
-    until [[ "$n" -ge 200 ]]; do
-       docker logs vllm-service > $LOG_PATH/vllm_service_start.log 2>&1
-       if grep -q complete $LOG_PATH/vllm_service_start.log; then
-           break
-       fi
-       sleep 5s
-       n=$((n+1))
-    done
+	# Start Docker Containers
+	docker compose -f compose_multilang.yaml up -d >${LOG_PATH}/start_services_with_compose.log
+	n=0
+	until [[ "$n" -ge 200 ]]; do
+		docker logs vllm-service >$LOG_PATH/vllm_service_start.log 2>&1
+		if grep -q complete $LOG_PATH/vllm_service_start.log; then
+			break
+		fi
+		sleep 5s
+		n=$((n + 1))
+	done
 }
-
 
 function validate_megaservice() {
-    sleep 120s
-    response=$(http_proxy="" curl --max-time 60 http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json')
+	sleep 120s
+	response=$(http_proxy="" curl --max-time 60 http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json')
 
-    if ! echo "$response" | sed 's/^"//;s/"$//' | base64 -d > speech.mp3 2>/dev/null || ! file speech.mp3 | grep -q "RIFF"; then
-        echo "Retrying..."
-        sleep 120s
-        response=$(http_proxy="" curl http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json')
+	if ! echo "$response" | sed 's/^"//;s/"$//' | base64 -d >speech.mp3 2>/dev/null || ! file speech.mp3 | grep -q "RIFF"; then
+		echo "Retrying..."
+		sleep 120s
+		response=$(http_proxy="" curl http://${ip_address}:3008/v1/audioqna -XPOST -d '{"audio": "UklGRigAAABXQVZFZm10IBIAAAABAAEARKwAAIhYAQACABAAAABkYXRhAgAAAAEA", "max_tokens":64}' -H 'Content-Type: application/json')
 
-        if echo "$response" | sed 's/^"//;s/"$//' | base64 -d > speech.mp3 2>/dev/null && file speech.mp3 | grep -q "RIFF"; then
-            echo "Retry succeeded."
-        else
-            echo "Retry failed. Invalid response."
-            exit 1
-        fi
-    else
-        echo "First attempt succeeded."
-    fi
-    # always print the log
-    docker logs whisper-service > $LOG_PATH/whisper-service.log
-    docker logs gpt-sovits-service > $LOG_PATH/tts-service.log
-    docker logs vllm-service > $LOG_PATH/vllm-service.log
-    docker logs audioqna-epyc-backend-server > $LOG_PATH/audioqna-epyc-backend-server.log
-    echo "$response" | sed 's/^"//;s/"$//' | base64 -d > speech.mp3
+		if echo "$response" | sed 's/^"//;s/"$//' | base64 -d >speech.mp3 2>/dev/null && file speech.mp3 | grep -q "RIFF"; then
+			echo "Retry succeeded."
+		else
+			echo "Retry failed. Invalid response."
+			exit 1
+		fi
+	else
+		echo "First attempt succeeded."
+	fi
+	# always print the log
+	docker logs whisper-service >$LOG_PATH/whisper-service.log
+	docker logs gpt-sovits-service >$LOG_PATH/tts-service.log
+	docker logs vllm-service >$LOG_PATH/vllm-service.log
+	docker logs audioqna-epyc-backend-server >$LOG_PATH/audioqna-epyc-backend-server.log
+	echo "$response" | sed 's/^"//;s/"$//' | base64 -d >speech.mp3
 
-    if [[ $(file speech.mp3) == *"RIFF"* ]]; then
-        echo "Result correct."
-    else
-        echo "Result wrong."
-        exit 1
-    fi
+	if [[ $(file speech.mp3) == *"RIFF"* ]]; then
+		echo "Result correct."
+	else
+		echo "Result wrong."
+		exit 1
+	fi
 
 }
 
-
 function stop_docker() {
-    cd $WORKPATH/docker_compose/amd/cpu/epyc/
-    docker compose -f compose_multilang.yaml stop && docker compose rm -f
+	cd $WORKPATH/docker_compose/amd/cpu/epyc/
+	docker compose -f compose_multilang.yaml stop && docker compose rm -f
 }
 
 function main() {
 
-    echo "::group::stop_docker"
-    stop_docker
-    docker system prune -f
-    echo "::endgroup::"
-    sleep 3s
+	echo "::group::stop_docker"
+	stop_docker
+	docker system prune -f
+	echo "::endgroup::"
+	sleep 3s
 
-    echo "::group::build_docker_images"
-    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
-    echo "::endgroup::"
-    sleep 3s
+	echo "::group::build_docker_images"
+	if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+	echo "::endgroup::"
+	sleep 3s
 
-    echo "::group::start_services"
-    start_services
-    echo "::endgroup::"
-    sleep 120s
+	echo "::group::start_services"
+	start_services
+	echo "::endgroup::"
+	sleep 120s
 
-    echo "::group::validate_megaservice"
-    validate_megaservice
-    echo "::endgroup::"
-    sleep 3s
+	echo "::group::validate_megaservice"
+	validate_megaservice
+	echo "::endgroup::"
+	sleep 3s
 
-    echo "::group::stop_docker"
-    stop_docker
-    docker system prune -f
-    echo "::endgroup::"
+	echo "::group::stop_docker"
+	stop_docker
+	docker system prune -f
+	echo "::endgroup::"
 
 }
 
--- ChatQnA/tests/test_compose_on_epyc.sh
+++ ChatQnA/tests/test_compose_on_epyc.sh
@@ -18,188 +18,192 @@ LOG_PATH="$WORKPATH/tests/logs"
 ip_address=$(hostname -I | awk '{print $1}')
 
 function build_docker_images() {
-    opea_branch=${opea_branch:-"main"}
-    cd $WORKPATH/docker_image_build
-    git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
-    pushd GenAIComps
-    echo "GenAIComps test commit is $(git rev-parse HEAD)"
-    docker build --no-cache -t ${REGISTRY}/comps-base:${TAG} --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
-    popd && sleep 1s
-    git clone https://github.com/vllm-project/vllm.git && cd vllm
-    VLLM_VER=v0.9.0.1
-    echo "Check out vLLM tag ${VLLM_VER}"
-    git checkout ${VLLM_VER} &> /dev/null
-    # make sure NOT change the pwd
-    cd ../
+	opea_branch=${opea_branch:-"main"}
+	cd $WORKPATH/docker_image_build
+	git clone --depth 1 --branch ${opea_branch} https://github.com/opea-project/GenAIComps.git
+	pushd GenAIComps
+	echo "GenAIComps test commit is $(git rev-parse HEAD)"
+	docker build --no-cache -t ${REGISTRY}/comps-base:${TAG} --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
+	popd && sleep 1s
+	git clone https://github.com/vllm-project/vllm.git && cd vllm
+	VLLM_VER=v0.9.0.1
+	echo "Check out vLLM tag ${VLLM_VER}"
+	git checkout ${VLLM_VER} &>/dev/null
+	VLLM_REQ_FILE="requirements/cpu.txt"
+	if ! grep -q "^transformers" "$VLLM_REQ_FILE"; then
+		echo "Adding transformers<4.54.0 to $VLLM_REQ_FILE"
+		echo "transformers<4.54.0" >>"$VLLM_REQ_FILE"
+	fi
+	cd ../
 
-    echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="chatqna chatqna-ui dataprep retriever vllm nginx"
-    docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
+	echo "Build all the images with --no-cache, check docker_image_build.log for details..."
+	service_list="chatqna chatqna-ui dataprep retriever vllm nginx"
+	docker compose -f build.yaml build ${service_list} --no-cache >${LOG_PATH}/docker_image_build.log
 
-    docker images && sleep 1s
+	docker images && sleep 1s
 }
 
 function start_services() {
-    cd $WORKPATH/docker_compose/amd/cpu/epyc
+	cd $WORKPATH/docker_compose/amd/cpu/epyc
 
-    source set_env.sh
+	source set_env.sh
 
-    # Start Docker Containers
-    docker compose -f compose.yaml -f compose.telemetry.yaml up -d --quiet-pull > ${LOG_PATH}/start_services_with_compose.log
-    n=0
-    until [[ "$n" -ge 100 ]]; do
-        docker logs vllm-service > ${LOG_PATH}/vllm_service_start.log 2>&1
-        if grep -q complete ${LOG_PATH}/vllm_service_start.log; then
-            break
-        fi
-        sleep 5s
-        n=$((n+1))
-    done
+	# Start Docker Containers
+	docker compose -f compose.yaml -f compose.telemetry.yaml up -d --quiet-pull >${LOG_PATH}/start_services_with_compose.log
+	n=0
+	until [[ "$n" -ge 100 ]]; do
+		docker logs vllm-service >${LOG_PATH}/vllm_service_start.log 2>&1
+		if grep -q complete ${LOG_PATH}/vllm_service_start.log; then
+			break
+		fi
+		sleep 5s
+		n=$((n + 1))
+	done
 }
 
 function validate_service() {
-    local URL="$1"
-    local EXPECTED_RESULT="$2"
-    local SERVICE_NAME="$3"
-    local DOCKER_NAME="$4"
-    local INPUT_DATA="$5"
+	local URL="$1"
+	local EXPECTED_RESULT="$2"
+	local SERVICE_NAME="$3"
+	local DOCKER_NAME="$4"
+	local INPUT_DATA="$5"
 
-    local HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL")
-    if [ "$HTTP_STATUS" -eq 200 ]; then
-        echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
+	local HTTP_STATUS=$(curl -s -o /dev/null -w "%{http_code}" -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL")
+	if [ "$HTTP_STATUS" -eq 200 ]; then
+		echo "[ $SERVICE_NAME ] HTTP status is 200. Checking content..."
 
-        local CONTENT=$(curl -s -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL" | tee ${LOG_PATH}/${SERVICE_NAME}.log)
+		local CONTENT=$(curl -s -X POST -d "$INPUT_DATA" -H 'Content-Type: application/json' "$URL" | tee ${LOG_PATH}/${SERVICE_NAME}.log)
 
-        if echo "$CONTENT" | grep -q "$EXPECTED_RESULT"; then
-            echo "[ $SERVICE_NAME ] Content is as expected."
-        else
-            echo "[ $SERVICE_NAME ] Content does not match the expected result: $CONTENT"
-            docker logs ${DOCKER_NAME} >> ${LOG_PATH}/${SERVICE_NAME}.log
-            exit 1
-        fi
-    else
-        echo "[ $SERVICE_NAME ] HTTP status is not 200. Received status was $HTTP_STATUS"
-        docker logs ${DOCKER_NAME} >> ${LOG_PATH}/${SERVICE_NAME}.log
-        exit 1
-    fi
-    sleep 1s
+		if echo "$CONTENT" | grep -q "$EXPECTED_RESULT"; then
+			echo "[ $SERVICE_NAME ] Content is as expected."
+		else
+			echo "[ $SERVICE_NAME ] Content does not match the expected result: $CONTENT"
+			docker logs ${DOCKER_NAME} >>${LOG_PATH}/${SERVICE_NAME}.log
+			exit 1
+		fi
+	else
+		echo "[ $SERVICE_NAME ] HTTP status is not 200. Received status was $HTTP_STATUS"
+		docker logs ${DOCKER_NAME} >>${LOG_PATH}/${SERVICE_NAME}.log
+		exit 1
+	fi
+	sleep 1s
 }
 
 function validate_microservices() {
-    # Check if the microservices are running correctly.
-    sleep 3m
+	# Check if the microservices are running correctly.
+	sleep 3m
 
-    # tei for embedding service
-    validate_service \
-        "${ip_address}:6006/embed" \
-        "\[\[" \
-        "tei-embedding" \
-        "tei-embedding-server" \
-        '{"inputs":"What is Deep Learning?"}'
+	# tei for embedding service
+	validate_service \
+		"${ip_address}:6006/embed" \
+		"\[\[" \
+		"tei-embedding" \
+		"tei-embedding-server" \
+		'{"inputs":"What is Deep Learning?"}'
 
-    # retrieval microservice
-    test_embedding=$(python3 -c "import random; embedding = [random.uniform(-1, 1) for _ in range(768)]; print(embedding)")
-    validate_service \
-        "${ip_address}:7000/v1/retrieval" \
-        " " \
-        "retrieval" \
-        "retriever-redis-server" \
-        "{\"text\":\"What is the revenue of Nike in 2023?\",\"embedding\":${test_embedding}}"
+	# retrieval microservice
+	test_embedding=$(python3 -c "import random; embedding = [random.uniform(-1, 1) for _ in range(768)]; print(embedding)")
+	validate_service \
+		"${ip_address}:7000/v1/retrieval" \
+		" " \
+		"retrieval" \
+		"retriever-redis-server" \
+		"{\"text\":\"What is the revenue of Nike in 2023?\",\"embedding\":${test_embedding}}"
 
-    # tei for rerank microservice
-    validate_service \
-        "${ip_address}:8808/rerank" \
-        '{"index":1,"score":' \
-        "tei-rerank" \
-        "tei-reranking-server" \
-        '{"query":"What is Deep Learning?", "texts": ["Deep Learning is not...", "Deep learning is..."]}'
+	# tei for rerank microservice
+	validate_service \
+		"${ip_address}:8808/rerank" \
+		'{"index":1,"score":' \
+		"tei-rerank" \
+		"tei-reranking-server" \
+		'{"query":"What is Deep Learning?", "texts": ["Deep Learning is not...", "Deep learning is..."]}'
 
-    # vllm for llm service
-    validate_service \
-        "${ip_address}:9009/v1/chat/completions" \
-        "content" \
-        "vllm-llm" \
-        "vllm-service" \
-        '{"model": "meta-llama/Meta-Llama-3-8B-Instruct", "messages": [{"role": "user", "content": "What is Deep Learning?"}], "max_tokens": 17}'
+	# vllm for llm service
+	validate_service \
+		"${ip_address}:9009/v1/chat/completions" \
+		"content" \
+		"vllm-llm" \
+		"vllm-service" \
+		'{"model": "meta-llama/Meta-Llama-3-8B-Instruct", "messages": [{"role": "user", "content": "What is Deep Learning?"}], "max_tokens": 17}'
 }
 
 function validate_megaservice() {
-    # Curl the Mega Service
-    validate_service \
-        "${ip_address}:8888/v1/chatqna" \
-        "Nike" \
-        "mega-chatqna" \
-        "chatqna-epyc-backend-server" \
-        '{"messages": "What is the revenue of Nike in 2023?"}'
+	# Curl the Mega Service
+	validate_service \
+		"${ip_address}:8888/v1/chatqna" \
+		"Nike" \
+		"mega-chatqna" \
+		"chatqna-epyc-backend-server" \
+		'{"messages": "What is the revenue of Nike in 2023?"}'
 
 }
 
 function validate_frontend() {
-    cd $WORKPATH/ui/svelte
-    local conda_env_name="OPEA_e2e"
-    export PATH=${HOME}/miniforge3/bin/:$PATH
-    if conda info --envs | grep -q "$conda_env_name"; then
-        echo "$conda_env_name exist!"
-    else
-        conda create -n ${conda_env_name} python=3.12 -y
-    fi
-    CONDA_ROOT=$(conda info --base)
-    source "${CONDA_ROOT}/etc/profile.d/conda.sh"
-    conda activate ${conda_env_name}
+	cd $WORKPATH/ui/svelte
+	local conda_env_name="OPEA_e2e"
+	export PATH=${HOME}/miniforge3/bin/:$PATH
+	if conda info --envs | grep -q "$conda_env_name"; then
+		echo "$conda_env_name exist!"
+	else
+		conda create -n ${conda_env_name} python=3.12 -y
+	fi
+	CONDA_ROOT=$(conda info --base)
+	source "${CONDA_ROOT}/etc/profile.d/conda.sh"
+	conda activate ${conda_env_name}
 
-    sed -i "s/localhost/$ip_address/g" playwright.config.ts
+	sed -i "s/localhost/$ip_address/g" playwright.config.ts
 
-    conda install -c conda-forge nodejs=22.6.0 -y
-    npm install && npm ci && npx playwright install --with-deps
-    node -v && npm -v && pip list
+	conda install -c conda-forge nodejs=22.6.0 -y
+	npm install && npm ci && npx playwright install --with-deps
+	node -v && npm -v && pip list
 
-    exit_status=0
-    npx playwright test || exit_status=$?
+	exit_status=0
+	npx playwright test || exit_status=$?
 
-    if [ $exit_status -ne 0 ]; then
-        echo "[TEST INFO]: ---------frontend test failed---------"
-        exit $exit_status
-    else
-        echo "[TEST INFO]: ---------frontend test passed---------"
-    fi
+	if [ $exit_status -ne 0 ]; then
+		echo "[TEST INFO]: ---------frontend test failed---------"
+		exit $exit_status
+	else
+		echo "[TEST INFO]: ---------frontend test passed---------"
+	fi
 }
 
 function stop_docker() {
-    cd $WORKPATH/docker_compose/amd/cpu/epyc
-    docker compose -f compose.yaml -f compose.telemetry.yaml down
+	cd $WORKPATH/docker_compose/amd/cpu/epyc
+	docker compose -f compose.yaml -f compose.telemetry.yaml down
 }
 
 function main() {
 
-    echo "::group::stop_docker"
-    stop_docker
-    echo "::endgroup::"
+	echo "::group::stop_docker"
+	stop_docker
+	echo "::endgroup::"
 
-    echo "::group::build_docker_images"
-    if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
-    echo "::endgroup::"
+	echo "::group::build_docker_images"
+	if [[ "$IMAGE_REPO" == "opea" ]]; then build_docker_images; fi
+	echo "::endgroup::"
 
-    echo "::group::start_services"
-    start_services
-    echo "::endgroup::"
+	echo "::group::start_services"
+	start_services
+	echo "::endgroup::"
 
-    echo "::group::validate_microservices"
-    validate_microservices
-    echo "::endgroup::"
+	echo "::group::validate_microservices"
+	validate_microservices
+	echo "::endgroup::"
 
-    echo "::group::validate_megaservice"
-    validate_megaservice
-    echo "::endgroup::"
+	echo "::group::validate_megaservice"
+	validate_megaservice
+	echo "::endgroup::"
 
-    echo "::group::validate_frontend"
-    validate_frontend
-    echo "::endgroup::"
+	echo "::group::validate_frontend"
+	validate_frontend
+	echo "::endgroup::"
 
-    echo "::group::stop_docker"
-    stop_docker
-    echo "::endgroup::"
+	echo "::group::stop_docker"
+	stop_docker
+	echo "::endgroup::"
 
-    docker system prune -f
+	docker system prune -f
 
 }
 
