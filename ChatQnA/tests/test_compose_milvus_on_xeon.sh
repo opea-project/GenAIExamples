e8e1564ddea90df6f27bb5d6cedd46695e7f04e8
ZePan110 <ze.pan@intel.com>
2025-09-08 14:45:22 +0800

    Use vllm release image for AudioQnA and ChatQnA. (#2239)
    Signed-off-by: ZePan110 <ze.pan@intel.com>

--- ChatQnA/tests/test_compose_milvus_on_xeon.sh
+++ ChatQnA/tests/test_compose_milvus_on_xeon.sh
@@ -24,15 +24,9 @@ function build_docker_images() {
     echo "GenAIComps test commit is $(git rev-parse HEAD)"
     docker build --no-cache -t ${REGISTRY}/comps-base:${TAG} --build-arg https_proxy=$https_proxy --build-arg http_proxy=$http_proxy -f Dockerfile .
     popd && sleep 1s
-    git clone https://github.com/vllm-project/vllm.git && cd vllm
-    VLLM_VER=v0.10.0
-    echo "Check out vLLM tag ${VLLM_VER}"
-    git checkout ${VLLM_VER} &> /dev/null
-    # make sure NOT change the pwd
-    cd ../
 
     echo "Build all the images with --no-cache, check docker_image_build.log for details..."
-    service_list="chatqna chatqna-ui dataprep retriever vllm nginx"
+    service_list="chatqna chatqna-ui dataprep retriever nginx"
     docker compose -f build.yaml build ${service_list} --no-cache > ${LOG_PATH}/docker_image_build.log
 
     docker images && sleep 1s
