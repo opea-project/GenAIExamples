04334a7a890b20ee942ef461c33b81a91b9c83f6
Yi Yao <yi.a.yao@intel.com>
2025-09-26 10:17:14 +0800

    Fix CI issues
    Signed-off-by: Yi Yao <yi.a.yao@intel.com>

--- DBQnA/ui/react/src/App.test.tsx
+++ DBQnA/ui/react/src/App.test.tsx
@@ -43,6 +43,6 @@ test('testing api with dynamic host', async () => {
   expect(result.hasOwnProperty('sql')).toBe(true);
   expect(result.hasOwnProperty('output')).toBe(true);
   expect(result.hasOwnProperty('input')).toBe(true);
-  expect(result.input.input_text).toBe(question);
+  expect(result.input.query).toBe(question);
 
 }, apiTimeOutInSeconds * 1000);
