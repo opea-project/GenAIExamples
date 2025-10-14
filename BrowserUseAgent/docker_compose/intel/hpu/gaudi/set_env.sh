b0950d346bb3607badc6038070c9b57090f304d1
letonghan <letong.han@intel.com>
2025-10-14 14:35:01 +0800

    init
    Signed-off-by: letonghan <letong.han@intel.com>

---
+++ BrowserUseAgent/docker_compose/intel/hpu/gaudi/set_env.sh
@@ -0,0 +1,9 @@
+# Copyright (C) 2024 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+export LLM_ENDPOINT_PORT=8008
+export DATA_PATH=./data
+export HF_TOKEN=<your_huggingface_token>
+export LLM_MODEL_ID="Qwen/Qwen2.5-VL-32B-Instruct"
+export MAX_TOTAL_TOKENS=4096
+export NUM_CARDS=4
